cbcae48be14f6f554ca74bf8e7f10b1d67b8124e
JimmyShi22 <417711026@qq.com>
2018-03-22 17:46:54 +0800

    Update deploy.js
    modify to english output

--- tool/deploy.js
+++ tool/deploy.js
@@ -21,7 +21,7 @@ if (typeof web3 !== 'undefined') {
 
 console.log('deploy.js  ........................Start........................');
 
-// 1 读取文件参数
+// 1 read file parameter
 var options = process.argv;
 if( options.length < 3 )
 {
@@ -35,13 +35,13 @@ console.log('Soc File :'+filename);
 
 
 try{ 
-  //用FISCO-BCOS的合约编译器fisco-solc进行编译
+  //Use fisco-solc to compile
   execSync("fisco-solc --abi  --bin   --overwrite -o "+config.Ouputpath+"  "+filename+".sol");
   
-  console.log(filename+'编译成功！');
+  console.log(filename+' compile success!');
 }catch(e){
   
-  console.log(filename+'编译失败!'+e);
+  console.log(filename+' compile failed!'+e);
 }
 
 var abi=JSON.parse(fs.readFileSync(config.Ouputpath/*+filename+".sol:"*/+filename+'.abi', 'utf-8'));
@@ -50,9 +50,9 @@ var binary=fs.readFileSync(config.Ouputpath+filename+'.bin', 'utf-8');
 (async function() {
 
   var Contract= await web3sync.rawDeploy(config.account, config.privKey,  filename);
-  console.log(filename+"部署成功！");
+  console.log(filename+" deploy success!");
   try {
-	 //尝试添加cns服务,有可能失败
+	 //try to add cns service, catch exceptions
 	 cns_tool.cnsAdd(filename);   
   } catch(e){
   //console.log(filename+'cns add failed , e = '+e);
