ba87ac716268bb2d506af3b75ed2cb961ea037dc
octopuswang <octopuswang@webank.com>
2018-03-27 17:11:06 +0800

    Add: 1. CNS添加对java客户端的支持 2. 添加支持CNS方式调用的rpc接口, CNS方式调用添加的新的接口如下： CNS方式调用接口                 原接口 eth_getCodeCNS             =>    eth_getCode eth_getBalanceCNS           =>    eth_getBalance eth_getStorageAtCNS         =>    eth_getStorageAt eth_getTransactionCountCNS   =>    eth_getTransactionCount
    Fix:
    CNS调用合约支持合约重载的接口

--- abi/ContractAbiMgr.cpp
+++ abi/ContractAbiMgr.cpp
@@ -125,15 +125,15 @@ namespace libabi
 
 	void ContractAbiMgr::getContractAbi(const std::string strContractName, const std::string &strVersion, SolidityAbi &abi)
 	{
-		std::string strFindKey = (strVersion.empty() ? strContractName : strContractName + "/" + strVersion);
+		std::string strCNSName = (strVersion.empty() ? strContractName : strContractName + "/" + strVersion);
 
 		bool isEndOK = false;
 		DEV_READ_GUARDED(m_abis_lock)
 		{
-			if (m_abis.find(strFindKey) != m_abis.end())
+			if (m_abis.find(strCNSName) != m_abis.end())
 			{
 				isEndOK = true;
-				abi = m_abis[strFindKey];
+				abi = m_abis[strCNSName];
 			}
 		}
 
@@ -157,11 +157,33 @@ namespace libabi
 
 	void ContractAbiMgr::addContractAbi(const SolidityAbi &abi)
 	{
-		std::string strFindKey = (abi.getVersion().empty() ? abi.getContractName() : abi.getContractName() + "/" + abi.getVersion());
+		std::string strCNSName = (abi.getVersion().empty() ? abi.getContractName() : abi.getContractName() + "/" + abi.getVersion());
 		//更新缓存
 		DEV_READ_GUARDED(m_abis_lock)
 		{
-			m_abis[strFindKey] = abi;
+			auto it = m_abis.find(strCNSName);
+			if (it != m_abis.end())
+			{
+				//更新操作
+				LOG(INFO) << "[ContractAbiMgr::addContractAbi] update operation"
+					<< " ,contract=" << it->second.getContractName()
+					<< " ,version=" << it->second.getVersion()
+					<< " ,address=" << it->second.getAddr()
+					<< " ,blocknumber=" << it->second.getBlockNumber().str()
+					<< " ,timestamp=" << it->second.getTimeStamp().str()
+					<< " ,abi=" << it->second.getAbi()
+					;
+			}
+			m_abis[strCNSName] = abi;
+
+			auto it0 = m_addrToName.find(abi.getAddr());
+			if (it0 != m_addrToName.end())
+			{
+				//更新操作
+				LOG(INFO) << "[ContractAbiMgr::addContractAbi] update operation"
+					<< " ,cnsname=" << it0->second;
+			}
+			m_addrToName[abi.getAddr()] = strCNSName;
 		}
 
 		//更新level-db
@@ -183,6 +205,62 @@ namespace libabi
 		libabi::SolidityAbi abiinfo{ strContractName,strVersion,"0x" + addr.hex(),strAbi ,blocknumber,timestamp };
 		addContractAbi(abiinfo);
 		return;
+	}
+
+	void ContractAbiMgr::getContractAbiByAddr(const std::string &strAddr, SolidityAbi &abi)
+	{
+		DEV_READ_GUARDED(m_abis_lock)
+		{
+			auto it = m_addrToName.find(strAddr);
+			if (it == m_addrToName.end())
+			{
+				ABI_EXCEPTION_THROW("not find cnsname by address,strAddr=" + strAddr, libabi::EnumAbiExceptionErrCode::EnumAbiExceptionErrCodeContractNotExist);
+			}
+
+			auto strCNSName = it -> second;
+			auto it0 = m_abis.find(strCNSName);
+			if (it0 == m_abis.end())
+			{
+				ABI_EXCEPTION_THROW("not find abi by cnsname,strAddr=" + strAddr + " ,cnsname=" + strCNSName, libabi::EnumAbiExceptionErrCode::EnumAbiExceptionErrCodeContractNotExist);
+			}
+
+			abi = it0->second;
+		}
+
+		//abi信息
+		LOG(DEBUG) << "[ContractAbiMgr::getContractAbiByAddr] end"
+			<< " ,contract=" << abi.getContractName()
+			<< " ,version=" << abi.getVersion()
+			<< " ,address=" << abi.getAddr()
+			<< " ,blocknumber=" << abi.getBlockNumber().str()
+			<< " ,timestamp=" << abi.getTimeStamp().str()
+			<< " ,abi=" << abi.getAbi()
+			;
+	}
+
+	void ContractAbiMgr::getContractAbi0(const std::string strContractName, const std::string &strVersion, SolidityAbi &abi)
+	{
+		if (m_isSystemContractInit)
+		{
+			getContractAbiFromCache(strContractName, strVersion, abi);
+		}
+		else
+		{
+			getContractAbiFromAbiDBMgr(strContractName, strVersion, abi);
+		}
+	}
+
+	void ContractAbiMgr::getContractAbiFromAbiDBMgr(const std::string strContractName, const std::string &strVersion, SolidityAbi &abi)
+	{
+		if (!m_contractAbiDBMgr->getAbi(strContractName, strVersion, abi))
+		{
+			ABI_EXCEPTION_THROW("get abi info from abi db mgr failed, contract|func|version=" + strContractName + "|" + strVersion, libabi::EnumAbiExceptionErrCode::EnumAbiExceptionErrCodeContractNotExist);
+		}
+	}
+
+	void ContractAbiMgr::getContractAbiFromCache(const std::string strContractName, const std::string &strVersion, SolidityAbi &abi)
+	{
+		getContractAbi(strContractName, strVersion, abi);
 	}
 
 	std::pair<Address, bytes> ContractAbiMgr::getAddrAndDataInfo(const std::string &strContractName, const std::string &strFunc, const std::string &strVer, const Json::Value &jParams)
