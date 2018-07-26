3e2960ef131de7a6dacfd3a7d30152c2be712e1d
651932351@qq.com <651932351@qq.com>
2018-07-26 19:46:29 +0800

    修复session()可能为空导致程序错误的问题

--- libpbftseal/PBFTPeer.h
+++ libpbftseal/PBFTPeer.h
@@ -51,7 +51,7 @@ public:
 	/// How many message types do we have?
 	static unsigned messageCount() { return PBFTPacketCount; }
 
-	p2p::NodeID id() const { return session()->id(); }
+	//p2p::NodeID id() const { return session()->id(); }
 
 protected:
 	virtual bool interpret(unsigned _id, RLP const& _r);
