7409954b72ac769054461b0210bebffe2c4aa2b6
wheatli <lhznice@gmail.com>
2018-08-28 19:06:58 +0800

    adjust coding style of VMFace.h

--- libevm/VMFace.h
+++ libevm/VMFace.h
@@ -1,18 +1,18 @@
 /*
-	This file is part of cpp-ethereum.
+    This file is part of cpp-ethereum.
 
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+    cpp-ethereum is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    cpp-ethereum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
 /** @file VMFace.h
  * @author wheatli
@@ -34,13 +34,13 @@ namespace eth
 class VMFace
 {
 public:
-	VMFace() = default;
-	virtual ~VMFace() = default;
-	VMFace(VMFace const&) = delete;
-	VMFace& operator=(VMFace const&) = delete;
+    VMFace() = default;
+    virtual ~VMFace() = default;
+    VMFace(VMFace const&) = delete;
+    VMFace& operator=(VMFace const&) = delete;
 
-	/// VM implementation
-	virtual owning_bytes_ref exec(u256& io_gas, ExtVMFace& _ext, OnOpFunc const& _onOp) = 0;
+    /// VM implementation
+    virtual owning_bytes_ref exec(u256& io_gas, ExtVMFace& _ext, OnOpFunc const& _onOp) = 0;
 };
-}
-}
+}  // namespace eth
+}  // namespace dev
