16796076ad25a55f6d2d26a071ae7023236ee256
bxq2011hust <bxq2011hust@qq.com>
2018-08-31 17:32:29 +0800

    Merge 1.5 2nd try; Single node can run

--- unittest/libstorage/test_EntryPrecompiled.cpp
+++ unittest/libstorage/test_EntryPrecompiled.cpp
@@ -47,9 +47,8 @@ BOOST_AUTO_TEST_CASE(testEntry) {
   BOOST_TEST(entryPrecompiled->getEntry() == entry);
 }
 
-BOOST_AUTO_TEST_CASE(testCall) {
+BOOST_AUTO_TEST_CASE(testGetInt) {
   entry->setField("keyInt", "100");
-  entry->setField("keyString", "hello");
   ContractABI abi;
 
   bytes bint = abi.abiIn("getInt(string)", "keyInt");
@@ -57,6 +56,68 @@ BOOST_AUTO_TEST_CASE(testCall) {
   u256 num;
   abi.abiOut(bytesConstRef(&out), num);
   BOOST_TEST(num == u256(100));
+}
+
+BOOST_AUTO_TEST_CASE(testGetString) {
+  entry->setField("keyString", "hello");
+  ContractABI abi;
+
+  bytes bstr = abi.abiIn("getString(string)", "keyString");
+  bytes out = entryPrecompiled->call(precompiledContext, bytesConstRef(&bstr));
+  Address address;
+  abi.abiOut(bytesConstRef(&out), address);
+  auto stringPrecompiled = precompiledContext->getPrecompiled(address);
+  std::string outstr = stringPrecompiled->toString(precompiledContext);
+  BOOST_TEST(outstr == "hello");
+}
+
+BOOST_AUTO_TEST_CASE(testSetAddress) {
+  ContractABI abi;
+  Address address =
+      stringFactoryPrecompiled->newString(precompiledContext, "world");
+  bytes sstr = abi.abiIn("set(string,address)", "keyAddress", address);
+  entryPrecompiled->call(precompiledContext, bytesConstRef(&sstr));
+
+  entryPrecompiled->getEntry()->getField("keyAddress") == "world";
+}
+
+BOOST_AUTO_TEST_CASE(testGetAddress) {
+  ContractABI abi;
+  entry->setField("keyAddress", "1000");
+  bytes gstr = abi.abiIn("getAddress(string)", "keyAddress");
+  bytes out = entryPrecompiled->call(precompiledContext, bytesConstRef(&gstr));
+  Address address;
+  abi.abiOut(bytesConstRef(&out), address);
+  BOOST_TEST(address == Address("1000"));
+}
+
+BOOST_AUTO_TEST_CASE(testSetInt) {
+  ContractABI abi;
+  bytes sstr = abi.abiIn("set(string,int256)", "keyInt", u256(200));
+  entryPrecompiled->call(precompiledContext, bytesConstRef(&sstr));
+  BOOST_TEST(entry->getField("keyInt") ==
+             boost::lexical_cast<std::string>(200));
+}
+
+BOOST_AUTO_TEST_CASE(testSetString) {
+  ContractABI abi;
+  bytes sstr = abi.abiIn("set(string,string)", "keyString", "you");
+  entryPrecompiled->call(precompiledContext, bytesConstRef(&sstr));
+  BOOST_TEST(entry->getField("keyString") == "you");
+}
+
+BOOST_AUTO_TEST_CASE(testGetBytes64) {
+  entry->setField("keyString", "1000");
+  ContractABI abi;
+  bytes sstr = abi.abiIn("getBytes64(string)", "keyString");
+  bytes out = entryPrecompiled->call(precompiledContext, bytesConstRef(&sstr));
+  string64 retout;
+  abi.abiOut(bytesConstRef(&out), retout);
+  std::string s = "1000";
+  string64 ret;
+  for (unsigned i = 0; i < retout.size(); ++i)
+        ret[i] = i < s.size() ? s[i] : 0;
+  BOOST_TEST(retout == ret);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
