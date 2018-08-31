b573d6cb011524588d6601d44127f24d964037e9
cyjseagull <1547158861@qq.com>
2018-08-31 22:14:24 +0800

    remove Exceptions related POW of Eth(seedHash and MixHash)

--- libethcore/Exceptions.h
+++ libethcore/Exceptions.h
@@ -38,11 +38,7 @@ using errinfo_name = boost::error_info<struct tag_field, std::string>;
 using errinfo_field = boost::error_info<struct tag_field, int>;
 using errinfo_data = boost::error_info<struct tag_data, std::string>;
 using errinfo_nonce = boost::error_info<struct tag_nonce, h64>;
-using errinfo_difficulty = boost::error_info<struct tag_difficulty, u256>;
 using errinfo_target = boost::error_info<struct tag_target, h256>;
-using errinfo_seedHash = boost::error_info<struct tag_seedHash, h256>;
-using errinfo_mixHash = boost::error_info<struct tag_mixHash, h256>;
-using errinfo_ethashResult = boost::error_info<struct tag_ethashResult, std::tuple<h256, h256>>;
 using errinfo_importResult = boost::error_info<struct tag_importResult, ImportResult>;
 using BadFieldError = boost::tuple<errinfo_field, errinfo_data>;
 
