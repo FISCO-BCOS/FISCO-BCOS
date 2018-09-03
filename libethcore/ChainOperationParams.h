f575b4e83747506c86ed2fa4a004e28b66e8e299
tabsu <yuhui@hackergay.com>
2018-09-03 11:35:47 +0800

    remove some unused func

--- libethcore/ChainOperationParams.h
+++ libethcore/ChainOperationParams.h
@@ -52,40 +52,24 @@ private:
     u256 m_startingBlock = 0;
 };
 
-static constexpr int64_t c_infiniteBlockNumer = std::numeric_limits<int64_t>::max();
-
 struct ChainOperationParams
 {
     ChainOperationParams();
 
     explicit operator bool() const { return accountStartNonce != Invalid256; }
 
-    /// The chain sealer name: e.g. Ethash, NoProof, BasicAuthority
-    std::string sealEngineName = "NoProof";
-
-    /// General chain params.
-private:
-    u256 m_blockReward;
+    /// The chain sealer name: e.g. PBFT, Raft, SinglePoint
+    std::string sealEngineName = "PBFT";
 
 public:
     EVMSchedule const& scheduleForBlockNumber(u256 const& _blockNumber) const;
-    u256 blockReward(EVMSchedule const& _schedule) const;
-    void setBlockReward(u256 const& _newBlockReward);
     u256 maximumExtraDataSize = 1024;
     u256 accountStartNonce = 0;
     bool tieBreakingGas = true;
     u256 minGasLimit;
     u256 maxGasLimit;
     u256 gasLimitBoundDivisor;
-    u256 homesteadForkBlock = c_infiniteBlockNumer;
-    u256 EIP150ForkBlock = c_infiniteBlockNumer;
-    u256 EIP158ForkBlock = c_infiniteBlockNumer;
-    u256 byzantiumForkBlock = c_infiniteBlockNumer;
-    u256 eWASMForkBlock = c_infiniteBlockNumer;
-    u256 constantinopleForkBlock = c_infiniteBlockNumer;
-    u256 daoHardforkBlock = c_infiniteBlockNumer;
-    u256 experimentalForkBlock = c_infiniteBlockNumer;
-    int chainID = 0;    // Distinguishes different chains (mainnet, Ropsten, etc).
+    
     int networkID = 0;  // Distinguishes different sub protocols.
 
     u256 minimumDifficulty;
