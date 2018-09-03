9792510bb3175b1f18d02ea19ef48476e1febbd9
tabsu <yuhui@hackergay.com>
2018-09-03 14:19:07 +0800

    fixed unused code in SealEngine

--- libethcore/ChainOperationParams.cpp
+++ libethcore/ChainOperationParams.cpp
@@ -25,21 +25,8 @@ using namespace std;
 using namespace dev;
 using namespace eth;
 
-PrecompiledContract::PrecompiledContract(
-    unsigned _base, unsigned _word, PrecompiledExecutor const& _exec, u256 const& _startingBlock)
-  : PrecompiledContract(
-        [=](bytesConstRef _in) -> bigint {
-            bigint s = _in.size();
-            bigint b = _base;
-            bigint w = _word;
-            return b + (s + 31) / 32 * w;
-        },
-        _exec, _startingBlock)
-{}
-
 ChainOperationParams::ChainOperationParams()
-  : m_blockReward("0x4563918244F40000"),
-    minGasLimit(0x1388),
+  : minGasLimit(0x1388),
     maxGasLimit("0x7fffffffffffffff"),
     gasLimitBoundDivisor(0x0400),
     networkID(0x0),
@@ -48,35 +35,7 @@ ChainOperationParams::ChainOperationParams()
     durationLimit(0x0d)
 {}
 
-EVMSchedule const& ChainOperationParams::scheduleForBlockNumber(u256 const& _blockNumber) const
+EVMSchedule const& ChainOperationParams::scheduleForBlockNumber(u256 const&) const
 {
-    if (_blockNumber >= experimentalForkBlock)
-        return ExperimentalSchedule;
-    else if (_blockNumber >= constantinopleForkBlock)
-        return ConstantinopleSchedule;
-    else if (_blockNumber >= eWASMForkBlock)
-        return EWASMSchedule;
-    else if (_blockNumber >= byzantiumForkBlock)
-        return ByzantiumSchedule;
-    else if (_blockNumber >= EIP158ForkBlock)
-        return EIP158Schedule;
-    else if (_blockNumber >= EIP150ForkBlock)
-        return EIP150Schedule;
-    else if (_blockNumber >= homesteadForkBlock)
-        return HomesteadSchedule;
-    else
-        return FrontierSchedule;
-}
-
-u256 ChainOperationParams::blockReward(EVMSchedule const& _schedule) const
-{
-    if (_schedule.blockRewardOverwrite)
-        return *_schedule.blockRewardOverwrite;
-    else
-        return m_blockReward;
-}
-
-void ChainOperationParams::setBlockReward(u256 const& _newBlockReward)
-{
-    m_blockReward = _newBlockReward;
+    return FiscoBcosSchedule;
 }
