bcd44e2290aaee23648fa728cc83c1428129d4ec
tabsu <yuhui@hackergay.com>
2018-09-04 11:05:08 +0800

    remoce single point

--- libethcore/ChainOperationParams.h
+++ libethcore/ChainOperationParams.h
@@ -23,6 +23,7 @@
 
 #include "Common.h"
 #include "EVMSchedule.h"
+#include "PrecompiledContract.h"
 #include <libdevcore/Common.h>
 #include <libethcore/Precompiled.h>
 
@@ -30,35 +31,13 @@ namespace dev
 {
 namespace eth
 {
-class PrecompiledContract
-{
-public:
-    PrecompiledContract() = default;
-    PrecompiledContract(PrecompiledPricer const& _cost, PrecompiledExecutor const& _exec,
-        u256 const& _startingBlock = 0)
-      : m_cost(_cost), m_execute(_exec), m_startingBlock(_startingBlock)
-    {}
-    PrecompiledContract(unsigned _base, unsigned _word, PrecompiledExecutor const& _exec,
-        u256 const& _startingBlock = 0);
-
-    bigint cost(bytesConstRef _in) const { return m_cost(_in); }
-    std::pair<bool, bytes> execute(bytesConstRef _in) const { return m_execute(_in); }
-
-    u256 const& startingBlock() const { return m_startingBlock; }
-
-private:
-    PrecompiledPricer m_cost;
-    PrecompiledExecutor m_execute;
-    u256 m_startingBlock = 0;
-};
-
 struct ChainOperationParams
 {
     ChainOperationParams();
 
     explicit operator bool() const { return accountStartNonce != Invalid256; }
 
-    /// The chain sealer name: e.g. PBFT, Raft, SinglePoint
+    /// The chain sealer name: e.g. PBFT, Raft.
     std::string sealEngineName = "PBFT";
 
 public:
