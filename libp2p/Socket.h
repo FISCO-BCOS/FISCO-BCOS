aeab4ef055ea0709ba2606b8c96ad884a1b78ee7
cyjseagull <1547158861@qq.com>
2018-09-19 22:04:31 +0800

    modify sslcontext initialize

--- libp2p/Socket.h
+++ libp2p/Socket.h
@@ -44,7 +44,6 @@ namespace p2p
 class Socket : public SocketFace, public std::enable_shared_from_this<Socket>
 {
 public:
-    Socket() = default;
     Socket(ba::io_service& _ioService, NodeIPEndpoint _nodeIPEndpoint)
       : m_nodeIPEndpoint(_nodeIPEndpoint)
     {
@@ -68,7 +67,6 @@ public:
         try
         {
             boost::system::error_code ec;
-
             m_sslSocket->lowest_layer().shutdown(bi::tcp::socket::shutdown_both, ec);
             if (m_sslSocket->lowest_layer().is_open())
                 m_sslSocket->lowest_layer().close();
@@ -85,9 +83,7 @@ public:
     }
 
     bi::tcp::socket& ref() { return m_sslSocket->next_layer(); }
-
     ba::ssl::stream<bi::tcp::socket>& sslref() { return *m_sslSocket; }
-
     static void initContext()
     {
         if (!Socket::m_isInit)
@@ -102,10 +98,16 @@ public:
 
             CertificateServer::GetInstance().getCertificateList(certificates, nodePrivateKey);
 
-            EC_KEY* ecdh = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
+#if defined(SSL_CTX_set_ecdh_auto)
+            SSL_CTX_set_ecdh_auto(m_sslContext.native_handle(), 1);
+            LOG(INFO) << "use SSL_CTX_set_ecdh_auto";
+#else
+
+            EC_KEY* ecdh = EC_KEY_new_by_curve_name(NID_secp256k1);
             SSL_CTX_set_tmp_ecdh(m_sslContext.native_handle(), ecdh);
             EC_KEY_free(ecdh);
-
+            LOG(INFO) << "use SSL_CTX_set_tmp_ecdh";
+#endif
             m_sslContext.set_verify_depth(3);
             m_sslContext.set_verify_mode(ba::ssl::verify_peer);
 
@@ -157,7 +159,6 @@ public:
                 LOG(ERROR) << "Ssl Socket Init Fail! Please Check node.key !";
                 exit(-1);
             }
-
             Socket::m_isInit = true;
         }
     }
@@ -165,11 +166,11 @@ public:
     const NodeIPEndpoint& nodeIPEndpoint() const { return m_nodeIPEndpoint; }
     void setNodeIPEndpoint(NodeIPEndpoint _nodeIPEndpoint) { m_nodeIPEndpoint = _nodeIPEndpoint; }
     boost::asio::ip::tcp::endpoint remote_endpoint() { return ref().remote_endpoint(); }
+    static ba::ssl::context m_sslContext;
+    static bool m_isInit;
 
 protected:
     NodeIPEndpoint m_nodeIPEndpoint;
-    static ba::ssl::context m_sslContext;
-    static bool m_isInit;
     std::shared_ptr<ba::ssl::stream<bi::tcp::socket> > m_sslSocket;
 };
 
