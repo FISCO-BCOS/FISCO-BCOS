15fc8926733d18afb731ab4d4096a7dad593e741
cyjseagull <1547158861@qq.com>
2018-09-25 19:56:28 +0800

    add implementation for TxPool

--- libtxpool/TxPoolInterface.h
+++ libtxpool/TxPoolInterface.h
@@ -38,8 +38,7 @@ public:
      * @brief Remove transaction from the queue
      * @param _txHash: transaction hash
      */
-    virtual void drop(h256 const& _txHash) = 0;
-    virtual void dropGood(Transaction const& _t) = 0;
+    virtual bool drop(h256 const& _txHash) = 0;
     /**
      * @brief Get top transactions from the queue
      *
@@ -47,11 +46,11 @@ public:
      * @param _avoid : Transactions to avoid returning.
      * @return Transactions : up to _limit transactions
      */
-    virtual Transactions topTransactions(
+    virtual dev::eth::Transactions topTransactions(
         unsigned _limit, h256Hash const& _avoid = h256Hash()) const = 0;
 
     /// get all current transactions(maybe blocksync module need this interface)
-    virtual Transactions pendingList() const = 0;
+    virtual dev::eth::Transactions pendingList() const = 0;
     /// get current transaction num
     virtual size_t pendingSize() = 0;
 
@@ -60,7 +59,7 @@ public:
      * @param _t : transaction
      * @return std::pair<h256, Address>: maps from transaction hash to contract address
      */
-    virtual std::pair<h256, Address> submit(Transaction const& _tx) = 0;
+    virtual std::pair<h256, Address> submit(dev::eth::Transaction& _tx) = 0;
 
     /**
      * @brief : submit a transaction through p2p, Verify and add transaction to the queue
@@ -69,14 +68,16 @@ public:
      * @param _ik : Set to Retry to force re-addinga transaction that was previously dropped.
      * @return ImportResult : Import result code.
      */
-    virtual ImportResult import(Transaction const& _tx, IfDropped _ik = IfDropped::Ignore);
-
+    virtual dev::eth::ImportResult import(
+        dev::eth::Transaction& _tx, dev::eth::IfDropped _ik = dev::eth::IfDropped::Ignore) = 0;
+    virtual dev::eth::ImportResult import(
+        bytesConstRef _txBytes, dev::eth::IfDropped _ik = dev::eth::IfDropped::Ignore) = 0;
     /// @returns the status of the transaction queue.
     virtual TxPoolStatus status() const = 0;
 
     /// protocol id used when register handler to p2p module
-    virtual const int32_t getProtocolId() const = 0;
-    virtual void setProtocolId(uint32_t const _protocolId) = 0;
+    virtual int32_t const& getProtocolId() const = 0;
+    virtual void setProtocolId(uint32_t const& _protocolId) = 0;
 };
 }  // namespace txpool
 }  // namespace dev
