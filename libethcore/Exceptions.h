67ede5b9a5d0205d7e7c3068d45d1aaf7a42e981
cyjseagull <1547158861@qq.com>
2018-09-26 20:43:01 +0800

    remove p2p related exceptions

--- libethcore/Exceptions.h
+++ libethcore/Exceptions.h
@@ -68,7 +68,8 @@ DEV_SIMPLE_EXCEPTION(PendingTransactionAlreadyExists);
 DEV_SIMPLE_EXCEPTION(TransactionAlreadyInChain);
 DEV_SIMPLE_EXCEPTION(UnknownTransactionValidationError);
 DEV_SIMPLE_EXCEPTION(InconsistentTransactionSha3);
-DEV_SIMPLE_EXCEPTION(P2pObtainTransactionFailed);
+DEV_SIMPLE_EXCEPTION(P2pEnqueueTransactionFailed);
+DEV_SIMPLE_EXCEPTION(MalformedTransactionException);
 
 /// state trie related
 DEV_SIMPLE_EXCEPTION(InvalidStateRoot);
