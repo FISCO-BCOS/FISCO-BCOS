0203d50fbf8bc4f4b59df484be252a68019bcfb5
bxq2011hust <bxq2011hust@qq.com>
2018-09-27 23:04:30 +0800

    fix: change MINIUPNPC FATDB default OFF

--- cmake/EthOptions.cmake
+++ cmake/EthOptions.cmake
@@ -12,10 +12,10 @@ macro(configure_project)
 	# features
 	eth_default_option(VMTRACE OFF)
 	eth_default_option(PROFILING OFF)
-	eth_default_option(FATDB ON)
+	eth_default_option(FATDB OFF)
 	eth_default_option(ROCKSDB OFF)
 	eth_default_option(PARANOID OFF)
-	eth_default_option(MINIUPNPC ON)
+	eth_default_option(MINIUPNPC OFF)
 	
 	#ARCH TYPE
 	eth_default_option(STATIC_BUILD OFF)
