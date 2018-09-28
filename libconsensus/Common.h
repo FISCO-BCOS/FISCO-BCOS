aea9600181ca870e693dbe1360765d6462dcd8c4
cyjseagull <1547158861@qq.com>
2018-09-28 11:04:46 +0800

    add common information related to PBFT message into Common.h

--- libconsensus/Common.h
+++ libconsensus/Common.h
@@ -22,6 +22,11 @@
  */
 #pragma once
 #include <libdevcore/FixedHash.h>
+#include <libdevcore/RLP.h>
+#include <libdevcore/SHA3.h>
+#include <libdevcore/easylog.h>
+#include <libdevcrypto/Common.h>
+#include <libethcore/Exceptions.h>
 namespace dev
 {
 namespace consensus
@@ -37,5 +42,121 @@ struct ConsensusStatus
     dev::h512s minerList;
 };
 
+// for pbft
+struct PBFTMsg
+{
+    u256 height = Invalid256;
+    u256 view = Invalid256;
+    u256 idx = Invalid256;
+    u256 timestamp = Invalid256;
+    h256 block_hash;
+    Signature sig;   // signature of block_hash
+    Signature sig2;  // other fileds' signature
+
+    virtual void encode(bytes& encodedBytes) const
+    {
+        RLPStream tmp;
+        streamRLPFields(tmp);
+        tmp.swapOut(encodedBytes);
+    }
+
+    virtual void decode(bytesConstRef data)
+    {
+        RLP rlp(data);
+        populate(rlp);
+    }
+
+    void streamRLPFields(RLPStream& _s) const
+    {
+        _s << height << view << idx << timestamp << block_hash << sig.asBytes() << sig2.asBytes();
+    }
+
+    void populate(RLP const& _rlp)
+    {
+        int field = 0;
+        try
+        {
+            height = _rlp[field = 0].toInt<u256>();
+            view = _rlp[field = 1].toInt<u256>();
+            idx = _rlp[field = 2].toInt<u256>();
+            timestamp = _rlp[field = 3].toInt<u256>();
+            block_hash = _rlp[field = 4].toHash<h256>(RLP::VeryStrict);
+            sig = dev::Signature(_rlp[field = 5].toBytesConstRef());
+            sig2 = dev::Signature(_rlp[field = 6].toBytesConstRef());
+        }
+        catch (Exception const& _e)
+        {
+            _e << dev::eth::errinfo_name("invalid msg format")
+               << dev::eth::BadFieldError(field, toHex(_rlp[field].data().toBytes()));
+            throw;
+        }
+    }
+
+    void clear()
+    {
+        height = Invalid256;
+        view = Invalid256;
+        idx = Invalid256;
+        timestamp = Invalid256;
+        block_hash = h256();
+        sig = Signature();
+        sig2 = Signature();
+    }
+
+    h256 fieldsWithoutBlock() const
+    {
+        RLPStream ts;
+        ts << height << view << idx << timestamp;
+        return dev::sha3(ts.out());
+    }
+};
+
+struct PrepareReq : public PBFTMsg
+{
+    bytes block;
+    virtual void encode(bytes& encodedBytes) const
+    {
+        RLPStream s;
+        streamRLPFields(s);
+        s.swapOut(encodedBytes);
+    }
+    virtual void decode(bytesConstRef data)
+    {
+        RLP rlp(data);
+        populate(rlp);
+    }
+
+    void streamRLPFields(RLPStream& _s) const
+    {
+        PBFTMsg::streamRLPFields(_s);
+        _s << block;
+    }
+
+    void populate(RLP const& _rlp)
+    {
+        PBFTMsg::populate(_rlp);
+        int field = 0;
+        try
+        {
+            block = _rlp[field = 7].toBytes();
+        }
+        catch (Exception const& _e)
+        {
+            _e << dev::eth::errinfo_name("invalid msg format")
+               << dev::eth::BadFieldError(field, toHex(_rlp[field].data().toBytes()));
+            throw;
+        }
+    }
+};
+
+struct SignReq : public PBFTMsg
+{
+};
+struct CommitReq : public PBFTMsg
+{
+};
+struct ViewChangeReq : public PBFTMsg
+{
+};
 }  // namespace consensus
 }  // namespace dev
