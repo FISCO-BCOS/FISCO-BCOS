93f4a13f1579b5c3c5ac7434aa03e57168ee40ff
bxq2011hust <bxq2011hust@qq.com>
2018-10-17 15:27:40 +0800

    fix: SIGINT ChannelServer quit failed

--- docker/node1/stop.sh
+++ docker/node1/stop.sh
@@ -2,13 +2,13 @@
 #stop fisco-bcos
 name=/fisco-bcos/node/config.json
 agent_pid=`ps aux|grep "$name"|grep -v grep|awk '{print $2}'`
-kill_cmd="kill -9 ${agent_pid}"
+kill_cmd="kill ${agent_pid}"
 echo "${kill_cmd}"
 eval ${kill_cmd}
 
 #stop tail -f log
 name=/fisco-bcos/node/log/info
 agent_pid=`ps aux|grep "$name"|grep -v grep|awk '{print $2}'`
-kill_cmd="kill -9 ${agent_pid}"
+kill_cmd="kill ${agent_pid}"
 echo "${kill_cmd}"
 eval ${kill_cmd}
