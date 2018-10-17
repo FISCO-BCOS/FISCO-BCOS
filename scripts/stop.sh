6f4e04699bafa5e71db0fb3831f022218bc7b224
bxq2011hust <bxq2011hust@qq.com>
2018-10-17 15:27:40 +0800

    fix: SIGINT ChannelServer quit failed

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,4 +2,4 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 fisco_bcos=${SHELL_FOLDER}/fisco-bcos
 weth_pid=`ps aux|grep "${fisco_bcos}"|grep -v grep|awk '{print $2}'`
-kill -9 ${weth_pid}
+kill ${weth_pid}
