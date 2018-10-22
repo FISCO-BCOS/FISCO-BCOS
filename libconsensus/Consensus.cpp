6b4a4d6e882c20a8dd0c69f47cecdb6786181e3d
cyjseagull <1547158861@qq.com>
2018-10-22 13:21:54 +0800

    recover the wait logic after checkTxEnough

--- libconsensus/Consensus.cpp
+++ libconsensus/Consensus.cpp
@@ -103,8 +103,8 @@ void Consensus::doWork(bool wait)
                 if (!checkTxsEnough(max_blockCanSeal))
                 {
                     ///< 10 milliseconds to next loop
-                    /// std::unique_lock<std::mutex> l(x_signalled);
-                    /// m_signalled.wait_for(l, std::chrono::milliseconds(1));
+                    std::unique_lock<std::mutex> l(x_signalled);
+                    m_signalled.wait_for(l, std::chrono::milliseconds(1));
                     return;
                 }
                 handleBlock();
