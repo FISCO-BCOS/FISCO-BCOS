a8dcb4f65300ed35e4b5087fbaa956a4a0d3a38a
JimmyShi22 <417711026@qq.com>
2018-10-24 10:39:37 +0800

    add unitest of sync status

--- test/unittests/libsync/SyncStatusTest.cpp
+++ test/unittests/libsync/SyncStatusTest.cpp
@@ -117,7 +117,6 @@ BOOST_AUTO_TEST_CASE(ForeachPeerRandomTest)
 
     set<NodeID> peers;
     shared_ptr<SyncPeerStatus> peerStatus;
-    cout << "foreachPeerRandom: " << endl;
     status.foreachPeerRandom([&](shared_ptr<SyncPeerStatus> _p) {
         peers.insert(_p->nodeId);
         cout << _p->nodeId << endl;
@@ -129,6 +128,59 @@ BOOST_AUTO_TEST_CASE(ForeachPeerRandomTest)
         return true;
     });
     BOOST_CHECK_EQUAL(peers.size(), 10);
+}
+
+BOOST_AUTO_TEST_CASE(RandomSelectionTest)
+{
+    for (unsigned id = 0; id < 10; id++)
+    {
+        SyncPeerInfo node{NodeID(id), id, h256(id), h256(id)};
+        status.newSyncPeerStatus(node);
+    }
+
+    NodeIDs peers;
+    shared_ptr<SyncPeerStatus> peerStatus;
+    peers = status.randomSelection(70, [&](shared_ptr<SyncPeerStatus> _p) {
+        peerStatus = status.peerStatus(_p->nodeId);
+        BOOST_CHECK_EQUAL(peerStatus->nodeId, _p->nodeId);
+        BOOST_CHECK_EQUAL(peerStatus->number, _p->number);
+        BOOST_CHECK_EQUAL(peerStatus->genesisHash, _p->genesisHash);
+        BOOST_CHECK_EQUAL(peerStatus->latestHash, _p->latestHash);
+        return true;
+    });
+    BOOST_CHECK_EQUAL(peers.size(), 7);
+
+    set<NodeID> peersSet;
+    for (auto peer : peers)
+        peersSet.insert(peer);
+    BOOST_CHECK_EQUAL(peersSet.size(), 7);
+}
+
+
+BOOST_AUTO_TEST_CASE(RandomSelectionSizeTest)
+{
+    for (unsigned id = 0; id < 10; id++)
+    {
+        SyncPeerInfo node{NodeID(id), id, h256(id), h256(id)};
+        status.newSyncPeerStatus(node);
+    }
+
+    NodeIDs peers;
+    shared_ptr<SyncPeerStatus> peerStatus;
+    peers = status.randomSelectionSize(3, [&](shared_ptr<SyncPeerStatus> _p) {
+        peerStatus = status.peerStatus(_p->nodeId);
+        BOOST_CHECK_EQUAL(peerStatus->nodeId, _p->nodeId);
+        BOOST_CHECK_EQUAL(peerStatus->number, _p->number);
+        BOOST_CHECK_EQUAL(peerStatus->genesisHash, _p->genesisHash);
+        BOOST_CHECK_EQUAL(peerStatus->latestHash, _p->latestHash);
+        return true;
+    });
+    BOOST_CHECK_EQUAL(peers.size(), 3);
+
+    set<NodeID> peersSet;
+    for (auto peer : peers)
+        peersSet.insert(peer);
+    BOOST_CHECK_EQUAL(peersSet.size(), 3);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
