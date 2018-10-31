cf12f8f47358ace9362efcc97929e5708cba3d01
JimmyShi22 <417711026@qq.com>
2018-10-31 22:04:46 +0800

    Resolving conflicts

--- libsync/SyncInterface.h
+++ libsync/SyncInterface.h
@@ -60,7 +60,6 @@ public:
     /// protocol id used when register handler to p2p module
     virtual PROTOCOL_ID const& protocolId() const = 0;
     virtual void setProtocolId(PROTOCOL_ID const _protocolId) = 0;
-    virtual void noteSealingBlockNumber(int64_t _number){};
 };
 
 }  // namespace sync
