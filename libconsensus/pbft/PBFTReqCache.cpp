a0309f104f3282c9e9c0496f1acd360c7cb26539
ancelmo <ancelmo@webank.com>
2018-11-14 13:42:08 +0800

    test

--- libconsensus/pbft/PBFTReqCache.cpp
+++ libconsensus/pbft/PBFTReqCache.cpp
@@ -101,7 +101,8 @@ bool PBFTReqCache::canTriggerViewChange(u256& minView, u256 const& maxInvalidNod
                     viewChangeEntry.second.height >= highestBlock.number())
                 {
                     /// update to lower view
-                    idx_view_map[viewChangeEntry.first] = viewChangeItem.first;
+                    idx_view_map[viewChangeEntry.first] = u256(viewChangeItem.first);
+
                     if (minView > viewChangeItem.first)
                         minView = viewChangeItem.first;
                     /// update to lower height
