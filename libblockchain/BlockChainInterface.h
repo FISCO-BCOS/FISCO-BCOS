c84eca166a5d763c892fe422dfec8b9e52b1a635
Chenxi Li <lichenxi.webank@gmail.com>
2018-11-15 21:38:47 +0800

    modify totalTransactionCount to return total transaction count and block number simultaneously

--- libblockchain/BlockChainInterface.h
+++ libblockchain/BlockChainInterface.h
@@ -57,7 +57,7 @@ public:
     virtual std::shared_ptr<dev::eth::Block> getBlockByNumber(int64_t _i) = 0;
     virtual CommitResult commitBlock(
         dev::eth::Block& block, std::shared_ptr<dev::blockverifier::ExecutiveContext>) = 0;
-    virtual int64_t totalTransactionCount() = 0;
+    virtual std::pair<int64_t, int64_t> totalTransactionCount() = 0;
     virtual dev::bytes getCode(dev::Address _address) = 0;
 
 
