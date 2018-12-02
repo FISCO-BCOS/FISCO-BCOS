7f0125fb64c85a0bb05974060c9de1b59d3a0f8c
bxq2011hust <bxq2011hust@qq.com>
2018-12-02 12:36:05 +0800

    Modify scripts to enable make coverage

--- cmake/EthCompilerSettings.cmake
+++ cmake/EthCompilerSettings.cmake
@@ -120,9 +120,10 @@ if (TESTS)
     find_program(LCOV_TOOL lcov)
     # message(STATUS "lcov tool: ${LCOV_TOOL}")
     if (LCOV_TOOL)
-        add_custom_target(coverage.info
+        add_custom_target(coverage
             COMMAND ${LCOV_TOOL} -o ${CMAKE_BINARY_DIR}/coverage.info -c -d ${CMAKE_BINARY_DIR}
             COMMAND ${LCOV_TOOL} -o ${CMAKE_BINARY_DIR}/coverage.info -r ${CMAKE_BINARY_DIR}/coverage.info '/usr*' '${CMAKE_BINARY_DIR}/deps/*' '${CMAKE_SOURCE_DIR}/deps/*'
+            COMMAND genhtml -q -o ${CMAKE_BINARY_DIR}/CodeCoverage ${CMAKE_BINARY_DIR}/coverage.info)
     )
     endif()
 endif ()
