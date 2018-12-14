3d2cfa227e8a53334d463cdde5be3a7fd93cbc2d
liangyongfu <liangyongpu@163.com>
2018-12-14 11:51:01 +0800

    change to english

--- libethereum/Defaults.cpp
+++ libethereum/Defaults.cpp
@@ -31,6 +31,6 @@ Defaults* Defaults::s_this = nullptr;
 Defaults::Defaults()
 {
 	m_dbPath = getDataDir();
-	m_stateDbCacheSize = 268435456;  //Ä¬ÈÏ»º´æ´óÐ¡256M
+	m_stateDbCacheSize = 268435456;  //default  256M
 	m_blockDbCacheSize = 268435456;
 }
