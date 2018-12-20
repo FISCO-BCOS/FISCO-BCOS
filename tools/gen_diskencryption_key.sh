c2d42fa71bdbc485f3ddbf674deb07efd9cd37e8
JimmyShi22 <417711026@qq.com>
2018-12-20 13:18:46 +0800

    update gen_diskencryption_key.sh for more stable

--- tools/gen_diskencryption_key.sh
+++ tools/gen_diskencryption_key.sh
@@ -2,7 +2,7 @@
 set -e
 [ ! -n "$2" ] && echo "Usage: sh $0 <URL 127.0.0.1:31443> <datakey>" && exit;
 
-cypherDataKey=`curl -X POST --data '{"jsonrpc":"2.0","method":"encDataKey","params":["'$2'"],"id":83}' $1 |jq .result.dataKey  |sed 's/\"//g'`
+cypherDataKey=$(curl -X POST --data '{"jsonrpc":"2.0","method":"encDataKey","params":["'$2'"],"id":83}' $1 |jq .result.dataKey  |sed 's/\"//g')
 [ -z "$cypherDataKey" ] && echo "Generate failed." && exit;
 echo "CiherDataKey generated: $cypherDataKey
 Append these into config.ini to enable disk encryption:
