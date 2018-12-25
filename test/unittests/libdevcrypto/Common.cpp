2e507d86b2a7225bb641c1354d00b96351f27e73
haoxuan40404 <444649358@qq.com>
2018-12-25 21:13:36 +0800

    add ecrecover

--- test/unittests/libdevcrypto/Common.cpp
+++ test/unittests/libdevcrypto/Common.cpp
@@ -430,17 +430,7 @@ BOOST_AUTO_TEST_CASE(testSigAndVerify)
 BOOST_AUTO_TEST_CASE(testSigecRocer)
 {
     std::pair<bool, bytes> KeyPair;
-#if FISCO_GM
-    bytes rlpBytes = fromHex(
-        "f901309f65f0d06e39dc3c08e32ac10a5070858962bc6c0f5760baca823f2d5582d14485174876e7ff8609"
-        "184e729fff8204a294d6f1a71052366dbae2f7ab2d5d5845e77965cf0d80b86448f85bce00000000000000"
-        "0000000000000000000000000000000000000000000000001bf5bd8a9e7ba8b936ea704292ff4aaa5797bf"
-        "671fdc8526dcd159f23c1f5a05f44e9fa862834dc7cb4541558f2b4961dc39eaaf0af7f7395028658d0e01"
-        "b86a37b840c7ca78e7ab80ee4be6d3936ba8e899d8fe12c12114502956ebe8c8629d36d88481dec9973574"
-        "2ea523c88cf3becba1cc4375bc9e225143fe1e8e43abc8a7c493a0ba3ce8383b7c91528bede9cf890b4b1e"
-        "9b99c1d8e56d6f8292c827470a606827a0ed511490a1666791b2bd7fc4f499eb5ff18fb97ba68ff9aee206"
-        "8fd63b88e817");
-#else
+    std::pair<bool, bytes> KeyPairR;
     bytes rlpBytes = fromHex(
         "f8ef9f65f0d06e39dc3c08e32ac10a5070858962bc6c0f5760baca823f2d5582d03f85174876e7ff"
         "8609184e729fff82020394d6f1a71052366dbae2f7ab2d5d5845e77965cf0d80b86448f85bce000000"
@@ -448,15 +438,25 @@ BOOST_AUTO_TEST_CASE(testSigecRocer)
         "ff4aaa5797bf671fdc8526dcd159f23c1f5a05f44e9fa862834dc7cb4541558f2b4961dc39eaaf0af7"
         "f7395028658d0e01b86a371ca00b2b3fabd8598fefdda4efdb54f626367fc68e1735a8047f0f1c4f84"
         "0255ca1ea0512500bc29f4cfe18ee1c88683006d73e56c934100b8abf4d2334560e1d2f75e");
-#endif
-    h256 ret;
+
+    bytes rlpBytesRight = fromHex(
+        "38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e"
+        "000000000000000000000000000000000000000000000000000000000000001b"
+        "38d18acb67d25c8bb9942764b62f18e17054f66a817bd4295423adf9ed98873e"
+        "789d1dd423d25f0772d2748d60f7e4b81bb14d086eba8e8e8efb6dcff8a4ae02");
+
+    h256 ret("000000000000000000000000ceaccac640adf55b2028469bd36ba501f28b699d");
     RLP rlpObj(rlpBytes);
     bytesConstRef _in = rlpObj.data();
     KeyPair = SignatureStruct::ecRecover(_in);
+    KeyPairR = SignatureStruct::ecRecover(ref(rlpBytesRight));
     BOOST_CHECK(KeyPair.first == true);
     BOOST_CHECK(KeyPair.second != ret.asBytes());
+    BOOST_CHECK(KeyPairR.second == ret.asBytes());
 }
 #endif
+
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }  // namespace test
