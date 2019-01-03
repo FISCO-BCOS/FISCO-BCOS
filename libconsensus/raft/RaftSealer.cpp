c74ab82b69ae05b6f2606ce7d384ac282e2ffe88
Chenxi Li <lichenxi.webank@gmail.com>
2019-01-03 20:26:59 +0800

    update log format of Raft Sealer

--- libconsensus/raft/RaftSealer.cpp
+++ libconsensus/raft/RaftSealer.cpp
@@ -55,10 +55,10 @@ void RaftSealer::handleBlock()
     resetSealingHeader(m_sealing.block.header());
     m_sealing.block.calTransactionRoot();
 
-    RAFTSEALER_LOG(INFO) << "+++++++++++++++++++++++++++ [#Generating seal on]:  "
-                         << "[blockNumber/txNum/hash]:  " << m_sealing.block.header().number()
-                         << "/" << m_sealing.block.getTransactionSize() << "/"
-                         << m_sealing.block.header().hash() << std::endl;
+    RAFTSEALER_LOG(INFO) << LOG_DESC("[#handleBlock]Generating seal")
+                         << LOG_KV("blockNumber", m_sealing.block.header().number())
+                         << LOG_KV("txNum", m_sealing.block.getTransactionSize())
+                         << LOG_KV("hash", m_sealing.block.header().hash());
     bool succ = m_raftEngine->commit(m_sealing.block);
     if (!succ)
     {
