f84c136c18d00fd953b273d956011e7f49494ea6
bxq2011hust <bxq2011hust@qq.com>
2019-01-04 08:51:39 +0800

    format code

--- libinitializer/P2PInitializer.cpp
+++ libinitializer/P2PInitializer.cpp
@@ -44,8 +44,9 @@ void P2PInitializer::initConfig(boost::property_tree::ptree const& _pt)
         {
             if (it.first.find("node.") == 0)
             {
-                INITIALIZER_LOG(TRACE) << "[#P2PInitializer::initConfig] add staticNode: " << it.first
-                                   << "/" << it.second.data();
+                INITIALIZER_LOG(TRACE)
+                    << "[#P2PInitializer::initConfig] add staticNode: " << it.first << "/"
+                    << it.second.data();
 
                 std::vector<std::string> s;
                 try
