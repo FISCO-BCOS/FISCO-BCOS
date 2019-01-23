902d429bd0a87bb7208952ed46b90e6c09a84dab
JimmyShi22 <417711026@qq.com>
2019-01-23 16:45:55 +0800

    rollback status sending logic: Send status to peers when the node in not in group

--- libsync/SyncMaster.h
+++ libsync/SyncMaster.h
@@ -174,7 +174,6 @@ private:
     bool m_newTransactions = false;
     bool m_newBlocks = false;
     uint64_t m_maintainBlocksTimeout = 0;
-    bool m_needMaintainBlocks = false;
     bool m_needMaintainTransactions = false;
 
 
