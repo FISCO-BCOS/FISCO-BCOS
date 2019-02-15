290ce61962b4e3f1b208953020edc3fd07096c12
Chenxi Li <lichenxi.webank@gmail.com>
2019-02-15 21:43:03 +0800

    fix code format

--- cmake/ProjectTBB.cmake
+++ cmake/ProjectTBB.cmake
@@ -13,7 +13,7 @@ ExternalProject_Add(tbb
     CONFIGURE_COMMAND ""
     BUILD_COMMAND make extra_inc=big_iron.inc
     INSTALL_COMMAND bash -c "cp ./build/*_release/*.${TBB_LIB_SUFFIX}* ${CMAKE_SOURCE_DIR}/deps/lib"
-                            " && export LD_LIBRARY_PATH=${CMAKE_SOURCE_DIR}/deps/lib:$LD_LIBRARY_PATH"
+                            #" && export LD_LIBRARY_PATH=${CMAKE_SOURCE_DIR}/deps/lib:$LD_LIBRARY_PATH"
 )
 
 ExternalProject_Get_Property(tbb SOURCE_DIR)
@@ -21,7 +21,7 @@ add_library(TBB STATIC IMPORTED)
 set(TBB_INCLUDE_DIR ${SOURCE_DIR}/include)
 set(TBB_LIBRARY ${CMAKE_SOURCE_DIR}/deps/lib/libtbb.${TBB_LIB_SUFFIX})
 file(MAKE_DIRECTORY ${TBB_INCLUDE_DIR})  # Must exist.
-file(MAKE_DIRECTORY ${CMAKE_SOURCE_DIR}/deps/lib/})  # Must exist.
+file(MAKE_DIRECTORY ${CMAKE_SOURCE_DIR}/deps/lib/)  # Must exist.
 
 set_property(TARGET TBB PROPERTY IMPORTED_LOCATION ${TBB_LIBRARY})
 set_property(TARGET TBB PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${TBB_INCLUDE_DIR})
