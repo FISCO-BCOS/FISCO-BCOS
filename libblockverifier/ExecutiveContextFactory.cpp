a7bd6d53ebd0e1a32119d677f5fa93483fb470cb
octopuswang <octopuswang@webank.com>
2019-02-22 19:23:55 +0800

    compile errof fix.

--- libblockverifier/ExecutiveContextFactory.cpp
+++ libblockverifier/ExecutiveContextFactory.cpp
@@ -19,12 +19,12 @@
  *  @date 20180921
  */
 #include "ExecutiveContextFactory.h"
+#include <extension/HelloWorldPrecompiled.h>
 #include <libdevcore/Common.h>
 #include <libprecompiled/AuthorityPrecompiled.h>
 #include <libprecompiled/CNSPrecompiled.h>
 #include <libprecompiled/CRUDPrecompiled.h>
 #include <libprecompiled/ConsensusPrecompiled.h>
-#include <libprecompiled/HelloWorldPrecompiled.h>
 #include <libprecompiled/SystemConfigPrecompiled.h>
 #include <libstorage/MemoryTableFactory.h>
 #include <libstorage/TableFactoryPrecompiled.h>
