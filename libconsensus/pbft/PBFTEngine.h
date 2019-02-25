5cc2b6f5f844508ed1cc59cfae9fa5645ce1c2d9
cyjseagull <1547158861@qq.com>
2019-02-25 10:06:58 +0800

    fix updateViewMap

--- libconsensus/pbft/PBFTEngine.h
+++ libconsensus/pbft/PBFTEngine.h
@@ -493,7 +493,7 @@ protected:
     void updateViewMap(IDXTYPE const& idx, VIEWTYPE const& view)
     {
         WriteGuard l(x_viewMap);
-        m_viewMap[idx] = m_view;
+        m_viewMap[idx] = view;
     }
 
 protected:
