eb8c7c11e18f6fbabcb0579d0166babe7c642215
JimmyShi22 <417711026@qq.com>
2019-03-22 10:54:00 +0800

    add commit db json encode backup

--- libstorage/LevelDBStorage.cpp
+++ libstorage/LevelDBStorage.cpp
@@ -154,6 +154,118 @@ size_t LevelDBStorage::commitTableDataRange(std::shared_ptr<dev::db::LevelDBWrit
     return total;
 }
 
+/*
+Entries::Ptr LevelDBStorage::select(h256, int, const std::string& table, const std::string& key)
+{
+    try
+    {
+        std::string entryKey = table;
+        entryKey.append("_").append(key);
+
+        std::string value;
+        // ReadGuard l(m_remoteDBMutex);
+        auto s = m_db->Get(leveldb::ReadOptions(), leveldb::Slice(entryKey), &value);
+        // l.unlock();
+        if (!s.ok() && !s.IsNotFound())
+        {
+            STORAGE_LEVELDB_LOG(ERROR)
+                << LOG_DESC("Query leveldb failed") << LOG_KV("status", s.ToString());
+
+            BOOST_THROW_EXCEPTION(StorageException(-1, "Query leveldb exception:" + s.ToString()));
+        }
+
+        Entries::Ptr entries = std::make_shared<Entries>();
+        if (!s.IsNotFound())
+        {
+            // parse json
+            std::stringstream ssIn;
+            ssIn << value;
+
+            Json::Value valueJson;
+            ssIn >> valueJson;
+
+            Json::Value values = valueJson["values"];
+            for (auto it = values.begin(); it != values.end(); ++it)
+            {
+                Entry::Ptr entry = std::make_shared<Entry>();
+
+                for (auto valueIt = it->begin(); valueIt != it->end(); ++valueIt)
+                {
+                    entry->setField(valueIt.key().asString(), valueIt->asString());
+                }
+
+                if (entry->getStatus() == Entry::Status::NORMAL)
+                {
+                    entry->setDirty(false);
+                    entries->addEntry(entry);
+                }
+            }
+        }
+
+        return entries;
+    }
+    catch (std::exception& e)
+    {
+        STORAGE_LEVELDB_LOG(ERROR) << LOG_DESC("Query leveldb exception")
+                                   << LOG_KV("msg", boost::diagnostic_information(e));
+
+        BOOST_THROW_EXCEPTION(e);
+    }
+
+    return Entries::Ptr();
+}
+
+size_t LevelDBStorage::commitTableDataRange(std::shared_ptr<dev::db::LevelDBWriteBatch>& batch,
+    TableData::Ptr tableData, h256 hash, int64_t num, size_t from, size_t to)
+{
+    // commit table data of given range, thread safe
+    // range to commit: [from, to)
+    if (from >= to)
+        return 0;
+    size_t total = 0;
+    auto dataIt = tableData->data.begin();
+    std::advance(dataIt, from);
+
+    while (from < to && dataIt != tableData->data.end())
+    {
+        if (dataIt->second->size() == 0u)
+        {
+            dataIt++;
+            from++;
+            continue;
+        }
+        std::string entryKey = tableData->tableName + "_" + dataIt->first;
+        Json::Value entry;
+
+        for (size_t i = 0; i < dataIt->second->size(); ++i)
+        {
+            Json::Value value;
+            for (auto& fieldIt : *(dataIt->second->get(i)->fields()))
+            {
+                value[fieldIt.first] = fieldIt.second;
+            }
+            value["_hash_"] = hash.hex();
+            value["_num_"] = num;
+            entry["values"].append(value);
+        }
+
+        std::stringstream ssOut;
+        ssOut << entry;
+
+        batch->insertSlice(leveldb::Slice(entryKey), leveldb::Slice(ssOut.str()));
+        ++total;
+        // ssOut.seekg(0, std::ios::end);
+        STORAGE_LEVELDB_LOG(TRACE)
+            << LOG_KV("commit key", entryKey) << LOG_KV("entries", dataIt->second->size());
+        //<< LOG_KV("len", ssOut.tellg());
+        dataIt++;
+        from++;
+    }
+
+    return total;
+}
+*/
+
 static const size_t c_commitTableDataRangeEachThread = 128;  // 128 is good after testing
 size_t LevelDBStorage::commit(
     h256 hash, int64_t num, const std::vector<TableData::Ptr>& datas, h256 const&)
