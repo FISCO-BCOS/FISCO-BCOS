ffed03c444edd73688f78d84a6d6bdafa7f1a3a2
fqliao <fqliao@yeah.net>
2019-03-29 01:34:20 +0800

    add CRUD UT

--- test/unittests/libstorage/test_CRUDPrecompiled.cpp
+++ test/unittests/libstorage/test_CRUDPrecompiled.cpp
@@ -23,6 +23,7 @@
 #include <libethcore/ABI.h>
 #include <libprecompiled/CRUDPrecompiled.h>
 #include <libstorage/MemoryTable.h>
+#include <libstorage/TableFactoryPrecompiled.h>
 #include <boost/test/unit_test.hpp>
 
 using namespace dev;
@@ -33,38 +34,112 @@ using namespace dev::precompiled;
 
 namespace test_CRUDPrecompiled
 {
+class MockMemoryTableFactory : public dev::storage::MemoryTableFactory
+{
+public:
+    virtual ~MockMemoryTableFactory(){};
+};
 struct CRUDPrecompiledFixture
 {
     CRUDPrecompiledFixture()
     {
+        context = std::make_shared<ExecutiveContext>();
         blockInfo.hash = h256(0);
         blockInfo.number = 0;
-        context = std::make_shared<ExecutiveContext>();
-        ExecutiveContextFactory factory;
+        context->setBlockInfo(blockInfo);
+
         auto storage = std::make_shared<MemoryStorage>();
         auto storageStateFactory = std::make_shared<StorageStateFactory>(h256(0));
+        ExecutiveContextFactory factory;
         factory.setStateStorage(storage);
         factory.setStateFactory(storageStateFactory);
         factory.initExecutiveContext(blockInfo, h256(0), context);
-        crudPrecompiled = std::make_shared<CRUDPrecompiled>();
         memoryTableFactory = context->getMemoryTableFactory();
+
+        tableFactoryPrecompiled = std::make_shared<dev::blockverifier::TableFactoryPrecompiled>();
+        tableFactoryPrecompiled->setMemoryTableFactory(memoryTableFactory);
+        crudPrecompiled = context->getPrecompiled(Address(0x1002));
     }
 
     ~CRUDPrecompiledFixture() {}
 
     ExecutiveContext::Ptr context;
     MemoryTableFactory::Ptr memoryTableFactory;
-    CRUDPrecompiled::Ptr crudPrecompiled;
+    Precompiled::Ptr crudPrecompiled;
+    dev::blockverifier::TableFactoryPrecompiled::Ptr tableFactoryPrecompiled;
     BlockInfo blockInfo;
 };
 
 BOOST_FIXTURE_TEST_SUITE(CRUDPrecompiled, CRUDPrecompiledFixture)
 
-BOOST_AUTO_TEST_CASE(call_add)
+BOOST_AUTO_TEST_CASE(CRUD)
 {
-    eth::ContractABI abi;
-    bytes in = abi.abiIn("select(string,string)", SYS_CONSENSUS, "sealer");
-    bytes out = crudPrecompiled->call(context, bytesConstRef(&in));
+    // createTable
+    dev::eth::ContractABI abi;
+    std::string tableName = "t_test", key = "name", valueField = "item_id,item_name";
+    bytes param = abi.abiIn("createTable(string,string,string)", tableName, key, valueField);
+    bytes out = tableFactoryPrecompiled->call(context, bytesConstRef(&param));
+    u256 createResult = 0;
+    abi.abiOut(&out, createResult);
+    BOOST_TEST(createResult == 1u);
+
+    // insert
+    std::string insertFunc = "insert(string,string,string,string)";
+    std::string entryStr = "{\"item_id\":\"1\",\"name\":\"fruit\",\"item_name\":\"apple\"}";
+    param.clear();
+    out.clear();
+    param = abi.abiIn(insertFunc, tableName, key, entryStr, "");
+    out = crudPrecompiled->call(context, bytesConstRef(&param));
+    u256 insertResult = 0;
+    abi.abiOut(&out, insertResult);
+    BOOST_TEST(insertResult == 1u);
+
+    // select
+    std::string selectFunc = "select(string,string,string,string)";
+    std::string conditonStr = "{\"item_id\":{\"eq\":\"1\"},\"lit\":{\"lit\":\"0,1\"}}";
+    param.clear();
+    out.clear();
+    param = abi.abiIn(selectFunc, tableName, key, conditonStr, "");
+    out = crudPrecompiled->call(context, bytesConstRef(&param));
+    std::string selectResult;
+    abi.abiOut(&out, selectResult);
+    Json::Value entryJson;
+    Json::Reader reader;
+    reader.parse(selectResult, entryJson);
+    BOOST_TEST(entryJson.size() == 1);
+
+    // update
+    std::string updateFunc = "update(string,string,string,string,string)";
+    entryStr = "{\"item_id\":\"1\",\"name\":\"fruit\",\"item_name\":\"orange\"}";
+    conditonStr = "{\"item_id\":{\"eq\":\"1\"}}";
+    param.clear();
+    out.clear();
+    param = abi.abiIn(updateFunc, tableName, key, entryStr, conditonStr, "");
+    out = crudPrecompiled->call(context, bytesConstRef(&param));
+    u256 updateResult = 0;
+    abi.abiOut(&out, updateResult);
+    BOOST_TEST(updateResult == 1u);
+
+    // remove
+    std::string removeFunc = "remove(string,string,string,string)";
+    conditonStr = "{\"item_id\":{\"eq\":\"1\"}}";
+    param.clear();
+    out.clear();
+    param = abi.abiIn(removeFunc, tableName, key, conditonStr, "");
+    out = crudPrecompiled->call(context, bytesConstRef(&param));
+    u256 removeResult = 0;
+    abi.abiOut(&out, removeResult);
+    BOOST_TEST(removeResult == 1u);
+
+    // errorFunc
+    std::string errorFunc = "errorFunc(string,string,string,string)";
+    param = abi.abiIn(errorFunc, tableName, key, conditonStr, "");
+    out = crudPrecompiled->call(context, bytesConstRef(&param));
+}
+
+BOOST_AUTO_TEST_CASE(toString)
+{
+    BOOST_TEST(crudPrecompiled->toString() == "CRUD");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
