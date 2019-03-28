2f3e698fddaf34763990d6a2c7934a049ed7cca3
JimmyShi22 <417711026@qq.com>
2019-03-28 11:12:23 +0800

    update get_tps script

--- tools/get_tps.sh
+++ tools/get_tps.sh
@@ -8,8 +8,10 @@ function Usage()
 
 
 log_file="${1}"
-if [ $# -eq 1 ]&&[ "${log_file}"="-h" -o "${log_file}"="--help" ];then
-   Usage
+if [ $# -eq 1 ];then
+    if [ "${log_file}"="-h" ]||[ "${log_file}"="--help" ];then
+       Usage
+    fi
 fi
 if [ $# -lt 3 ];then
     Usage
@@ -18,23 +20,23 @@ fi
 start_time="${2}"
 end_time="${3}"
 #txList=$(cat "${log_file}" | grep Report | awk -F',' '{print $5}' | cut -d'=' -f2 )
-timeList=$(cat "${log_file}" | grep Report | grep PBFT | awk -F' ' '{print $2}' | cut -d ' ' -f2 | cut -d'|' -f1 | sort | uniq)
+timeList=$(grep Report "${log_file}" | grep PBFT | awk -F' ' '{print $2}' | cut -d ' ' -f2 | cut -d'|' -f1 | sort | uniq)
 txTotal=0
 statistic_end=${end_time}
 statistic_start=${start_time}
 started="0"
 for time in ${timeList}
 do
-   if [ "${time}" \> "${start_time}" -a "${time}" \< "$end_time" ];then
+   if [ "${time}" \> "${start_time}" ]&&[ "${time}" \< "$end_time" ];then
 	#echo "====time:${time}"
         if [ "${started}" == "0" ];then
            started="1"
            statistic_start="${time}"  
         else
-   	txList=$(cat "${log_file}" | grep Report |grep PBFT| grep ${time} | awk -F',' '{print $6}' | cut -d'=' -f2 )
+   	txList=$(grep Report "${log_file}" | grep PBFT| grep ${time} | awk -F',' '{print $6}' | cut -d'=' -f2 )
    	for tx in ${txList}
    	do
-  	    txTotal=$((${txTotal}+${tx}))
+  	    txTotal=$((txTotal+tx))
    	done
         statistic_end=${time}
         fi
@@ -55,7 +57,7 @@ end_s=$(echo ${statistic_end} | awk -F':' '{print $3}'| cut -d'.' -f1)
 end_ms=$(echo ${statistic_end} | awk -F':' '{print $3}'| cut -d'.' -f2)
 end_t=$(echo "$end_hour*3600000+ $end_min*60000 + $end_s*1000 + $end_ms/1000" | bc)
 
-delta_time=$(($end_t-$start_t))
+delta_time=$((end_t-start_t))
 
 tps=$(echo "${txTotal}*1000/$delta_time" | bc)
 
