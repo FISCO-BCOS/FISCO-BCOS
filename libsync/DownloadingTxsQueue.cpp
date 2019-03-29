95ae19636a18c1cf23e67d74153177c257d70659
JimmyShi22 <417711026@qq.com>
2019-03-29 13:05:18 +0800

    fix tx receive logic when tx comes from RC1

--- libsync/DownloadingTxsQueue.cpp
+++ libsync/DownloadingTxsQueue.cpp
@@ -63,18 +63,19 @@ void DownloadingTxsQueue::pop2TxPool(
         // TODO drop by Txs Shard
 
         NodeID fromPeer = txsShard.fromPeer;
-        RLP const& txsBytesRLP = RLP(ref(txsShard.txsBytes))[0];
         auto constructRLP_time_cost = utcTime() - record_time;
         record_time = utcTime();
 
         if (g_BCOSConfig.version() >= RC2_VERSION)
         {
+            RLP const& txsBytesRLP = RLP(ref(txsShard.txsBytes))[0];
             // std::cout << "decode sync txs " << toHex(txsShard.txsBytes) << std::endl;
             dev::eth::TxsParallelParser::decode(
                 txs, txsBytesRLP.toBytesConstRef(), _checkSig, true);
         }
         else
         {
+            RLP const& txsBytesRLP = RLP(ref(txsShard.txsBytes));
             unsigned txNum = txsBytesRLP.itemCount();
             txs.resize(txNum);
             for (unsigned j = 0; j < txNum; j++)
