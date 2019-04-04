d1da69e1f42c9e6c122c4e004d8ea5d2482227ee
bxq2011hust <bxq2011hust@qq.com>
2019-04-04 17:02:45 +0800

    fix MemoryTableFactory:createTable

--- libstorage/MemoryTableFactory.cpp
+++ libstorage/MemoryTableFactory.cpp
@@ -142,11 +142,6 @@ Table::Ptr MemoryTableFactory::createTable(const std::string& tableName,
     Address const& _origin, bool isPara)
 {
     RecursiveGuard l(x_name2Table);
-    auto table = openTable(tableName, authorityFlag, isPara);
-    if (table)
-    {
-        return table;
-    }
 
     auto sysTable = openTable(SYS_TABLES, authorityFlag);
     // To make sure the table exists
