6a80bd4d1c5736040b2e7f35f5917c85fe953566
狐狸宝宝_ <352600801@qq.com>
2019-04-08 15:31:20 +0800

    update libevm

--- libevm/VMFactory.h
+++ libevm/VMFactory.h
@@ -26,39 +26,33 @@
 
 #include <boost/program_options/options_description.hpp>
 
-namespace dev
-{
-namespace eth
-{
-enum class VMKind
-{
-    Interpreter,
-    JIT,
-    Hera,
-    DLL
-};
+namespace dev {
+namespace eth {
+enum class VMKind { Interpreter, JIT, Hera, DLL, Force };
 
 /// Returns the EVM-C options parsed from command line.
-std::vector<std::pair<std::string, std::string>>& evmcOptions() noexcept;
+std::vector<std::pair<std::string, std::string>> &evmcOptions() noexcept;
 
 /// Provide a set of program options related to VMs.
 ///
-/// @param _lineLength  The line length for description text wrapping, the same as in
+/// @param _lineLength  The line length for description text wrapping, the same
+/// as in
 ///                     boost::program_options::options_description::options_description().
 boost::program_options::options_description vmProgramOptions(
-    unsigned _lineLength = boost::program_options::options_description::m_default_line_length);
+    unsigned _lineLength =
+        boost::program_options::options_description::m_default_line_length);
 
-class VMFactory
-{
+class VMFactory {
 public:
-    VMFactory() = delete;
-    ~VMFactory() = delete;
+  VMFactory() = delete;
+  ~VMFactory() = delete;
 
-    /// Creates a VM instance of the global kind (controlled by the --vm command line option).
-    static std::unique_ptr<VMFace> create();
+  /// Creates a VM instance of the global kind (controlled by the --vm command
+  /// line option).
+  static std::unique_ptr<VMFace> create();
 
-    /// Creates a VM instance of the kind provided.
-    static std::unique_ptr<VMFace> create(VMKind _kind);
+  /// Creates a VM instance of the kind provided.
+  static std::unique_ptr<VMFace> create(VMKind _kind);
 };
-}  // namespace eth
-}  // namespace dev
+} // namespace eth
+} // namespace dev
