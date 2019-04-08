9352c88d8ad64b4ee2e13b02661590b3d0366a7b
octopus <ywy2090@gmail.com>
2019-04-08 17:58:51 +0800

    update start.sh stop.sh/

--- tools/scripts/start.sh
+++ tools/scripts/start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-setsid fisco-bcos   --genesis `pwd`/genesis.json --config `pwd`/config.json > fisco-bcos.log 2>&1 &
+
+dirpath="$(cd "$(dirname "$0")" && pwd)"
+cd $dirpath
+node=$(basename ${dirpath})
+ulimit -c unlimited 2>/dev/null
+pid=`ps aux|grep "${dirpath}/config.json"|grep "fisco-bcos"|grep -v grep|awk '{print $2}'`
+if [ ! -z $pid ];then
+    echo " ${node} is running, pid is $pid."
+else 
+    echo " start ${node} ..."
+    setsid fisco-bcos  --genesis ${dirpath}/genesis.json  --config ${dirpath}/config.json  >> fisco-bcos.log 2>&1 &
+fi
