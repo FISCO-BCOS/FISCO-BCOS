cca038b42ac79914bd4aac292b13adbf3f06e90c
dynamicAna <fuyinger95@gmail.com>
2019-04-10 14:20:43 +0800

    update

--- libevm/EVMC.cpp
+++ libevm/EVMC.cpp
@@ -25,103 +25,104 @@
 
 #include <libdevcore/easylog.h>
 
-namespace dev {
-namespace eth {
+namespace dev
+{
+namespace eth
+{
 /// Error info for EVMC status code.
-using errinfo_evmcStatusCode =
-    boost::error_info<struct tag_evmcStatusCode, evmc_status_code>;
+using errinfo_evmcStatusCode = boost::error_info<struct tag_evmcStatusCode, evmc_status_code>;
 
-EVM::EVM(evmc_instance *_instance) noexcept : m_instance(_instance) {
-  assert(m_instance != nullptr);
-  assert(m_instance->abi_version == EVMC_ABI_VERSION);
+EVM::EVM(evmc_instance* _instance) noexcept : m_instance(_instance)
+{
+    assert(m_instance != nullptr);
+    assert(m_instance->abi_version == EVMC_ABI_VERSION);
 
-  // Set the options.
-  if (m_instance->set_option)
-    for (auto &pair : evmcOptions())
-      m_instance->set_option(m_instance, pair.first.c_str(),
-                             pair.second.c_str());
+    // Set the options.
+    if (m_instance->set_option)
+        for (auto& pair : evmcOptions())
+            m_instance->set_option(m_instance, pair.first.c_str(), pair.second.c_str());
 }
 
-owning_bytes_ref EVMC::exec(u256 &io_gas, ExtVMFace &_ext,
-                            const OnOpFunc &_onOp) {
-  assert(_ext.envInfo().number() >= 0);
+owning_bytes_ref EVMC::exec(u256& io_gas, ExtVMFace& _ext, const OnOpFunc& _onOp)
+{
+    assert(_ext.envInfo().number() >= 0);
 
-  constexpr int64_t int64max = std::numeric_limits<int64_t>::max();
+    constexpr int64_t int64max = std::numeric_limits<int64_t>::max();
 
-  // TODO: The following checks should be removed by changing the types
-  //       used for gas, block number and timestamp.
-  (void)int64max;
-  assert(io_gas <= int64max);
-  assert(_ext.envInfo().gasLimit() <= int64max);
-  assert(_ext.depth() <=
-         static_cast<size_t>(std::numeric_limits<int32_t>::max()));
+    // TODO: The following checks should be removed by changing the types
+    //       used for gas, block number and timestamp.
+    (void)int64max;
+    assert(io_gas <= int64max);
+    assert(_ext.envInfo().gasLimit() <= int64max);
+    assert(_ext.depth() <= static_cast<size_t>(std::numeric_limits<int32_t>::max()));
 
-  auto gas = static_cast<int64_t>(io_gas);
-  EVM::Result r = execute(_ext, gas);
+    auto gas = static_cast<int64_t>(io_gas);
+    EVM::Result r = execute(_ext, gas);
 
-  switch (r.status()) {
-  case EVMC_SUCCESS:
-    io_gas = r.gasLeft();
-    // FIXME: Copy the output for now, but copyless version possible.
-    return {r.output().toVector(), 0, r.output().size()};
+    switch (r.status())
+    {
+    case EVMC_SUCCESS:
+        io_gas = r.gasLeft();
+        // FIXME: Copy the output for now, but copyless version possible.
+        return {r.output().toVector(), 0, r.output().size()};
 
-  case EVMC_REVERT:
-    io_gas = r.gasLeft();
-    // FIXME: Copy the output for now, but copyless version possible.
-    throw RevertInstruction{{r.output().toVector(), 0, r.output().size()}};
+    case EVMC_REVERT:
+        io_gas = r.gasLeft();
+        // FIXME: Copy the output for now, but copyless version possible.
+        throw RevertInstruction{{r.output().toVector(), 0, r.output().size()}};
 
-  case EVMC_OUT_OF_GAS:
-  case EVMC_FAILURE:
-    BOOST_THROW_EXCEPTION(OutOfGas());
+    case EVMC_OUT_OF_GAS:
+    case EVMC_FAILURE:
+        BOOST_THROW_EXCEPTION(OutOfGas());
 
-  case EVMC_INVALID_INSTRUCTION: // NOTE: this could have its own exception
-  case EVMC_UNDEFINED_INSTRUCTION:
-    BOOST_THROW_EXCEPTION(BadInstruction());
+    case EVMC_INVALID_INSTRUCTION:  // NOTE: this could have its own exception
+    case EVMC_UNDEFINED_INSTRUCTION:
+        BOOST_THROW_EXCEPTION(BadInstruction());
 
-  case EVMC_BAD_JUMP_DESTINATION:
-    BOOST_THROW_EXCEPTION(BadJumpDestination());
+    case EVMC_BAD_JUMP_DESTINATION:
+        BOOST_THROW_EXCEPTION(BadJumpDestination());
 
-  case EVMC_STACK_OVERFLOW:
-    BOOST_THROW_EXCEPTION(OutOfStack());
+    case EVMC_STACK_OVERFLOW:
+        BOOST_THROW_EXCEPTION(OutOfStack());
 
-  case EVMC_STACK_UNDERFLOW:
-    BOOST_THROW_EXCEPTION(StackUnderflow());
+    case EVMC_STACK_UNDERFLOW:
+        BOOST_THROW_EXCEPTION(StackUnderflow());
 
-  case EVMC_INVALID_MEMORY_ACCESS:
-    BOOST_THROW_EXCEPTION(BufferOverrun());
+    case EVMC_INVALID_MEMORY_ACCESS:
+        BOOST_THROW_EXCEPTION(BufferOverrun());
 
-  case EVMC_STATIC_MODE_VIOLATION:
-    BOOST_THROW_EXCEPTION(DisallowedStateChange());
+    case EVMC_STATIC_MODE_VIOLATION:
+        BOOST_THROW_EXCEPTION(DisallowedStateChange());
 
-  case EVMC_REJECTED:
-    LOG(WARNING) << "Execution rejected by EVMC, executing with default VM "
-                    "implementation";
-    return VMFactory::create(VMKind::Interpreter)->exec(io_gas, _ext, _onOp);
+    case EVMC_REJECTED:
+        LOG(WARNING) << "Execution rejected by EVMC, executing with default VM "
+                        "implementation";
+        return VMFactory::create(VMKind::Interpreter)->exec(io_gas, _ext, _onOp);
 
-  case EVMC_INTERNAL_ERROR:
-  default:
-    if (r.status() <= EVMC_INTERNAL_ERROR)
-      BOOST_THROW_EXCEPTION(InternalVMError{}
-                            << errinfo_evmcStatusCode(r.status()));
-    else
-      // These cases aren't really internal errors, just more specific
-      // error codes returned by the VM. Map all of them to OOG.
-      BOOST_THROW_EXCEPTION(OutOfGas());
-  }
+    case EVMC_INTERNAL_ERROR:
+    default:
+        if (r.status() <= EVMC_INTERNAL_ERROR)
+            BOOST_THROW_EXCEPTION(InternalVMError{} << errinfo_evmcStatusCode(r.status()));
+        else
+            // These cases aren't really internal errors, just more specific
+            // error codes returned by the VM. Map all of them to OOG.
+            BOOST_THROW_EXCEPTION(OutOfGas());
+    }
 }
 
-evmc_revision toRevision(EVMSchedule const &_schedule) {
-  if (_schedule.haveCreate2)
-    return EVMC_CONSTANTINOPLE;
-  if (_schedule.haveRevert)
-    return EVMC_BYZANTIUM;
-  if (_schedule.eip158Mode)
-    return EVMC_SPURIOUS_DRAGON;
-  if (_schedule.eip150Mode)
-    return EVMC_TANGERINE_WHISTLE;
-  if (_schedule.haveDelegateCall)
-    return EVMC_HOMESTEAD;
-  return EVMC_FRONTIER;
+evmc_revision toRevision(EVMSchedule const& _schedule)
+{
+    if (_schedule.haveCreate2)
+        return EVMC_CONSTANTINOPLE;
+    if (_schedule.haveRevert)
+        return EVMC_BYZANTIUM;
+    if (_schedule.eip158Mode)
+        return EVMC_SPURIOUS_DRAGON;
+    if (_schedule.eip150Mode)
+        return EVMC_TANGERINE_WHISTLE;
+    if (_schedule.haveDelegateCall)
+        return EVMC_HOMESTEAD;
+    return EVMC_FRONTIER;
 }
-} // namespace eth
-} // namespace dev
+}  // namespace eth
+}  // namespace dev
