d5b0695975e019ff179cc3fad234922fd09af6f3
JimmyShi22 <417711026@qq.com>
2019-04-12 18:02:54 +0800

    fix compile problem

--- libexecutive/Executive.cpp
+++ libexecutive/Executive.cpp
@@ -66,7 +66,7 @@ void Executive::initialize(Transaction const& _transaction)
 }
 
 void Executive::verifyTransaction(
-    ImportRequirements::value _ir, Transaction const& _t, BlockHeader const&, u256 const&) const
+    ImportRequirements::value _ir, Transaction const& _t, BlockHeader const&, u256 const&)
 {
     eth::EVMSchedule const& schedule = DefaultSchedule;
 
