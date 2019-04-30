b7fd5e1fd57a9c1de811d0811079f82552b1b19a
bxq2011hust <bxq2011hust@qq.com>
2019-04-30 15:16:13 +0800

    fix rocksDb ilegal instruction

--- cmake/ProjectRocksDB.cmake
+++ cmake/ProjectRocksDB.cmake
@@ -13,7 +13,7 @@ ExternalProject_Add(rocksdb
     URL https://codeload.github.com/facebook/rocksdb/tar.gz/v6.0.2
     URL_HASH SHA256=89e0832f1fb00ac240a9438d4bbdae37dd3e52f7c15c3f646dc26887da16f342
     # remove dynamic lib and gtest. NOTE: sed line number should update once RocksDB upgrade
-    PATCH_COMMAND ${SED_CMMAND} "464d" CMakeLists.txt COMMAND ${SED_CMMAND} "739,749d" CMakeLists.txt COMMAND ${SED_CMMAND} "805,813d" CMakeLists.txt
+    PATCH_COMMAND ${SED_CMMAND} "217d" CMakeLists.txt COMMAND ${SED_CMMAND} "463d" CMakeLists.txt COMMAND ${SED_CMMAND} "738,748d" CMakeLists.txt COMMAND ${SED_CMMAND} "804,812d" CMakeLists.txt
     BUILD_IN_SOURCE 0
     CMAKE_COMMAND ${CMAKE_COMMAND}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
