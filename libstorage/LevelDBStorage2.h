7320e5be4334fd8f6b2401ea6ea14987b9e63129
bxq2011hust <bxq2011hust@qq.com>
2019-05-08 14:06:38 +0800

    modify rocksdb to use boost serialization
    modify leveldb2 to use boost serialization

--- libstorage/LevelDBStorage2.h
+++ libstorage/LevelDBStorage2.h
@@ -49,8 +49,9 @@ public:
 
 private:
     void processEntries(h256 hash, int64_t num,
-        std::shared_ptr<std::map<std::string, Json::Value> > key2value, TableInfo::Ptr tableInfo,
-        Entries::Ptr entries);
+        std::shared_ptr<std::map<std::string, std::vector<std::map<std::string, std::string>>>>
+            key2value,
+        TableInfo::Ptr tableInfo, Entries::Ptr entries);
 
     std::shared_ptr<dev::db::BasicLevelDB> m_db;
     dev::SharedMutex m_remoteDBMutex;
