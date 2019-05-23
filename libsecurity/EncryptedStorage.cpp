dfb799653ec36ad65f4c22d7c5c69a9de37dda94
JimmyShi22 <417711026@qq.com>
2019-05-23 14:53:25 +0800

    Add encrypted storage

---
+++ libsecurity/EncryptedStorage.cpp
@@ -0,0 +1,116 @@
+/*
+ * @CopyRight:
+ * FISCO-BCOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FISCO-BCOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FISCO-BCOS.  If not, see <http://www.gnu.org/licenses/>
+ * (c) 2016-2018 fisco-dev contributors.
+ */
+/**
+ * @brief : Encrypt Storage
+ * @author: jimmyshi
+ * @date: 2019-05-22
+ */
+
+#include "EncryptedStorage.h"
+#include "Common.h"
+#include "KeyCenter.h"
+#include <libdevcrypto/AES.h>
+
+using namespace dev;
+using namespace dev::storage;
+using namespace std;
+
+std::string encryptValue(const bytes& dataKey, const string value)
+{
+    bytesConstRef valueRef{(const unsigned char*)value.c_str(), value.length()};
+    bytes enData = aesCBCEncrypt(valueRef, ref(dataKey));
+    return asString(enData);
+}
+
+std::string decryptValue(const bytes& dataKey, const std::string& value)
+{
+    bytes deData = aesCBCDecrypt(
+        bytesConstRef{(const unsigned char*)value.c_str(), value.length()}, ref(dataKey));
+    return asString(deData);
+}
+
+
+Entries::Ptr EncryptedStorage::select(
+    h256 hash, int num, TableInfo::Ptr tableInfo, const std::string& key, Condition::Ptr condition)
+{
+    Entries::Ptr encEntries = m_backend->select(hash, num, tableInfo, key, condition);
+    return decryptEntries(encEntries);
+}
+
+size_t EncryptedStorage::commit(h256 hash, int64_t num, const std::vector<TableData::Ptr>& datas)
+{
+    for (auto data : datas)
+    {
+        data->dirtyEntries = encryptEntries(data->dirtyEntries);
+        data->newEntries = encryptEntries(data->newEntries);
+    }
+    return m_backend->commit(hash, num, datas);
+}
+
+void EncryptedStorage::setBackend(Storage::Ptr backend)
+{
+    // Check backend storage
+    m_backend = backend;
+}
+
+Entries::Ptr EncryptedStorage::encryptEntries(Entries::Ptr inEntries)
+{
+    checkDataKey();
+
+    Entries::Ptr entries = make_shared<Entries>();
+    for (size_t i = 0; i < inEntries->size(); i++)  // XX need parallel
+    {
+        Entry::Ptr inEntry = inEntries->get(i);
+        Entry::Ptr entry = make_shared<Entry>();
+        for (auto const& inKV : *inEntry->fields())
+        {
+            string k = encryptValue(m_dataKey, inKV.first);
+            string v = encryptValue(m_dataKey, inKV.second);
+            entry->setField(k, v);
+        }
+        entries->addEntry(entry);
+    }
+    return entries;
+}
+
+Entries::Ptr EncryptedStorage::decryptEntries(Entries::Ptr inEntries)
+{
+    checkDataKey();
+
+    Entries::Ptr entries = make_shared<Entries>();
+    for (size_t i = 0; i < inEntries->size(); i++)  // XX need parallel
+    {
+        Entry::Ptr inEntry = inEntries->get(i);
+        Entry::Ptr entry = make_shared<Entry>();
+        for (auto const& inKV : *inEntry->fields())
+        {
+            string k = decryptValue(m_dataKey, inKV.first);
+            string v = decryptValue(m_dataKey, inKV.second);
+            entry->setField(k, v);
+        }
+        entries->addEntry(entry);
+    }
+    return entries;
+}
+
+void EncryptedStorage::checkDataKey()
+{
+    if (m_dataKey == bytes())
+    {
+        m_dataKey = g_keyCenter->getDataKey(g_BCOSConfig.diskEncryption.cipherDataKey);
+    }
+}
