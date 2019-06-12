8a1431cc72b879ba4030750a99690e23569c8856
JimmyShi22 <417711026@qq.com>
2019-06-12 16:28:33 +0800

    Add unitest

--- test/unittests/libexecutive/ExecuteVMTest.cpp
+++ test/unittests/libexecutive/ExecuteVMTest.cpp
@@ -330,6 +330,137 @@ BOOST_AUTO_TEST_CASE(CallAddressErrorTest)
     BOOST_CHECK_EQUAL(e.getException(), TransactionException::CallAddressError);
 }
 
+BOOST_AUTO_TEST_CASE(DeployGetSetContractTestRC1)
+{
+    g_BCOSConfig.setSupportedVersion("2.0.0-rc1", RC1_VERSION);
+    /*
+pragma solidity ^0.4.2;
+contract HelloWorld{
+    uint256 x;
+    function HelloWorld(){
+       x = 123;
+    }
+    function get()constant returns(uint256){
+        return x;
+    }
+    function set(uint256 n){
+        x = n;
+    }
+    function getByCall()constant returns(uint256){
+        return HelloWorld(this).get();
+    }
+    function destroy()
+    {
+        selfdestruct(0x0);
+    }
+}
+    */
+
+    // Deploy a contract
+    u256 value = 0;
+    u256 gasPrice = 0;
+    u256 gas = 100000000;
+    Address caller = Address("1000000000000000000000000000000000000000");
+    bytes code = fromHex(
+        string("608060405234801561001057600080fd5b50607b6000819055506101ea806100286000396000f300608"
+               "060405260043610610062576000357c0100000000000000000000000000000000000000000000000000"
+               "000000900463ffffffff16806360fe47b1146100675780636d4ce63c1461009457806383197ef014610"
+               "0bf578063b7379733146100d6575b600080fd5b34801561007357600080fd5b50610092600480360381"
+               "01908080359060200190929190505050610101565b005b3480156100a057600080fd5b506100a961010"
+               "b565b6040518082815260200191505060405180910390f35b3480156100cb57600080fd5b506100d461"
+               "0114565b005b3480156100e257600080fd5b506100eb610118565b60405180828152602001915050604"
+               "05180910390f35b8060008190555050565b60008054905090565b6000ff5b60003073ffffffffffffff"
+               "ffffffffffffffffffffffffff16636d4ce63c6040518163ffffffff167c01000000000000000000000"
+               "00000000000000000000000000000000000028152600401602060405180830381600087803b15801561"
+               "017e57600080fd5b505af1158015610192573d6000803e3d6000fd5b505050506040513d60208110156"
+               "101a857600080fd5b81019080805190602001909291905050509050905600a165627a7a723058207eb7"
+               "0dc4a752ca8296fc60a702b8fba26d66ed4ac38c8801fe7eff92664b671e0029") +
+        string(""));
+
+    Executive e0(m_mptStates, initEnvInfo());
+    Transaction tx(value, gasPrice, gas, code);  // Use contract creation constructor
+    tx.forceSender(caller);
+    executeTransaction(e0, tx);
+
+    Address newAddress = e0.newAddress();
+    cout << "Contract created at: " << newAddress << endl;
+
+    bytes createdCode = m_mptStates->code(newAddress);
+    cout << "Created code: " << toHex(createdCode) << endl;
+
+    bytes runtimeCode = fromHex(
+        "608060405260043610610062576000357c01000000000000000000000000000000000000000000000000000000"
+        "00900463ffffffff16806360fe47b1146100675780636d4ce63c1461009457806383197ef0146100bf578063b7"
+        "379733146100d6575b600080fd5b34801561007357600080fd5b50610092600480360381019080803590602001"
+        "90929190505050610101565b005b3480156100a057600080fd5b506100a961010b565b60405180828152602001"
+        "91505060405180910390f35b3480156100cb57600080fd5b506100d4610114565b005b3480156100e257600080"
+        "fd5b506100eb610118565b6040518082815260200191505060405180910390f35b8060008190555050565b6000"
+        "8054905090565b6000ff5b60003073ffffffffffffffffffffffffffffffffffffffff16636d4ce63c60405181"
+        "63ffffffff167c0100000000000000000000000000000000000000000000000000000000028152600401602060"
+        "405180830381600087803b15801561017e57600080fd5b505af1158015610192573d6000803e3d6000fd5b5050"
+        "50506040513d60208110156101a857600080fd5b81019080805190602001909291905050509050905600a16562"
+        "7a7a723058207eb70dc4a752ca8296fc60a702b8fba26d66ed4ac38c8801fe7eff92664b671e0029");
+
+    BOOST_CHECK(runtimeCode == createdCode);
+
+    // set()
+    bytes callDataToSet =
+        fromHex(string("0x60fe47b1") +  // set(0xaa)
+                string("00000000000000000000000000000000000000000000000000000000000000aa"));
+    Transaction setTx(value, gasPrice, gas, newAddress, callDataToSet);
+    setTx.forceSender(caller);
+
+    Executive e1(m_mptStates, initEnvInfo());
+    ExecutionResult setExeRes;
+    e1.setResultRecipient(setExeRes);
+    executeTransaction(e1, setTx);
+
+    // get()
+    bytes callDataToGet = fromHex(string("6d4ce63c") +  // get()
+                                  string(""));
+
+    Transaction getTx(value, gasPrice, gas, newAddress, callDataToGet);
+    getTx.forceSender(caller);
+
+    Executive e2(m_mptStates, initEnvInfo());
+    ExecutionResult getExeRes;
+    e2.setResultRecipient(getExeRes);
+    executeTransaction(e2, getTx);
+
+    bytes compareName = fromHex("00000000000000000000000000000000000000000000000000000000000000aa");
+    cout << "get() result: " << toHex(getExeRes.output) << endl;
+    BOOST_CHECK(getExeRes.output == compareName);
+
+    // getByCall()
+    bytes callDataToGetByCall = fromHex(string("b7379733") +  // getByCall()
+                                        string(""));
+
+    Transaction getByCallTx(value, gasPrice, gas, newAddress, callDataToGetByCall);
+    getByCallTx.forceSender(caller);
+
+    Executive e3(m_mptStates, initEnvInfo());
+    ExecutionResult getExeResByCall;
+    e3.setResultRecipient(getExeResByCall);
+    executeTransaction(e3, getByCallTx);
+
+    cout << "getByCall() result: " << toHex(getExeResByCall.output) << endl;
+    BOOST_CHECK(getExeResByCall.output == compareName);
+
+    // destroy
+    bytes callDestroy = fromHex(string("83197ef0") +  // destroy()
+                                string(""));
+
+    Transaction destroyTx(value, gasPrice, gas, newAddress, callDestroy);
+    destroyTx.forceSender(caller);
+
+    Executive e4(m_mptStates, initEnvInfo());
+    ExecutionResult destroyExeRes;
+    e4.setResultRecipient(destroyExeRes);
+    executeTransaction(e4, destroyTx);
+    BOOST_CHECK(!m_mptStates->addressHasCode(newAddress));
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
 }  // namespace test
 }  // namespace dev
