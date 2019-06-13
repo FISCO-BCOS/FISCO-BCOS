dc4d5c0f201f8d4ba38129ec784c8bcce28f5e81
JimmyShi22 <417711026@qq.com>
2019-06-13 13:05:31 +0800

    Add more sync ci test

--- tools/ci/ci_check.sh
+++ tools/ci/ci_check.sh
@@ -18,14 +18,14 @@ cd nodes/127.0.0.1
 bash start_all.sh
 sleep 1
 
-LOG_INFO "==============send a transaction"
+LOG_INFO "[round1]==============send a transaction"
 if [ $(bash .transTest.sh | grep "result" | wc -l) -ne 1 ];then
     LOG_ERROR "send transaction failed!"
     exit 1
 fi
-LOG_INFO "==============send a transaction is ok"
+LOG_INFO "[round1]==============send a transaction is ok"
 
-LOG_INFO "==============check report block"
+LOG_INFO "[round1]==============check report block"
 sleep 2
 num=$(cat node*/log/* | grep Report | wc -l)
 if [ ${num} -ne 8 ];then
@@ -33,20 +33,55 @@ if [ ${num} -ne 8 ];then
     cat node*/log/* 
     exit 1
 fi
-LOG_INFO "==============check report block is ok"
+LOG_INFO "[round1]==============check report block is ok"
 
-LOG_INFO "==============check sync block"
+LOG_INFO "[round1]==============check sync block"
 bash stop_all.sh
 rm -rf node0/data node0/log
 bash start_all.sh
 sleep 5
 num=$(cat node*/log/* | grep Report | wc -l)
 if [ ${num} -ne 11 ];then
-    LOG_ERROR "sync block failed! ${num} != 11"
+    LOG_ERROR "[round1] sync block failed! ${num} != 11"
     cat node*/log/* 
     exit 1
 fi
-LOG_INFO "==============check sync block is ok"
+LOG_INFO "[round1]==============check sync block is ok"
+
+LOG_INFO "[round2]==============restart all node"
+bash stop_all.sh
+bash start_all.sh
+
+LOG_INFO "[round2]==============send a transaction"
+if [ $(bash .transTest.sh | grep "result" | wc -l) -ne 1 ];then
+    LOG_ERROR "[round1] send transaction failed!"
+    exit 1
+fi
+LOG_INFO "[round2]==============send a transaction is ok"
+
+LOG_INFO "[round2]==============check report block"
+sleep 2
+num=$(cat node*/log/* | grep Report | wc -l)
+if [ ${num} -ne 8 ];then
+    LOG_ERROR "[round2] check report block failed! ${num} != 8"
+    cat node*/log/* 
+    exit 1
+fi
+LOG_INFO "[round2]==============check report block is ok"
+
+LOG_INFO "[round2]==============check sync block"
+bash stop_all.sh
+rm -rf node0/data node0/log
+bash start_all.sh
+sleep 5
+num=$(cat node*/log/* | grep Report | wc -l)
+if [ ${num} -ne 11 ];then
+    LOG_ERROR "[round2] sync block failed! ${num} != 11"
+    cat node*/log/* 
+    exit 1
+fi
+LOG_INFO "[round2]==============check sync block is ok"
+
 cd ../..
 python ci/ci_check.py
 
