f466d7dca6c09efaf173089e6aaab35af6ce02de
chaychen2005 <605966652@qq.com>
2019-08-16 16:41:53 +0800

    encode implementation

--- libstorage/BinLogHandler.cpp
+++ libstorage/BinLogHandler.cpp
@@ -75,7 +75,9 @@ bool BinLogHandler::writeBlocktoBinLog(int64_t num, const std::vector<TableData:
                 << LOG_DESC("open binary file successful!") << LOG_KV("fileName", buffer.size())
                 << LOG_KV("writtenBytesLength", m_writtenBytesLength);
             m_writtenBytesLength = 0;
-            // TODO:write version
+            // write version
+            uint32_t version = htonl(BINLOG_VERSION);
+            m_outBinaryFile.write((char*)&version, sizeof(uint32_t));
         }
         else
         {
@@ -99,4 +101,122 @@ bool BinLogHandler::getMissingBlocksFromBinLog(
     return true;
 }
 
-void BinLogHandler::encodeBlock(int64_t, const std::vector<TableData::Ptr>&, bytes&) {}
+void BinLogHandler::writeUINT32(bytes& buffer, uint32_t ui)
+{
+    uint32_t ui_tmp = htonl(ui);
+    buffer.insert(buffer.end(), (byte*)&ui_tmp, (byte*)&ui_tmp + 4);
+}
+
+void BinLogHandler::writeUINT64(bytes& buffer, uint64_t ui)
+{
+    uint64_t ui_tmp = HTONLL(ui);
+    buffer.insert(buffer.end(), (byte*)&ui_tmp, (byte*)&ui_tmp + 8);
+}
+
+void BinLogHandler::writeString(bytes& buffer, const std::string& str)
+{
+    writeUINT32(buffer, str.length());
+    buffer.insert(buffer.end(), str.begin(), str.end());
+}
+
+uint32_t BinLogHandler::readUINT32(const bytes& buffer, uint32_t& offset)
+{
+    uint32_t ui = ntohl(*((uint32_t*)&buffer[offset]));
+    offset += 4;
+    return ui;
+}
+
+uint64_t BinLogHandler::readUINT64(const bytes& buffer, uint32_t& offset)
+{
+    uint64_t ui = NTOHLL(*((uint64_t*)&buffer[offset]));
+    offset += 8;
+    return ui;
+}
+
+void BinLogHandler::readString(const bytes& buffer, std::string& str, uint32_t& offset)
+{
+    uint32_t strLen = readUINT32(buffer, offset);
+    std::string s((char*)&buffer[offset], strLen);
+    str = s;
+    offset += strLen;
+}
+
+void BinLogHandler::encodeEntries(
+    const std::vector<std::string>& vecField, Entries::Ptr entries, bytes& buffer)
+{
+    writeUINT32(buffer, entries->size());
+    for (size_t j = 0; j < entries->size(); j++)
+    {
+        auto entry = entries->get(j);
+        writeUINT64(buffer, entry->getID());
+        uint8_t status = entry->getStatus();
+        BINLOG_HANDLER_LOG(TRACE) << "entry id:" << entry->getID()
+                                  << ", status:" << (uint32_t)status;
+        buffer.insert(buffer.end(), (byte*)&status, (byte*)&status + 1);
+        for (size_t k = 0; k < vecField.size(); k++)
+        {
+            std::string value = "";
+            auto fieldIt = entry->find(vecField[k]);
+            if (fieldIt != entry->end())
+            {
+                value = fieldIt->second;
+            }
+            BINLOG_HANDLER_LOG(TRACE) << "key:" << vecField[k] << ", value:" << value;
+            writeString(buffer, value);
+        }
+    }
+}
+
+void BinLogHandler::encodeTable(TableData::Ptr table, bytes& buffer)
+{
+    // table info: name and fields
+    TableInfo::Ptr info = table->info;
+    uint8_t tableNameLen = info->name.length();
+    buffer.insert(buffer.end(), (byte*)&tableNameLen, (byte*)&tableNameLen + 1);
+    buffer.insert(buffer.end(), info->name.begin(), info->name.end());
+    std::vector<std::string> vecField;  // record a list of column names used in encodeEntries
+    std::stringstream ss;               // used to output column names
+    ss << info->key;
+    vecField.push_back(info->key);
+    for (size_t j = 0; j < info->fields.size(); j++)
+    {
+        if (info->fields[j] != info->key)
+        {
+            ss << "," << info->fields[j];
+            vecField.push_back(info->fields[j]);
+        }
+    }
+    writeString(buffer, ss.str());
+    BINLOG_HANDLER_LOG(TRACE) << "fields(include key):" << ss.str();
+    // table data : dirty entries and new entries
+    encodeEntries(vecField, table->dirtyEntries, buffer);
+    encodeEntries(vecField, table->newEntries, buffer);
+}
+
+void BinLogHandler::encodeBlock(
+    int64_t num, const std::vector<TableData::Ptr>& datas, bytes& buffer)
+{
+    // block heigth
+    writeUINT64(buffer, num);
+
+    // block data
+    writeUINT32(buffer, datas.size());
+    for (size_t i = 0; i < datas.size(); i++)
+    {
+        encodeTable(datas[i], buffer);
+    }
+
+    // CRC32
+    boost::crc_32_type result;
+    result.process_block((char*)&buffer[0], (char*)&buffer[0] + buffer.size());
+    uint32_t crc32 = result.checksum();
+    BINLOG_HANDLER_LOG(DEBUG) << LOG_KV("CRC32", crc32);
+    writeUINT32(buffer, crc32);
+
+    // insert length
+    uint32_t length = htonl(buffer.size() + 4);
+    buffer.insert(buffer.begin(), (byte*)&length, (byte*)&length + 4);
+    BINLOG_HANDLER_LOG(INFO) << LOG_DESC("block binary data length include header and CRC32")
+                             << LOG_KV("num", num) << LOG_KV("size", buffer.size())
+                             << LOG_KV("CRC32", crc32);
+}
