a7e2c5b30a1fb55b7a65ab1a20a458fdc858f1d9
chaychen2005 <35914409+chaychen2005@users.noreply.github.com>
2019-08-19 14:50:52 +0800

    binlog encode implementation (#949)

--- libstorage/BinLogHandler.h
+++ libstorage/BinLogHandler.h
@@ -29,6 +29,17 @@ namespace dev
 {
 namespace storage
 {
+#ifndef HTONLL
+#define HTONLL(x)            \
+    ((1 == htonl(1)) ? (x) : \
+                       (((uint64_t)htonl((x)&0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
+#endif
+#ifndef NTOHLL
+#define NTOHLL(x)            \
+    ((1 == ntohl(1)) ? (x) : \
+                       (((uint64_t)ntohl((x)&0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
+#endif
+
 class BinLogHandler
 {
 public:
@@ -52,6 +63,26 @@ public:
         int64_t num, std::map<int64_t, std::vector<TableData::Ptr>> binLogData);
 
 private:
+    /// write data interface
+    /// @param buffer : [in/out] data buffer
+    /// @param ui/str : [in] data to write
+    void writeUINT32(bytes& buffer, uint32_t ui);
+    void writeUINT64(bytes& buffer, uint64_t ui);
+    void writeString(bytes& buffer, const std::string& str);
+    /// readUINT32 and readUINT64
+    /// @param buffer : [in] data buffer
+    /// @param offset : [in/out] data offset in buffer, will be increased after read
+    /// @return : data need to read
+    uint32_t readUINT32(const bytes& buffer, uint32_t& offset);
+    uint64_t readUINT64(const bytes& buffer, uint32_t& offset);
+    /// readString
+    /// @param buffer : [in] data buffer
+    /// @param str : [out] data need to read
+    /// @param offset : [in/out] data offset in buffer, will be increased after read
+    void readString(const bytes& buffer, std::string& str, uint32_t& offset);
+    void encodeEntries(
+        const std::vector<std::string>& vecField, Entries::Ptr entries, bytes& buffer);
+    void encodeTable(TableData::Ptr table, bytes& buffer);
     void encodeBlock(int64_t num, const std::vector<TableData::Ptr>& datas, bytes& buffer);
 
     uint32_t m_writtenBytesLength = 0;  // length already written
@@ -59,6 +90,7 @@ private:
     std::string m_path;                 // storage path of binlog
 
     const uint32_t BINLOG_FILE_MAX_SIZE = 256 * 1024 * 1024;  // the max size of binlog file
+    const uint32_t BINLOG_VERSION = 1;                        // binlog version
 };
 }  // namespace storage
 }  // namespace dev
