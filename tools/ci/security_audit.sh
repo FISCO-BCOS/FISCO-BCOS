0f9a81241fb350b9981ff7ffe181f6802b6f5125
bxq2011hust <bxq2011hust@qq.com>
2019-09-02 13:56:06 +0800

    fix cobra issue

--- tools/ci/security_audit.sh
+++ tools/ci/security_audit.sh
@@ -3,7 +3,7 @@
 set -e
 
 scan_code_script="cobra/cobra.py -f json -o /tmp/report.json -t "
-ignore_files=(tools/ EventLogFilterTest cmake)
+ignore_files=(tools/ EventLogFilterTest cmake LedgerTest.cpp FakeModule.h SyncMasterTest.cpp)
 
 LOG_ERROR() {
     content=${1}
