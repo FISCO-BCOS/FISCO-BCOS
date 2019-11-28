2075af7fd039a85bd4193e2ca30fbc3afb7ec659
bxq2011hust <bxq2011hust@qq.com>
2019-11-28 17:22:29 +0800

    add calculate_address tool to calculate publick key of hex format

---
+++ fisco-bcos/tools/calculate_address.cpp
@@ -0,0 +1,20 @@
+#include "libdevcore/CommonData.h"
+#include "libdevcore/FixedHash.h"
+#include "libdevcrypto/Hash.h"
+#include "libdevcrypto/gm/sm3/sm3.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace dev;
+
+int main(int, char** argv)
+{
+    auto source = dev::fromHex(argv[1]);
+    auto addr = right160(dev::sha3(bytesConstRef((const unsigned char*)source.data(), 64)));
+    cout << addr << endl;
+    return 0;
+}
