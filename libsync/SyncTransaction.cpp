035843e87ced645374d00a0991756f1436808828
cyjseagull <1547158861@qq.com>
2019-12-20 15:32:56 +0800

    fix coredump caused by notify_all when stop consensus (#1258)

--- libsync/SyncTransaction.cpp
+++ libsync/SyncTransaction.cpp
@@ -40,10 +40,6 @@ void SyncTransaction::start()
 
 void SyncTransaction::stop()
 {
-    // notify all when stop, in case of the process stucked in 'doWork' when the system-time has
-    // been updated
-    m_signalled.notify_all();
-
     doneWorking();
     stopWorking();
     // will not restart worker, so terminate it
