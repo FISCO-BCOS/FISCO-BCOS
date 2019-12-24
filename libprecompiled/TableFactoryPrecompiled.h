a0b07b8718e25d9159e8e761face128dbff1cfa7
qwdarrenyin <48435992+qwdarrenyin@users.noreply.github.com>
2019-12-24 22:51:35 +0800

    fix bug for TableFactoryPrecompiled (#1275)

--- libprecompiled/TableFactoryPrecompiled.h
+++ libprecompiled/TableFactoryPrecompiled.h
@@ -68,6 +68,9 @@ public:
 
 private:
     std::shared_ptr<dev::storage::TableFactory> m_memoryTableFactory;
+
+    void checkFieldNameValidate(const std::string& tableName, std::string& keyField,
+        std::vector<std::string>& valueFieldList);
 };
 
 }  // namespace blockverifier
