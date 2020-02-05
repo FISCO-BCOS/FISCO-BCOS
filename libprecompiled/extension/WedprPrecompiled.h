b6adef4a0c67adfccdaffa75a8c30199847431a2
fqliao <fqliao@yeah.net>
2020-02-05 18:05:32 +0800

    rename payment

--- libprecompiled/extension/WedprPrecompiled.h
+++ libprecompiled/extension/WedprPrecompiled.h
@@ -27,11 +27,11 @@ namespace dev
 {
 namespace precompiled
 {
-// hidden payment
-extern const char API_HIDDEN_PAYMENT_VERIFY_ISSUED_CREDIT[];
-extern const char API_HIDDEN_PAYMENT_VERIFY_FULFILLED_CREDIT[];
-extern const char API_HIDDEN_PAYMENT_VERIFY_TRANSFERRED_CREDIT[];
-extern const char API_HIDDEN_PAYMENT_VERIFY_SPLIT_CREDIT[];
+// confidential payment
+extern const char API_CONFIDENTIAL_PAYMENT_VERIFY_ISSUED_CREDIT[];
+extern const char API_CONFIDENTIAL_PAYMENT_VERIFY_FULFILLED_CREDIT[];
+extern const char API_CONFIDENTIAL_PAYMENT_VERIFY_TRANSFERRED_CREDIT[];
+extern const char API_CONFIDENTIAL_PAYMENT_VERIFY_SPLIT_CREDIT[];
 
 // anonymous voting
 extern const char API_ANONYMOUS_VOTING_BOUNDED_VERIFY_VOTE_REQUEST[];
