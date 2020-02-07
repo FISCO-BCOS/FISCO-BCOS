385d85d9a461a62056b78e3aab8795faa6898ec5
bxq2011hust <bxq2011hust@qq.com>
2020-02-07 19:14:44 +0800

    add SimpleTablePrecompiled

--- libprecompiled/solidity/Table.sol
+++ libprecompiled/solidity/Table.sol
@@ -2,71 +2,67 @@ pragma solidity ^0.4.24;
 
 contract TableFactory {
     function openTable(string) public constant returns (Table); //open table
-    function createTable(string,string,string) public returns(int); //create table
+    function createTable(string, string, string) public returns (int256); //create table
 }
 
 //select condition
 contract Condition {
-    function EQ(string, int) public;
+    function EQ(string, int256) public;
     function EQ(string, string) public;
-    
-    function NE(string, int) public;
-    function NE(string, string)  public;
 
-    function GT(string, int) public;
-    function GE(string, int) public;
-    
-    function LT(string, int) public;
-    function LE(string, int) public;
-    
-    function limit(int) public;
-    function limit(int, int) public;
+    function NE(string, int256) public;
+    function NE(string, string) public;
+
+    function GT(string, int256) public;
+    function GE(string, int256) public;
+
+    function LT(string, int256) public;
+    function LE(string, int256) public;
+
+    function limit(int256) public;
+    function limit(int256, int256) public;
 }
 
-//one record 
+//one record
 contract Entry {
-    function getInt(string) public constant returns(int);
-    function getUInt(string) public constant returns(int);
-    function getAddress(string) public constant returns(address);
-    function getBytes64(string) public constant returns(byte[64]);
-    function getBytes32(string) public constant returns(bytes32);
-    function getString(string) public constant returns(string);
-    
-    function set(string, int) public;
-    function set(string, uint) public;
+    function getInt(string) public constant returns (int256);
+    function getUInt(string) public constant returns (int256);
+    function getAddress(string) public constant returns (address);
+    function getBytes64(string) public constant returns (bytes1[64]);
+    function getBytes32(string) public constant returns (bytes32);
+    function getString(string) public constant returns (string);
+
+    function set(string, int256) public;
+    function set(string, uint256) public;
     function set(string, string) public;
     function set(string, address) public;
 }
 
 //record sets
 contract Entries {
-    function get(int) public constant returns(Entry);
-    function size() public constant returns(int);
+    function get(int256) public constant returns (Entry);
+    function size() public constant returns (int256);
 }
 
 //Table main contract
 contract Table {
-    //select api
-    function select(string, Condition) public constant returns(Entries);
-    //insert api
-    function insert(string, Entry) public returns(int);
-    //update api
-    function update(string, Entry, Condition) public returns(int);
-    //remove api
-    function remove(string, Condition) public returns(int);
-    
-    function newEntry() public constant returns(Entry);
-    function newCondition() public constant returns(Condition);
+    function select(string, Condition) public constant returns (Entries);
+    function insert(string, Entry) public returns (int256);
+    function update(string, Entry, Condition) public returns (int256);
+    function remove(string, Condition) public returns (int256);
+
+    function newEntry() public constant returns (Entry);
+    function newCondition() public constant returns (Condition);
 }
 
 contract SimpleTableFactory {
     function openTable(string) public constant returns (SimpleTable);
-    function createTable(string,string,string) public returns(bool,int);
+    function createTable(string, string, string) public returns (int256);
 }
 
 //SimpleTable per permiary key has only one Entry
 contract SimpleTable {
-    function get(string) public constant returns(bool, Entry);
-    function set(string, Entry) public;
-    function newEntry() public constant returns(Entry);
+    function get(string) public constant returns (bool, Entry);
+    function set(string, Entry) public returns (int256);
+    function newEntry() public constant returns (Entry);
 }
