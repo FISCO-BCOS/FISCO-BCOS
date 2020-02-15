da7d3e169e1659d70178422149b508702764ca10
bxq2011hust <bxq2011hust@qq.com>
2020-02-15 12:34:03 +0800

    modify to get secp256k1 from FISCO-BCOS

--- cmake/ProjectSecp256k1.cmake
+++ cmake/ProjectSecp256k1.cmake
@@ -27,7 +27,7 @@ ExternalProject_Add(secp256k1
     PREFIX ${CMAKE_SOURCE_DIR}/deps
     DOWNLOAD_NAME secp256k1-ac8ccf29.tar.gz
     DOWNLOAD_NO_PROGRESS 1
-    URL https://github.com/chfast/secp256k1/archive/ac8ccf29b8c6b2b793bc734661ce43d1f952977a.tar.gz
+    URL https://github.com/FISCO-BCOS/secp256k1/archive/ac8ccf29b8c6b2b793bc734661ce43d1f952977a.tar.gz
     URL_HASH SHA256=02f8f05c9e9d2badc91be8e229a07ad5e4984c1e77193d6b00e549df129e7c3a
     PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${CMAKE_CURRENT_LIST_DIR}/secp256k1/CMakeLists.txt <SOURCE_DIR>
@@ -39,7 +39,7 @@ ExternalProject_Add(secp256k1
                -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
                -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
     LOG_CONFIGURE 1
-    BUILD_COMMAND ""
+    # BUILD_COMMAND ""
     LOG_BUILD 1
     LOG_INSTALL 1
     BUILD_BYPRODUCTS ${CMAKE_SOURCE_DIR}/deps/lib/libsecp256k1.a
