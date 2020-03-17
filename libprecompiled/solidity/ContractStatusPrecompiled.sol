6ce20cdec905d51add8cc88574c56267895409e9
chaychen2005 <605966652@qq.com>
2020-03-17 22:35:38 +0800

    remove destroy function in contract status management

--- libprecompiled/solidity/ContractStatusPrecompiled.sol
+++ libprecompiled/solidity/ContractStatusPrecompiled.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.4.24;
 
 contract ContractStatusPrecompiled {
-    function destroy(address addr) public returns(int);
     function freeze(address addr) public returns(int);
     function unfreeze(address addr) public returns(int);
     function grantManager(address contractAddr, address userAddr) public returns(int);
-    function getStatus(address addr) public constant returns(uint,string);
-    function listManager(address addr) public constant returns(uint,address[]);
+    function getStatus(address addr) public constant returns(int,string);
+    function listManager(address addr) public constant returns(int,address[]);
 }
