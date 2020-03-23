e87c3022fdf33d82e47752eb733b9dd82b626f34
cyjseagull <yujiechen_hust@163.com>
2020-03-23 11:02:41 +0800

    modify boost log initializer to support log rotating of multiple sink (#1445)

--- fisco-bcos/main/main.cpp
+++ fisco-bcos/main/main.cpp
@@ -85,7 +85,6 @@ int main(int argc, const char* argv[])
     {
         checkAndCall(configPath, initialize);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        LogInitializer::logRotateByTime();
     }
     initialize.reset();
     std::cout << "[" << getCurrentDateTime() << "] ";
