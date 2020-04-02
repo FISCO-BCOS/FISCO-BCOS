815ef9c7fb12074be1398ecbba3c68e72ca78d47
Xingqang Bai <bxq2011hust@qq.com>
2020-04-02 23:34:17 +0800

    fix docker build hooks use wrong dockerfile (#1496)

--- tools/ci/ci_check.sh
+++ tools/ci/ci_check.sh
@@ -117,7 +117,7 @@ check_binarylog()
     fi
     ${sed_cmd} "s/binary_log=false/binary_log=true/" node0/conf/group.1.ini
     ${sed_cmd} "s/binary_log=false/binary_log=true/" node1/conf/group.1.ini
-    check_consensus_and_sync 5
+    check_consensus_and_sync 6
 }
 
 check_raft()
@@ -141,7 +141,7 @@ check_rpbft()
         sed_cmd="sed -i .bkp"
     fi
     ${sed_cmd} "s/consensus_type=raft/consensus_type=rpbft/" node*/conf/group.1.genesis
-    check_consensus_and_sync 5
+    check_consensus_and_sync 6
 }
 
 init
