e50764bedaaa285568d232e3efecb0cd2b8e8d5a
bxq2011hust <bxq2011hust@qq.com>
2020-05-20 19:56:36 +0800

    add ubuntu16 ci of GitHub Action

--- cmake/ProjectGroupSig.cmake
+++ cmake/ProjectGroupSig.cmake
@@ -3,10 +3,10 @@ include(GNUInstallDirs)
 
 ExternalProject_Add(GroupSigLib
     PREFIX ${CMAKE_SOURCE_DIR}/deps
-    DOWNLOAD_NAME group_sig_lib.tgz
+    DOWNLOAD_NAME group_sig_lib-6c5e7d47.tgz
     DOWNLOAD_NO_PROGRESS 1
-    GIT_REPOSITORY https://github.com/FISCO-BCOS/group-signature-lib.git
-    GIT_TAG 6c5e7d47cb4cb43b15da064e3793727dd1b59192
+    URL https://github.com/FISCO-BCOS/group-signature-lib/archive/6c5e7d47cb4cb43b15da064e3793727dd1b59192.tar.gz
+    URL_HASH SHA256=13b74d8027eb0b03b5f224b56793c833fd1a7404bccd31d94e600e397d4b239a
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON
     LOG_CONFIGURE 1
--- cmake/ProjectPaillier.cmake
+++ cmake/ProjectPaillier.cmake
@@ -1,9 +1,10 @@
 include(ExternalProject)
 ExternalProject_Add(paillier
     PREFIX ${CMAKE_SOURCE_DIR}/deps
+    DOWNLOAD_NAME paillier-1daf3b23.tar.gz
     DOWNLOAD_NO_PROGRESS 1
-    GIT_REPOSITORY https://github.com/FISCO-BCOS/paillier-lib.git
-	GIT_TAG 1daf3b23b01121e8522a8b264be933f6d236fdb8
+    URL https://github.com/FISCO-BCOS/paillier-lib/archive/1daf3b23b01121e8522a8b264be933f6d236fdb8.tar.gz
+    URL_HASH SHA256=574c8315961ea2ba9534a739675172a0e580ca140c9b2a6fb1008aaf608ae1c9
     BUILD_IN_SOURCE 1
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON
