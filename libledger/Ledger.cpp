27ba6ec59a1793bb46c80c5bcc76f9ad4667a09a
cyjseagull <yujiechen_hust@163.com>
2020-05-26 18:11:52 +0800

    add tx_pool.notify_worker_num to set the thread num used to notify transaction (#1590)

--- libledger/Ledger.cpp
+++ libledger/Ledger.cpp
@@ -191,7 +191,8 @@ bool Ledger::initTxPool()
         return false;
     }
     auto txPool = std::make_shared<dev::txpool::TxPool>(
-        m_service, m_blockChain, protocol_id, m_param->mutableTxPoolParam().txPoolLimit);
+        m_service, m_blockChain, protocol_id, m_param->mutableTxPoolParam().txPoolLimit,
+        m_param->mutableTxPoolParam().notifyWorkerNum);
     txPool->setMaxBlockLimit(g_BCOSConfig.c_blockLimit);
     txPool->setMaxMemoryLimit(m_param->mutableTxPoolParam().maxTxPoolMemorySize);
     m_txPool = txPool;
