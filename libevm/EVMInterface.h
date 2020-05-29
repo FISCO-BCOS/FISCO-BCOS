c39c9472ec2f1ef3d38950b27ed7aaa944b5f1f5
Xingqang Bai <bxq2011hust@qq.com>
2020-05-29 19:46:34 +0800

    move Instruction into libinterpreter and rename EVMC to EVMInstance (#1601)

--- libevm/EVMInterface.h
+++ libevm/EVMInterface.h
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
-/** @file VMFace.h
+/** @file EVMInterface.h
  * @author wheatli
  * @date 2018.8.28
  * @record copy from aleth, this is a vm interface
@@ -22,7 +22,7 @@
 
 #pragma once
 
-#include "ExtVMFace.h"
+#include "EVMHostInterface.h"
 #include <libethcore/Exceptions.h>
 #include <memory>
 
@@ -31,16 +31,16 @@ namespace dev
 namespace eth
 {
 /// EVM Virtual Machine interface
-class VMFace
+class EVMInterface
 {
 public:
-    VMFace() = default;
-    virtual ~VMFace() = default;
-    VMFace(VMFace const&) = delete;
-    VMFace& operator=(VMFace const&) = delete;
+    EVMInterface() = default;
+    virtual ~EVMInterface() = default;
+    EVMInterface(EVMInterface const&) = delete;
+    EVMInterface& operator=(EVMInterface const&) = delete;
 
     /// VM implementation
-    virtual owning_bytes_ref exec(u256& io_gas, ExtVMFace& _ext, OnOpFunc const& _onOp) = 0;
+    virtual owning_bytes_ref exec(u256& io_gas, EVMHostInterface& _ext, OnOpFunc const& _onOp) = 0;
 };
 }  // namespace eth
 }  // namespace dev
