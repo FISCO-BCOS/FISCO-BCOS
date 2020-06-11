c85a822876e7dbe52642b9930b01fa367404f6fe
cyjseagull <yujiechen_hust@163.com>
2020-06-11 14:39:19 +0800

    Complete the remaining logic of WorkingSealerManager (#1642)

--- libconsensus/pbft/PBFTSealer.cpp
+++ libconsensus/pbft/PBFTSealer.cpp
@@ -57,6 +57,7 @@ void PBFTSealer::handleBlock()
         return;
     }
     setBlock();
+    hookAfterHandleBlock();
     PBFTSEALER_LOG(INFO) << LOG_DESC("++++++++++++++++ Generating seal on")
                          << LOG_KV("blkNum", m_sealing.block->header().number())
                          << LOG_KV("tx", m_sealing.block->getTransactionSize())
