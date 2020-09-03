a2c2cd3f504a101fbc5e97833ea0f4443b68098e
Xingqang Bai <bxq2011hust@qq.com>
2020-09-03 17:51:40 +0800

    fix gen_node_cert failed on Linux (#1749)

--- tools/gen_node_cert.sh
+++ tools/gen_node_cert.sh
@@ -182,7 +182,7 @@ gen_node_cert_with_extensions_gm() {
     extensions="$5"
 
     $TASSL_CMD genpkey -paramfile $capath/gmsm2.param -out $certpath/gm${type}.key
-    $TASSL_CMD req -new -subj "/CN=$name/O=fiscobcos/OU=agency" -key "$certpath/gm${type}.key" -out "$certpath/gm${type}.csr"
+    $TASSL_CMD req -new -subj "/CN=$name/O=fiscobcos/OU=agency" -config "$capath/gmcert.cnf" -key "$certpath/gm${type}.key" -out "$certpath/gm${type}.csr"
     $TASSL_CMD x509 -req -CA $capath/gmagency.crt -CAkey $capath/gmagency.key -days 3650 -CAcreateserial -in $certpath/gm${type}.csr -out $certpath/gm${type}.crt -extfile $capath/gmcert.cnf -extensions $extensions
 
     rm -f $certpath/gm${type}.csr
