a4ece0979c4aa62923cb145c85d3d9477e4f53d5
cyjseagull <yujiechen_hust@163.com>
2020-12-11 12:38:07 +0800

    modify keccak256 to keccak256Hash in case of interface conflict (#1834)

--- libprecompiled/extension/CryptoPrecompiled.h
+++ libprecompiled/extension/CryptoPrecompiled.h
@@ -30,8 +30,8 @@ namespace precompiled
 contract Crypto
 {
     function sm3(bytes data) public view returns(bytes32);
-    function keccak256(bytes data) public view returns(bytes32);
-    function sm2Verify(bytes32 message, bytes publicKey, bytes32 r, bytes32 s) public view returns(bool, Address);
+    function keccak256Hash(bytes data) public view returns(bytes32);
+    function sm2Verify(bytes32 message, bytes publicKey, bytes32 r, bytes32 s) public view returns(bool, address);
     function curve25519VRFVerify(string input, string vrfPublicKey, string vrfProof) public view returns(bool,uint256);
 }
 #endif
