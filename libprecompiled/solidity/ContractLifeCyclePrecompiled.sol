cb8bbd4d2f1165c16f65fb8d0591a0d2c774dedc
cyjseagull <yujiechen_hust@163.com>
2021-04-02 09:53:44 +0800

    Supplement the interface of ContractLifeCyclePrecompiled.sol

--- libprecompiled/solidity/ContractLifeCyclePrecompiled.sol
+++ libprecompiled/solidity/ContractLifeCyclePrecompiled.sol
@@ -4,6 +4,7 @@ contract ContractLifeCyclePrecompiled {
     function freeze(address addr) public returns(int);
     function unfreeze(address addr) public returns(int);
     function grantManager(address contractAddr, address userAddr) public returns(int);
+    function revokeManager(address contractAddr, address userAddr) public returns(int);
     function getStatus(address addr) public constant returns(int,string);
     function listManager(address addr) public constant returns(int,address[]);
 }
