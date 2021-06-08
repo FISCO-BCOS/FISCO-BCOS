1f11224f9e040216a03bd5ad52c130128c7e582f
maggie <hao.maggie.wu@gmail.com>
2021-06-08 07:36:20 +0000

    add #ifdef FISCO_SDF

--- libdevcrypto/Common.h
+++ libdevcrypto/Common.h
@@ -104,7 +104,9 @@ public:
     }
 
     /// Set pub by cert file
+#ifdef FISCO_SDF
     void set_pub(const std::string& certfile);
+#endif
 
     bool operator==(KeyPair const& _c) const { return m_public == _c.m_public; }
     bool operator!=(KeyPair const& _c) const { return m_public != _c.m_public; }
