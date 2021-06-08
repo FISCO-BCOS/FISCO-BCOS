4bf6e8dde042831341c1b92fe1734ba865a9fe09
卡拉马佐夫 <973459080@qq.com>
2021-06-08 10:53:53 +0800

    tableName add _d

--- fisco-bcos/datasync/StashSQLBasicAccess.cpp
+++ fisco-bcos/datasync/StashSQLBasicAccess.cpp
@@ -33,7 +33,7 @@ using namespace std;
 StashSQLBasicAccess::StashSQLBasicAccess() {}
 
 int StashSQLBasicAccess::Select(int64_t, const string& _table, const string&, Condition::Ptr _condition,
-                           vector<map<string, string>>& _values)
+                                vector<map<string, string>>& _values)
 {
     string sql = this->BuildQuerySql(_table, _condition);
     Connection_T conn = m_connPool->GetConnection();
@@ -50,7 +50,7 @@ int StashSQLBasicAccess::Select(int64_t, const string& _table, const string&, Co
     if (conn == NULL)
     {
         StashSQLBasicAccess_LOG(ERROR) << "table:" << _table << "sql:" << sql
-                                  << " get connection failed";
+                                       << " get connection failed";
         return -1;
     }
     TRY
@@ -139,7 +139,7 @@ string StashSQLBasicAccess::BuildQuerySql(string _table, Condition::Ptr _conditi
 }
 
 string StashSQLBasicAccess::BuildConditionSql(const string& _strPrefix,
-                                         map<string, Condition::Range>::const_iterator& _it, Condition::Ptr _condition)
+                                              map<string, Condition::Range>::const_iterator& _it, Condition::Ptr _condition)
 {
     string strConditionSql = _strPrefix;
     if (_it->second.left.second == _it->second.right.second && _it->second.left.first &&
@@ -175,13 +175,39 @@ string StashSQLBasicAccess::BuildConditionSql(const string& _strPrefix,
     return strConditionSql;
 }
 
-int StashSQLBasicAccess::SelectTableDataByNum(int64_t num, TableInfo::Ptr tableInfo, uint64_t start,
-    uint32_t counts, vector<map<string, string>>& _values)
+bool endswith(const std::string& str, const std::string& end)
 {
+    int srclen = str.size();
+    int endlen = end.size();
+    if(srclen >= endlen)
+    {
+        string temp = str.substr(srclen - endlen, endlen);
+        if(temp == end)
+            return true;
+    }
+
+    return false;
+}
+
+int StashSQLBasicAccess::SelectTableDataByNum(int64_t num, TableInfo::Ptr tableInfo, uint64_t start,
+                                              uint32_t counts, vector<map<string, string>>& _values)
+{
+    string tableName = tableInfo->name;
+    if (dev::stringCmpIgnoreCase(tableName, "_sys_tables_"))
+    {
+        if (endswith(tableName, "_"))
+        {
+            tableName = tableName + "d_";
+        }
+        else
+        {
+            tableName = tableName + "_d_";
+        }
+    }
     string sql =
-        "select * from " + tableInfo->name +
+        "select * from " + tableName +
         " where (_id_, _num_) in ( select _id_, num from (select _id_, max(_num_) as num from  " +
-        tableInfo->name + " where _id_ > " + to_string(start) + " and _num_ <= " + to_string(num) +
+        tableName + " where _id_ > " + to_string(start) + " and _num_ <= " + to_string(num) +
         " group by _id_ order by _id_ limit " + to_string(counts) + ") as tmp)";
 
     Connection_T conn = m_connPool->GetConnection();
@@ -190,62 +216,62 @@ int StashSQLBasicAccess::SelectTableDataByNum(int64_t num, TableInfo::Ptr tableI
     while (conn == NULL && retryCnt++ < retryMax)
     {
         StashSQLBasicAccess_LOG(WARNING)
-            << "table:" << tableInfo->name << "sql:" << sql << " get connection failed";
+                << "table:" << tableInfo->name << "sql:" << sql << " get connection failed";
         sleep(1);
         conn = m_connPool->GetConnection();
     }
     if (conn == NULL)
     {
         StashSQLBasicAccess_LOG(ERROR)
-            << "table:" << tableInfo->name << "sql:" << sql << " get connection failed";
+                << "table:" << tableInfo->name << "sql:" << sql << " get connection failed";
         return -1;
     }
     TRY
-    {
-        PreparedStatement_T _prepareStatement =
-            Connection_prepareStatement(conn, "%s", sql.c_str());
-        ResultSet_T result = PreparedStatement_executeQuery(_prepareStatement);
-        int32_t columnCnt = ResultSet_getColumnCount(result);
-        bool tableWithBlobField = isBlobType(tableInfo->name);
-        while (ResultSet_next(result))
-        {
-            map<string, string> value;
-            for (int32_t index = 1; index <= columnCnt; ++index)
             {
-                auto fieldName = ResultSet_getColumnName(result, index);
-                if (tableWithBlobField)
+                PreparedStatement_T _prepareStatement =
+                    Connection_prepareStatement(conn, "%s", sql.c_str());
+                ResultSet_T result = PreparedStatement_executeQuery(_prepareStatement);
+                int32_t columnCnt = ResultSet_getColumnCount(result);
+                bool tableWithBlobField = isBlobType(tableInfo->name);
+                while (ResultSet_next(result))
                 {
-                    int size;
-                    auto bytes = ResultSet_getBlob(result, index, &size);
-                    // Note: Since the field may not be set, it must be added here to determine
-                    //       whether the value of the obtained field is nullptr
-                    if (bytes)
+                    map<string, string> value;
+                    for (int32_t index = 1; index <= columnCnt; ++index)
                     {
-                        value[fieldName] = string((char*)bytes, size);
+                        auto fieldName = ResultSet_getColumnName(result, index);
+                        if (tableWithBlobField)
+                        {
+                            int size;
+                            auto bytes = ResultSet_getBlob(result, index, &size);
+                            // Note: Since the field may not be set, it must be added here to determine
+                            //       whether the value of the obtained field is nullptr
+                            if (bytes)
+                            {
+                                value[fieldName] = string((char*)bytes, size);
+                            }
+                        }
+                        else
+                        {
+                            auto selectResult = ResultSet_getString(result, index);
+                            if (selectResult)
+                            {
+                                value[fieldName] = selectResult;
+                            }
+                        }
                     }
-                }
-                else
-                {
-                    auto selectResult = ResultSet_getString(result, index);
-                    if (selectResult)
-                    {
-                        value[fieldName] = selectResult;
-                    }
+                    _values.push_back(move(value));
                 }
             }
-            _values.push_back(move(value));
-        }
-    }
-    CATCH(SQLException)
-    {
-        m_connPool->ReturnConnection(conn);
-        // Note: when select exception caused by table doesn't exist and sql error,
-        //       here must return 0, in case of the DB is normal but the sql syntax error or the
-        //       table will be created later
-        StashSQLBasicAccess_LOG(ERROR)
-            << "select exception for sql error:" << Exception_frame.message;
-        return 0;
-    }
+        CATCH(SQLException)
+            {
+                m_connPool->ReturnConnection(conn);
+                // Note: when select exception caused by table doesn't exist and sql error,
+                //       here must return 0, in case of the DB is normal but the sql syntax error or the
+                //       table will be created later
+                StashSQLBasicAccess_LOG(ERROR)
+                        << "select exception for sql error:" << Exception_frame.message;
+                return 0;
+            }
     END_TRY;
     m_connPool->ReturnConnection(conn);
     return 0;
