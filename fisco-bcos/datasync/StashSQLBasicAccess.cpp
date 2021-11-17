7106df3eaee709a2aff263f76247ebfc9667652d
卡拉马佐夫 <973459080@qq.com>
2021-11-17 14:59:59 +0800

    sql update

--- fisco-bcos/datasync/StashSQLBasicAccess.cpp
+++ fisco-bcos/datasync/StashSQLBasicAccess.cpp
@@ -210,6 +210,7 @@ int StashSQLBasicAccess::SelectTableDataByNum(int64_t num, TableInfo::Ptr tableI
         +  " group by _id_ order by _id_ limit" + to_string(counts)
         + ") b on a._id_=b._id_ and a._num_=b.num";
 
+    std::cout << "SelectTableDataByNum sql:" << sql << std::endl;
 //        "select * from " + tableName +
 //        " where (_id_, _num_) in ( select _id_, num from (select _id_, max(_num_) as num from  " +
 //        tableName + " where _id_ > " + to_string(start) + " and _num_ <= " + to_string(num) +
