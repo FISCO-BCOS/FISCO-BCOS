1e27109eaef6c512b10db8ab8b17835d71c72967
MO NAN <651932351@qq.com>
2021-12-15 18:46:08 +0800

    Add clang-format (#2072)
    * Add clang-format
    
    * Add gateway changes
    
    * Merge rpc changes
    
    * Fix rpc compile error

--- bcos-rpc/bcos-rpc/amop/AMOPClient.cpp
+++ bcos-rpc/bcos-rpc/amop/AMOPClient.cpp
@@ -249,8 +249,8 @@ void AMOPClient::sendMessageToClient(std::string const& _topic,
     std::function<void(Error::Ptr&&, bytesPointer)> _callback)
 {
     _selectSession->asyncSendMessage(_msg, Options(30000),
-        [_msg, _topic, _callback](auto _error, std::shared_ptr<WsMessage> _responseMsg,
-            std::shared_ptr<WsSession> _session) {
+        [_msg, _topic, _callback](bcos::boostssl::utilities::Error::Ptr _error,
+            std::shared_ptr<WsMessage> _responseMsg, std::shared_ptr<WsSession> _session) {
             auto seq = std::string(_msg->seq()->begin(), _msg->seq()->end());
             if (_error && _error->errorCode() != bcos::protocol::CommonError::SUCCESS)
             {
@@ -273,16 +273,15 @@ void AMOPClient::sendMessageToClient(std::string const& _topic,
                 _responseMsg->encode(*buffer);
             }
 
-
-            if (!_error)
-            {
-                _callback(nullptr, buffer);
-            }
-            else
+            if (_error)
             {
                 _callback(
                     std::make_shared<bcos::Error>(_error->errorCode(), _error->errorMessage()),
-                    buffer);
+                    std::move(buffer));
+            }
+            else
+            {
+                _callback(nullptr, std::move(buffer));
             }
         });
 }
