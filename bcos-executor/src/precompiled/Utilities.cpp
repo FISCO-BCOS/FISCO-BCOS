1afa71085707a52d37974d32b2c5274907d1ae1e
kyonRay <rui.kwok@outlook.com>
2022-01-12 10:23:09 +0800

    <fix>(precompiled): fix regex in wchar_t.

--- bcos-executor/src/precompiled/Utilities.cpp
+++ bcos-executor/src/precompiled/Utilities.cpp
@@ -326,7 +326,8 @@ bool precompiled::checkPathValid(std::string const& _path)
                                << LOG_KV("path", _path);
         return false;
     }
-    std::regex reg(R"(^[0-9a-zA-Z\u4e00-\u9fa5][^\>\<\*\?\/\=\+\(\)\$\"\']+$)");
+    // TODO: adapt Chinese
+    std::regex reg(R"(^[0-9a-zA-Z][^\>\<\*\?\/\=\+\(\)\$\"\']+$)");
     auto checkFieldNameValidate = [&reg](const std::string& fieldName) -> bool {
         if (fieldName.empty())
         {
