2a2abd2a059d79b84f09d5aeb05d5a0c932df72d
cyjseagull <yujiechen_hust@163.com>
2022-03-30 00:39:38 +0800

    Squashed 'bcos-framework/' changes from e7473681f..eb6caecaa
    eb6caecaa update HandshakeRequest (#419)
    8adcf1ecb add HandshakeRequest to bcos-framework (#418)
    
    git-subtree-dir: bcos-framework
    git-subtree-split: eb6caecaa642a8df39f23752b997b4bc869ccd8f

--- interfaces/protocol/ProtocolInfo.h
+++ interfaces/protocol/ProtocolInfo.h
@@ -31,8 +31,7 @@ public:
     using Ptr = std::shared_ptr<ProtocolInfo>;
     using ConstPtr = std::shared_ptr<ProtocolInfo const>;
     ProtocolInfo() = default;
-    ProtocolInfo(
-        ProtocolModuleID _moduleID, ProtocolVersion _minVersion, ProtocolVersion _maxVersion)
+    ProtocolInfo(ProtocolModuleID _moduleID, uint32_t _minVersion, uint32_t _maxVersion)
       : m_protocolModuleID(_moduleID),
         m_minVersion(_minVersion),
         m_maxVersion(_maxVersion),
@@ -40,26 +39,27 @@ public:
     {}
     virtual ~ProtocolInfo() {}
     virtual void setProtocolModuleID(ProtocolModuleID _moduleID) { m_protocolModuleID = _moduleID; }
-    virtual void setMinVersion(ProtocolVersion _minVersion) { m_minVersion = _minVersion; }
-    virtual void setMaxVersion(ProtocolVersion _maxVersion) { m_maxVersion = _maxVersion; }
+    virtual void setMinVersion(uint32_t _minVersion) { m_minVersion = _minVersion; }
+    virtual void setMaxVersion(uint32_t _maxVersion) { m_maxVersion = _maxVersion; }
     // set the negotiated version
-    virtual void setVersion(ProtocolVersion _version) { m_version = _version; }
+    virtual void setVersion(uint32_t _version) { m_version = _version; }
 
     // the moduleID
     virtual ProtocolModuleID protocolModuleID() const { return m_protocolModuleID; }
     // the minimum supported version number
-    virtual ProtocolVersion minVersion() const { return m_minVersion; }
+    virtual uint32_t minVersion() const { return m_minVersion; }
     // the maximum supported version number
-    virtual ProtocolVersion maxVersion() const { return m_maxVersion; }
+    virtual uint32_t maxVersion() const { return m_maxVersion; }
 
     // the negotiated version
-    virtual ProtocolVersion version() const { return m_version; }
+    virtual uint32_t version() const { return m_version; }
 
 protected:
     ProtocolModuleID m_protocolModuleID;
-    ProtocolVersion m_minVersion;
-    ProtocolVersion m_maxVersion;
-    ProtocolVersion m_version;
+    // Note: here can't use enum Version type in case of setVersion failed for no-defined Version
+    uint32_t m_minVersion;
+    uint32_t m_maxVersion;
+    uint32_t m_version;
 };
 }  // namespace protocol
 }  // namespace bcos
