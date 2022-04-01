18407aab9036ca861919c23074e9376025bc39dd
cyjseagull <yujiechen_hust@163.com>
2022-04-01 21:54:50 +0800

    fillBlock without threadPool

--- bcos-txpool/bcos-txpool/TxPool.h
+++ bcos-txpool/bcos-txpool/TxPool.h
@@ -41,7 +41,6 @@ public:
         // threadpool for verify block
         m_verifier = std::make_shared<ThreadPool>("verifier", 4);
         m_sealer = std::make_shared<ThreadPool>("txsSeal", 1);
-        m_filler = std::make_shared<ThreadPool>("txsFiller", 4);
         TXPOOL_LOG(INFO) << LOG_DESC("create TxPool")
                          << LOG_KV("submitterWorkerNum", _verifierWorkerNum);
     }
@@ -155,7 +154,6 @@ private:
     ThreadPool::Ptr m_worker;
     ThreadPool::Ptr m_verifier;
     ThreadPool::Ptr m_sealer;
-    ThreadPool::Ptr m_filler;
     std::atomic_bool m_running = {false};
 };
 }  // namespace txpool
