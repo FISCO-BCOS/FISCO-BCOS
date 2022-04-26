9d7160454ecd4657d8265fc7f104d68c40f2e4b1
chuwen <786675283@qq.com>
2022-04-26 15:23:44 +0800

    Remove data compression logic when disk encryption

--- bcos-security/bcos-security/StorageEncDecHelper.cpp
+++ bcos-security/bcos-security/StorageEncDecHelper.cpp
@@ -23,7 +23,6 @@
 #include "StorageEncDecHelper.h"
 #include <bcos-crypto/encrypt/AESCrypto.h>
 #include <bcos-crypto/encrypt/SM4Crypto.h>
-#include <bcos-tool/SnappyCompress.h>
 #include <bcos-utilities/DataConvertUtility.h>
 
 using namespace bcos;
@@ -35,87 +34,41 @@ namespace bcos
 namespace security
 {
 
-EncHookFunction StorageEncDecHelper::getEncryptHandler(
-    const std::vector<uint8_t>& _encryptKey, bool _enableCompress)
+EncHookFunction StorageEncDecHelper::getEncryptHandler(const std::vector<uint8_t>& _encryptKey)
 {
     // get dataKey according to ciperDataKey from keyCenter
     return [=](std::string const& data, std::string& encData) {
         try
         {
-            if (!_enableCompress)
-            {
-                SymmetricEncryption::Ptr encrypt = std::make_shared<AESCrypto>();
-                bytesPointer result =
-                    encrypt->symmetricEncrypt((const unsigned char*)data.data(),
-                        data.size(), (const unsigned char*)_encryptKey.data(), _encryptKey.size());
-
-                encData.resize(result->size());
-                memcpy(encData.data(), result->data(), result->size());
-
-                return;
-            }
-            // compress before encrypt
-            std::shared_ptr<bytes> compressedData = std::make_shared<bytes>();
-            size_t compressedSize = bcos::tool::SnappyCompress::compress(
-                bytesConstRef((const unsigned char*)data.data(), data.length()), *compressedData);
-            if (compressedSize == 0)
-            {
-                std::string errorInfo =
-                    "Compress data for " + toHex(_encryptKey) + " failed for compress failed";
-                BCOS_LOG(ERROR) << LOG_DESC(errorInfo);
-                BOOST_THROW_EXCEPTION(EncryptFailed() << errinfo_comment(errorInfo));
-            }
-
             SymmetricEncryption::Ptr encrypt = std::make_shared<AESCrypto>();
-            bytesPointer result = encrypt->symmetricEncrypt(
-                (const unsigned char*)compressedData->data(), compressedData->size(),
-                (const unsigned char*)_encryptKey.data(), _encryptKey.size());
+            bytesPointer result = encrypt->symmetricEncrypt((const unsigned char*)data.data(),
+                data.size(), (const unsigned char*)_encryptKey.data(), _encryptKey.size());
 
             encData.resize(result->size());
             memcpy(encData.data(), result->data(), result->size());
         }
         catch (const std::exception& e)
         {
-            std::string error_info = "encryt value for data=" + std::string(data.data(), data.size()) +
-                                     " failed, EINFO: " + boost::diagnostic_information(e);
+            std::string error_info =
+                "encryt value for data=" + std::string(data.data(), data.size()) +
+                " failed, EINFO: " + boost::diagnostic_information(e);
             BCOS_LOG(ERROR) << LOG_DESC(error_info);
             BOOST_THROW_EXCEPTION(EncryptFailed() << errinfo_comment(error_info));
         }
     };
 }
 
-DecHookFunction StorageEncDecHelper::getDecryptHandler(
-    const std::vector<uint8_t>& _decryptKey, bool _enableCompress)
+DecHookFunction StorageEncDecHelper::getDecryptHandler(const std::vector<uint8_t>& _decryptKey)
 {
     return [=](std::string& data) {
         try
         {
             SymmetricEncryption::Ptr encrypt = std::make_shared<AESCrypto>();
-            bytesPointer result = encrypt->symmetricDecrypt((const unsigned char*)data.data(), data.size(),
-                (const unsigned char*)_decryptKey.data(), _decryptKey.size());
+            bytesPointer result = encrypt->symmetricDecrypt((const unsigned char*)data.data(),
+                data.size(), (const unsigned char*)_decryptKey.data(), _decryptKey.size());
 
             data.resize(result->size());
             memcpy(data.data(), result->data(), result->size());
-
-            if (!_enableCompress)
-            {
-                return;
-            }
-            // uncompress the decrypted data
-            std::shared_ptr<bytes> uncompressedData = std::make_shared<bytes>();
-            auto uncompressedDataSize = bcos::tool::SnappyCompress::uncompress(
-                bytesConstRef((const unsigned char*)data.data(), data.length()), *uncompressedData);
-            if (uncompressedDataSize == 0)
-            {
-                std::string errorInfo =
-                    "decrypt value for key " + toHex(_decryptKey) + " failed for uncompress failed";
-                BCOS_LOG(ERROR) << LOG_DESC(errorInfo);
-                BOOST_THROW_EXCEPTION(DecryptFailed() << errinfo_comment(errorInfo));
-            }
-            // resize and copy the uncompressed data
-            data.resize(uncompressedData->size());
-            memcpy((void*)data.data(), (const void*)uncompressedData->data(),
-                uncompressedData->size());
         }
         catch (const std::exception& e)
         {
@@ -126,87 +79,41 @@ DecHookFunction StorageEncDecHelper::getDecryptHandler(
     };
 }
 
-EncHookFunction StorageEncDecHelper::getEncryptHandlerSM(
-    const std::vector<uint8_t>& _encryptKey, bool _enableCompress)
+EncHookFunction StorageEncDecHelper::getEncryptHandlerSM(const std::vector<uint8_t>& _encryptKey)
 {
     // get dataKey according to ciperDataKey from keyCenter
     return [=](std::string const& data, std::string& encData) {
         try
         {
-            if (!_enableCompress)
-            {
-                SymmetricEncryption::Ptr encrypt = std::make_shared<SM4Crypto>();
-                bytesPointer result =
-                    encrypt->symmetricEncrypt((const unsigned char*)data.data(),
-                        data.size(), (const unsigned char*)_encryptKey.data(), _encryptKey.size());
-
-                encData.resize(result->size());
-                memcpy(encData.data(), result->data(), result->size());
-
-                return;
-            }
-            // compress before encrypt
-            std::shared_ptr<bytes> compressedData = std::make_shared<bytes>();
-            size_t compressedSize = bcos::tool::SnappyCompress::compress(
-                bytesConstRef((const unsigned char*)data.data(), data.length()), *compressedData);
-            if (compressedSize == 0)
-            {
-                std::string errorInfo =
-                    "Compress data for " + toHex(_encryptKey) + " failed for compress failed";
-                BCOS_LOG(ERROR) << LOG_DESC(errorInfo);
-                BOOST_THROW_EXCEPTION(EncryptFailed() << errinfo_comment(errorInfo));
-            }
-
             SymmetricEncryption::Ptr encrypt = std::make_shared<SM4Crypto>();
-            bytesPointer result = encrypt->symmetricEncrypt(
-                (const unsigned char*)compressedData->data(), compressedData->size(),
-                (const unsigned char*)_encryptKey.data(), _encryptKey.size());
+            bytesPointer result = encrypt->symmetricEncrypt((const unsigned char*)data.data(),
+                data.size(), (const unsigned char*)_encryptKey.data(), _encryptKey.size());
 
             encData.resize(result->size());
             memcpy(encData.data(), result->data(), result->size());
         }
         catch (const std::exception& e)
         {
-            std::string error_info = "encryt value for data=" + std::string(data.data(), data.size()) +
-                                     " failed, EINFO: " + boost::diagnostic_information(e);
+            std::string error_info =
+                "encryt value for data=" + std::string(data.data(), data.size()) +
+                " failed, EINFO: " + boost::diagnostic_information(e);
             BCOS_LOG(ERROR) << LOG_DESC(error_info);
             BOOST_THROW_EXCEPTION(EncryptFailed() << errinfo_comment(error_info));
         }
     };
 }
 
-DecHookFunction StorageEncDecHelper::getDecryptHandlerSM(
-    const std::vector<uint8_t>& _decryptKey, bool _enableCompress)
+DecHookFunction StorageEncDecHelper::getDecryptHandlerSM(const std::vector<uint8_t>& _decryptKey)
 {
     return [=](std::string& data) {
         try
         {
             SymmetricEncryption::Ptr encrypt = std::make_shared<SM4Crypto>();
-            bytesPointer result = encrypt->symmetricDecrypt((const unsigned char*)data.data(), data.size(),
-                (const unsigned char*)_decryptKey.data(), _decryptKey.size());
+            bytesPointer result = encrypt->symmetricDecrypt((const unsigned char*)data.data(),
+                data.size(), (const unsigned char*)_decryptKey.data(), _decryptKey.size());
 
             data.resize(result->size());
             memcpy(data.data(), result->data(), result->size());
-
-            if (!_enableCompress)
-            {
-                return;
-            }
-            // uncompress the decrypted data
-            std::shared_ptr<bytes> uncompressedData = std::make_shared<bytes>();
-            auto uncompressedDataSize = bcos::tool::SnappyCompress::uncompress(
-                bytesConstRef((const unsigned char*)data.data(), data.length()), *uncompressedData);
-            if (uncompressedDataSize == 0)
-            {
-                std::string errorInfo =
-                    "decrypt value for key " + toHex(_decryptKey) + " failed for uncompress failed";
-                BCOS_LOG(ERROR) << LOG_DESC(errorInfo);
-                BOOST_THROW_EXCEPTION(DecryptFailed() << errinfo_comment(errorInfo));
-            }
-            // resize and copy the uncompressed data
-            data.resize(uncompressedData->size());
-            memcpy((void*)data.data(), (const void*)uncompressedData->data(),
-                uncompressedData->size());
         }
         catch (const std::exception& e)
         {
