17525e988678f0b76a0194019347ebb6f08c731a
morebtcg <651932351@qq.com>
2022-04-27 15:07:14 +0800

    Add ExceptionHolder

--- bcos-tool/BinaryMerkleTrie.h
+++ bcos-tool/BinaryMerkleTrie.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "ExceptionHolder.h"
 #include <bcos-crypto/interfaces/crypto/Hasher.h>
 #include <boost/throw_exception.hpp>
 #include <algorithm>
+#include <exception>
 #include <iterator>
 #include <ranges>
 #include <type_traits>
@@ -50,28 +52,32 @@ public:
             BOOST_THROW_EXCEPTION(MerkleTrieException{});
         }
 
+        ExceptionHolder holder;
 #pragma omp parallel
         {
             Hasher localHasher;
 #pragma omp for
             for (size_t i = 0; i < inputSize; i += 2)
             {
-                if (i + 1 < inputSize)
-                {
-                    auto& hash1 = input[i];
-                    auto& hash2 = input[i + 1];
-                    localHasher.update(std::span<byte const>{hash1.data(), hash1.size});
-                    localHasher.update(std::span<byte const>{hash2.data(), hash2.size});
-                    output[i / 2] = localHasher.final();
-                }
-                else
-                {
-                    auto& hash = input[i];
-                    localHasher.update(std::span<byte const>{hash.data(), hash.size});
-                    output[i / 2] = localHasher.final();
-                }
+                holder([&]() {
+                    if (i + 1 < inputSize) [[likely]]
+                    {
+                        auto& hash1 = input[i];
+                        auto& hash2 = input[i + 1];
+                        localHasher.update(std::span<byte const>{hash1.data(), hash1.size});
+                        localHasher.update(std::span<byte const>{hash2.data(), hash2.size});
+                        output[i / 2] = localHasher.final();
+                    }
+                    else
+                    {
+                        auto& hash = input[i];
+                        localHasher.update(std::span<byte const>{hash.data(), hash.size});
+                        output[i / 2] = localHasher.final();
+                    }
+                });
             }
         }
+        holder.rethrow();
     }
 
 private:
