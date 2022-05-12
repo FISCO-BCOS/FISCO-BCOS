fb6c056350f127ce787e0816aefd5971108e1d69
morebtcg <651932351@qq.com>
2022-05-12 18:15:46 +0800

    Rename merkle

--- bcos-tool/MerkleTrie.h
+++ bcos-tool/MerkleTrie.h
@@ -18,29 +18,64 @@ namespace bcos::tool
 template <class Range, class HashType>
 concept InputRange =
     std::ranges::random_access_range<Range> && bcos::crypto::TrivialObject<HashType> &&
-    std::is_same_v<std::remove_cvref_t<std::remove_cvref_t<std::ranges::range_value_t<Range>>>,
-        HashType>;
+    std::is_same_v<std::remove_cvref_t<std::ranges::range_value_t<Range>>, HashType>;
 
 template <class Range, class HashType>
 concept OutputRange = std::ranges::random_access_range<Range> &&
     std::ranges::output_range<Range, HashType> && bcos::crypto::TrivialObject<HashType>;
 
-template <bcos::crypto::Hasher HasherType, class HashType, size_t stepSize = 2>
-class MerkleTrie
+template <class Range, class HashType>
+concept Proof =
+    std::ranges::input_range<Range> && std::is_same_v<std::ranges::range_value_t<Range>, HashType>;
+
+template <bcos::crypto::Hasher HasherType, class HashType, size_t width = 2>
+class Merkle
 {
 public:
-    static_assert(stepSize >= 2, "Step size too short!");
+    static_assert(width >= 2, "Width too short, at least 2");
 
-    MerkleTrie(){};
+    Merkle() = default;
+    Merkle(const Merkle&) = default;
+    Merkle(Merkle&&) = default;
+    Merkle& operator=(const Merkle&) = default;
+    Merkle& operator=(Merkle&&) = default;
+    ~Merkle() = default;
 
-    void calculateAllHashes(InputRange<HashType> auto const& input)
+    static bool verify(Proof<HashType> auto&& proof, HashType&& root)
     {
-        auto const inputSize = std::size(input);
+        if (std::empty(proof))
+            BOOST_THROW_EXCEPTION(std::invalid_argument{"Empty proof input!"});
+
+        size_t count = 0;
+        std::optional<HashType> lastHash;
+        HasherType hasher;
+        for (auto& hash : proof)
+        {
+            if (lastHash)
+            {
+                hasher.update(*lastHash);
+                hasher.update(hash);
+                hasher.final(*lastHash);
+            }
+            else
+            {
+                hasher.update(hash);
+                lastHash.emplace(hash);
+            }
+
+            ++count;
+        }
+    }
+
+    HashType import(InputRange<HashType> auto&& input, bool parallel = false)
+    {
+        auto inputSize = std::size(input);
         if (inputSize <= 0)
             BOOST_THROW_EXCEPTION(std::invalid_argument{
                 (boost::format{"Input size too short: %ld"} % inputSize).str()});
 
-        m_nodes.resize(inputSize + (inputSize - 1));
+        m_count = inputSize;
+        m_nodes.resize(getNodeSize(inputSize));
 
         std::copy_n(std::begin(input), inputSize, m_nodes.begin());
         std::sort(m_nodes.begin(), m_nodes.begin() + inputSize);
@@ -49,42 +84,67 @@ public:
 
         while (range.begin() != range.end())
         {
-            auto length = calculateLevelHashes<HashType>(
-                range, std::ranges::subrange(range.begin(), m_nodes.end()));
+            assert(range.end() <= m_nodes.end());
+            auto length = calculateLevelHashes(
+                range, std::ranges::subrange(range.begin(), m_nodes.end()), parallel);
+
             range = std::ranges::subrange{range.end(), range.end() + length};
         }
+
+        return *m_nodes.rbegin();
+    }
+
+    void clear()
+    {
+        m_nodes.clear();
+        m_count = 0;
+    }
+    auto empty() const { return !m_count; }
+
+private:
+    auto getNodeSize(std::integral auto inputSize)
+    {
+        auto nodeSize = inputSize;
+        while (inputSize > 1)
+        {
+            inputSize = getLevelSize(inputSize);
+            nodeSize += inputSize;
+        }
+
+        return nodeSize;
+    }
+
+    auto getLevelSize(std::integral auto inputSize)
+    {
+        return inputSize == 1 ? 0 : (inputSize + (width - 1)) / width;
     }
 
     size_t calculateLevelHashes(
-        InputRange<HashType> auto&& input, OutputRange<HashType> auto&& output)
+        InputRange<HashType> auto&& input, OutputRange<HashType> auto&& output, bool parallel)
     {
         auto inputSize = std::size(input);
         auto outputSize = std::size(output);
-        auto expectOutputSize = (inputSize + (stepSize - 1)) / stepSize;
+        auto expectOutputSize = getLevelSize(inputSize);
 
-        if (inputSize <= 0) [[unlikely]]
-            BOOST_THROW_EXCEPTION(std::invalid_argument{
-                (boost::format{"Input size too short: %ld"} % inputSize).str()});
-
-        if (outputSize < expectOutputSize) [[unlikely]]
-            BOOST_THROW_EXCEPTION(
-                std::invalid_argument{(boost::format{"Output size too short: %ld, expect: %ld"} %
-                                       outputSize % expectOutputSize)
-                                          .str()});
+        assert(inputSize > 0);
+        assert(outputSize >= expectOutputSize);
 
         ExceptionHolder holder;
-#pragma omp parallel if (inputSize >= MIN_PARALLEL_SIZE)
+#pragma omp parallel if ((inputSize >= MIN_PARALLEL_SIZE) && parallel)
         {
             HasherType hasher;
 #pragma omp for
-            for (decltype(inputSize) i = 0; i < inputSize; i += stepSize)
+            for (decltype(inputSize) i = 0; i < inputSize; i += width)
             {
                 holder.run([&]() {
-                    for (auto j = i; j < i + stepSize && j < inputSize; ++j)
+                    for (auto j = i; j < i + width && j < inputSize; ++j)
                     {
                         hasher.update(input[j]);
                     }
-                    hasher.final(output[i / stepSize]);
+                    auto outputOffset = i / width;
+                    assert(outputOffset < outputSize);
+
+                    hasher.final(output[outputOffset]);
                 });
             }
         }
@@ -95,7 +155,9 @@ public:
 
 private:
     std::vector<HashType> m_nodes;
+    size_t m_count = 0;
 
     constexpr static size_t MIN_PARALLEL_SIZE = 32;
 };
+
 }  // namespace bcos::tool
