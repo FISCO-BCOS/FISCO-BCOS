eb573432f37321c2bb658722a50214bbf9302c5c
morebtcg <651932351@qq.com>
2022-06-21 19:15:59 +0800

    Add block concept

--- bcos-framework/bcos-framework/concepts/Block.h
+++ bcos-framework/bcos-framework/concepts/Block.h
@@ -5,18 +5,44 @@
 
 namespace bcos::concepts
 {
-template <class BlockHeaderType>
-concept BlockHeader = requires(BlockHeaderType blockHeader)
+template <class ParentInfoType>
+concept ParentInfo = requires(ParentInfoType parentInfo)
 {
-    std::integral<decltype(blockHeader.version)>;
-    std::integral<decltype(blockHeader.type)>;
-    blockHeader.txRoot;
-    blockHeader.receiptRoot;
-    blockHeader.stateRoot;
-    std::unsigned_integral<decltype(blockHeader.number)>;
-    std::unsigned_integral<decltype(blockHeader.gasUsed)>;
-    std::integral<decltype(blockHeader.timestamp)>;
-    std::integral<decltype(blockHeader.sealer)>;
-    std::ranges::range<decltype(blockHeader.sealerList)>;
+    std::integral<decltype(parentInfo.blockNumber)>;
+    parentInfo.blockHash;
+};
+
+template <class SignatureType>
+concept Signature = requires(SignatureType signature)
+{
+    std::integral<decltype(signature.sealerIndex)>;
+    signature.signature;
+};
+
+template <class BlockHeaderDataType>
+concept BlockHeaderData = requires(BlockHeaderDataType blockHeaderData)
+{
+    std::integral<decltype(blockHeaderData.version)>;
+    requires std::ranges::range<decltype(blockHeaderData.parentInfo)> &&
+        ParentInfo<std::ranges::range_value_t<decltype(blockHeaderData.parentInfo)>>;
+    blockHeaderData.txRoot;
+    blockHeaderData.receiptRoot;
+    blockHeaderData.stateRoot;
+    std::unsigned_integral<decltype(blockHeaderData.blockNumber)>;
+    std::unsigned_integral<decltype(blockHeaderData.gasUsed)>;
+    std::integral<decltype(blockHeaderData.timestamp)>;
+    std::integral<decltype(blockHeaderData.sealer)>;
+    std::ranges::range<decltype(blockHeaderData.sealerList)>;
+    blockHeaderData.extraData;
+    std::ranges::range<decltype(blockHeaderData.consensusWeights)>;
+};
+
+template <class BlockHeaderType>
+concept BlockHeader = requires(BlockHeaderType block)
+{
+    BlockHeaderData<decltype(block.data)>;
+    block.dataHash;
+    requires std::ranges::range<decltype(block.signatureList)> &&
+        Signature<std::ranges::range_value_t<decltype(block.signatureList)>>;
 };
 }  // namespace bcos::concepts
