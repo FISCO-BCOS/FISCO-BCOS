18e28a82ad69174261d43c9d995ca8ff85c075e8
XingQiang Bai <bxq2011hust@qq.com>
2022-06-27 09:47:27 +0800

    add -k option of build_chain and support key_page_size for pro & max (#2537)
    Co-authored-by: cyjseagull <yujiechen_hust@163.com>

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -107,5 +107,4 @@ hunter_config(etcd-cpp-apiv3 VERSION 0.2.5-local
 
 hunter_config(cryptopp VERSION 8.6.0
     URL "https://${URL_BASE}/FISCO-BCOS/cryptopp/archive/1887f2f05679debf5d5b9553b8d1a686049eb8c0.tar.gz"
-    SHA1 062e044d15415151ddebe67cce63fbaeba4ba596
-)
+    SHA1 062e044d15415151ddebe67cce63fbaeba4ba596)
