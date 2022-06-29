cdcd80a922289b37ab99989ce323a3f90b4aab4a
wenlinli <1574249665@qq.com>
2022-06-29 11:11:51 +0800

    add executivePool ut

--- bcos-scheduler/test/testExecutivePool.cpp
+++ bcos-scheduler/test/testExecutivePool.cpp
@@ -129,7 +129,7 @@ BOOST_AUTO_TEST_CASE(forEachTest)
     }
     executivePool->forEach(
         ExecutivePool::MessageHint::ALL, [this](int64_t contextID, ExecutiveState::Ptr) {
-            auto executiveState = get(contextID);
+            auto executiveState = executivePool->get(contextID);
             auto BCOS_LOG(DEBUG) << LOG_BADGE("SCHEDULE")
                                  << LOG_DESC("test executivepool whether is null");
             return true;
