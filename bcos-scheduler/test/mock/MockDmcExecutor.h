609b3313b53395c14c34617e68c4df8f35c26df9
wenlinli <1574249665@qq.com>
2022-08-11 17:09:50 +0800

    update ut

--- bcos-scheduler/test/mock/MockDmcExecutor.h
+++ bcos-scheduler/test/mock/MockDmcExecutor.h
@@ -115,109 +115,106 @@ public:
     {
         SCHEDULER_LOG(TRACE) << "Receiving nextBlock: " << blockHeader->number();
         m_blockNumber = blockHeader->number();
-        callback(nullptr);  // always success}
+        callback(nullptr);  // always success
+    }
 
 
-        void executeTransaction(bcos::protocol::ExecutionMessage::UniquePtr input,
-            std::function<void(bcos::Error::UniquePtr, bcos::protocol::ExecutionMessage::UniquePtr)>
-                callback) override
+    void executeTransaction(bcos::protocol::ExecutionMessage::UniquePtr input,
+        std::function<void(bcos::Error::UniquePtr, bcos::protocol::ExecutionMessage::UniquePtr)>
+            callback) override
+    {
+        if (input->to() == "0xaabbccdd")
         {
-            if (input->to() == "0xaabbccdd")
-            {
-                callback(BCOS_ERROR_UNIQUE_PTR(-1, "i am an error!!!!"), nullptr);
-                return;
-            }
-
-            // Always success
-            BOOST_CHECK(input);
-            if (input->type() == bcos::protocol::ExecutionMessage::TXHASH)
-            {
-                BOOST_CHECK_NE(input->transactionHash(), bcos::crypto::HashType());
-            }
-
-            input->setStatus(0);
-            input->setMessage("");
-
-            std::string data = "Hello world!";
-            input->setData(bcos::bytes(data.begin(), data.end()));
-            input->setType(bcos::protocol::ExecutionMessage::FINISHED);
-
-            callback(nullptr, std::move(input));
+            callback(BCOS_ERROR_UNIQUE_PTR(-1, "i am an error!!!!"), nullptr);
+            return;
         }
 
-        void call(bcos::protocol::ExecutionMessage::UniquePtr input,
-            std::function<void(bcos::Error::UniquePtr, bcos::protocol::ExecutionMessage::UniquePtr)>
-                callback) override
-        {}
-
-        void executeTransactions(std::string,
-            gsl::span<bcos::protocol::ExecutionMessage::UniquePtr>,
-            std::function<void(bcos::Error::UniquePtr,
-                std::vector<bcos::protocol::ExecutionMessage::UniquePtr>)>) override
-        {}
-
-        void dagExecuteTransactions(gsl::span<bcos::protocol::ExecutionMessage::UniquePtr> inputs,
-            std::function<void(
-                bcos::Error::UniquePtr, std::vector<bcos::protocol::ExecutionMessage::UniquePtr>)>
-                callback) override
+        // Always success
+        BOOST_CHECK(input);
+        if (input->type() == bcos::protocol::ExecutionMessage::TXHASH)
         {
-            std::vector<bcos::protocol::ExecutionMessage::UniquePtr> messages(inputs.size());
-            for (auto i = 0u; i < inputs.size(); ++i)
+            BOOST_CHECK_NE(input->transactionHash(), bcos::crypto::HashType());
+        }
+
+        input->setStatus(0);
+        input->setMessage("");
+
+        std::string data = "Hello world!";
+        input->setData(bcos::bytes(data.begin(), data.end()));
+        input->setType(bcos::protocol::ExecutionMessage::FINISHED);
+
+        callback(nullptr, std::move(input));
+    }
+
+    void call(bcos::protocol::ExecutionMessage::UniquePtr input,
+        std::function<void(bcos::Error::UniquePtr, bcos::protocol::ExecutionMessage::UniquePtr)>
+            callback) override
+    {}
+
+    void executeTransactions(std::string, gsl::span<bcos::protocol::ExecutionMessage::UniquePtr>,
+        std::function<void(bcos::Error::UniquePtr,
+            std::vector<bcos::protocol::ExecutionMessage::UniquePtr>)>) override
+    {}
+
+    void dagExecuteTransactions(gsl::span<bcos::protocol::ExecutionMessage::UniquePtr> inputs,
+        std::function<void(
+            bcos::Error::UniquePtr, std::vector<bcos::protocol::ExecutionMessage::UniquePtr>)>
+            callback) override
+    {
+        std::vector<bcos::protocol::ExecutionMessage::UniquePtr> messages(inputs.size());
+        for (auto i = 0u; i < inputs.size(); ++i)
+        {
+            auto [it, inserted] = m_dagHashes.emplace(inputs[i]->transactionHash());
+            boost::ignore_unused(it);
+            BOOST_TEST(inserted);
+
+            // SCHEDULER_LOG(TRACE) << "Executing: " << inputs[i].get();
+            BOOST_TEST(inputs[i].get());
+            messages.at(i) = std::move(inputs[i]);
+            if (i < 5)
             {
-                auto [it, inserted] = m_dagHashes.emplace(inputs[i]->transactionHash());
-                boost::ignore_unused(it);
-                BOOST_TEST(inserted);
-
-                // SCHEDULER_LOG(TRACE) << "Executing: " << inputs[i].get();
-                BOOST_TEST(inputs[i].get());
-                messages.at(i) = std::move(inputs[i]);
-                if (i < 5)
-                {
-                    messages[i]->setType(protocol::ExecutionMessage::SEND_BACK);
-                }
-                else
-                {
-                    messages[i]->setType(protocol::ExecutionMessage::FINISHED);
-
-                    std::string result = "OK!";
-                    messages[i]->setData(bcos::bytes(result.begin(), result.end()));
-                }
+                messages[i]->setType(protocol::ExecutionMessage::SEND_BACK);
             }
+            else
+            {
+                messages[i]->setType(protocol::ExecutionMessage::FINISHED);
 
-            callback(nullptr, std::move(messages));
+                std::string result = "OK!";
+                messages[i]->setData(bcos::bytes(result.begin(), result.end()));
+            }
         }
 
-        void getHash(bcos::protocol::BlockNumber,
-            std::function<void(bcos::Error::UniquePtr, crypto::HashType)>) override
-        {}
+        callback(nullptr, std::move(messages));
+    }
 
-        void prepare(const bcos::protocol::TwoPCParams&, std::function<void(bcos::Error::Ptr)>)
-            override
-        {}
+    void getHash(bcos::protocol::BlockNumber,
+        std::function<void(bcos::Error::UniquePtr, crypto::HashType)>) override
+    {}
 
-        void commit(const bcos::protocol::TwoPCParams&, std::function<void(bcos::Error::Ptr)>)
-            override
-        {}
+    void prepare(const bcos::protocol::TwoPCParams&, std::function<void(bcos::Error::Ptr)>) override
+    {}
 
-        void rollback(const bcos::protocol::TwoPCParams&, std::function<void(bcos::Error::Ptr)>)
-            override
-        {}
+    void commit(const bcos::protocol::TwoPCParams&, std::function<void(bcos::Error::Ptr)>) override
+    {}
 
-        void getCode(std::string_view, std::function<void(bcos::Error::Ptr, bcos::bytes)> callback)
-            override
-        {
-            callback(nullptr, {});
-        }
-        void getABI(std::string_view, std::function<void(bcos::Error::Ptr, std::string)>) override
-        {}
-        void reset(std::function<void(bcos::Error::Ptr)>) override {}
-        // void start() override() {}
-        // void stop() override() {}
+    void rollback(
+        const bcos::protocol::TwoPCParams&, std::function<void(bcos::Error::Ptr)>) override
+    {}
+
+    void getCode(
+        std::string_view, std::function<void(bcos::Error::Ptr, bcos::bytes)> callback) override
+    {
+        callback(nullptr, {});
+    }
+    void getABI(std::string_view, std::function<void(bcos::Error::Ptr, std::string)>) override {}
+    void reset(std::function<void(bcos::Error::Ptr)>) override {}
+    // void start() override() {}
+    // void stop() override() {}
 
 
-        std::string m_name;
-        bcos::protocol::BlockNumber m_blockNumber = 0;
-        std::set<bcos::crypto::HashType> m_dagHashes;
-    };
+    std::string m_name;
+    bcos::protocol::BlockNumber m_blockNumber = 0;
+    std::set<bcos::crypto::HashType> m_dagHashes;
+};
 #pragma GCC diagnostic pop
 }  // namespace bcos::test
