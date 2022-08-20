28889cd391f7b28462286e0f0f9bf9e6ec96df04
麦立健 <102428352+MaiLiJian-CN@users.noreply.github.com>
2022-08-20 08:23:56 +0800

    Delete bcos-executor directory
    Signed-off-by: 麦立健 <102428352+MaiLiJian-CN@users.noreply.github.com>

--- bcos-executor/test/solidity/precompiled/SystemConfigPrecompiled.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.0;
-
-contract SystemConfigPrecompiled
-{
-    function setValueByKey(string memory key, string memory value) public returns(int256){}
-    function getValueByKey(string memory key) public view returns(string memory,int256){}
-}
