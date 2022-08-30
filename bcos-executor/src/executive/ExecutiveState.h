dca5469a1bf1a8eefb6ab4ea2411afedf7533a47
wenlinli <1574249665@qq.com>
2022-08-30 12:11:53 +0800

    add Dag unitTest

--- bcos-executor/src/executive/ExecutiveState.h
+++ bcos-executor/src/executive/ExecutiveState.h
@@ -50,7 +50,7 @@ public:
     };
 
     Status getStatus() { return m_status; }
-    virtual CallParameters::UniquePtr go();
+    CallParameters::UniquePtr go();
     void setResumeParam(CallParameters::UniquePtr pullParam);
     int64_t getContextID() { return m_contextID; }
     int64_t getSeq() { return m_seq; }
