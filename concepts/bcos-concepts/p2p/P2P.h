d5b5e760bffc01f183cfa3bf3bb97f31b5888aa4
monan <651932351@qq.com>
2022-10-28 14:00:13 +0800

    Update P2P nodeIDs method

--- concepts/bcos-concepts/p2p/P2P.h
+++ concepts/bcos-concepts/p2p/P2P.h
@@ -12,17 +12,24 @@ template <class Impl>
 class P2P
 {
 public:
-    task::Task<std::set<std::string>> nodeIDs() { return impl().impl_nodeIDs(); }
+    using NodeType = uint16_t;
+    using ModuleID = int;
 
-    task::Task<void> sendMessageByNodeID(int moduleID, auto const& nodeID,
+    template <RANGES::range NodeIDs>
+    task::Task<NodeIDs> nodeIDs()
+    {
+        return impl().impl_nodeIDs();
+    }
+
+    task::Task<void> sendMessageByNodeID(ModuleID moduleID, auto const& nodeID,
         bcos::concepts::serialize::Serializable auto const& request,
         bcos::concepts::serialize::Serializable auto& response)
     {
         return impl().impl_sendMessageByNodeID(moduleID, nodeID, request, response);
     }
 
-    task::Task<void> broadcastMessage(
-        uint16_t type, int moduleID, bcos::concepts::serialize::Serializable auto const& request)
+    task::Task<void> broadcastMessage(NodeType type, ModuleID moduleID,
+        bcos::concepts::serialize::Serializable auto const& request)
     {
         return impl().impl_broadcastMessage(type, moduleID, request);
     }
