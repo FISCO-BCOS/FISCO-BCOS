d1f6e06b42a23d4b885d562ee9b4ff9fadf4155e
JimmyShi22 <417711026@qq.com>
2022-11-29 17:15:03 +0800

    move constant precompile to executor constructor

--- bcos-executor/src/executor/TransactionExecutor.cpp
+++ bcos-executor/src/executor/TransactionExecutor.cpp
@@ -216,6 +216,11 @@ void TransactionExecutor::initEvmEnvironment()
         m_constantPrecompiled->insert({AUTH_CONTRACT_MGR_ADDRESS,
             std::make_shared<precompiled::ContractAuthMgrPrecompiled>(m_hashImpl)});
     }
+
+    m_constantPrecompiled->insert(
+        {ACCOUNT_MGR_ADDRESS, std::make_shared<AccountManagerPrecompiled>()});
+    m_constantPrecompiled->insert({ACCOUNT_ADDRESS, std::make_shared<AccountPrecompiled>()});
+
     m_constantPrecompiled->insert(
         {GROUP_SIG_ADDRESS, std::make_shared<precompiled::GroupSigPrecompiled>(m_hashImpl)});
     m_constantPrecompiled->insert(
@@ -227,6 +232,7 @@ void TransactionExecutor::initEvmEnvironment()
     // create the zkp-precompiled
     m_constantPrecompiled->insert(
         {DISCRETE_ZKP_ADDRESS, std::make_shared<bcos::precompiled::ZkpPrecompiled>(m_hashImpl)});
+
 
     // test precompiled
     CpuHeavyPrecompiled::registerPrecompiled(m_constantPrecompiled, m_hashImpl);
@@ -271,6 +277,10 @@ void TransactionExecutor::initWasmEnvironment()
         m_constantPrecompiled->insert({AUTH_CONTRACT_MGR_ADDRESS,
             std::make_shared<precompiled::ContractAuthMgrPrecompiled>(m_hashImpl)});
     }
+
+    m_constantPrecompiled->insert(
+        {ACCOUNT_MANAGER_NAME, std::make_shared<AccountManagerPrecompiled>()});
+    m_constantPrecompiled->insert({ACCOUNT_ADDRESS, std::make_shared<AccountPrecompiled>()});
 
     set<string> builtIn = {CRYPTO_NAME, GROUP_SIG_NAME, RING_SIG_NAME};
     m_builtInPrecompiled = make_shared<set<string>>(builtIn);
