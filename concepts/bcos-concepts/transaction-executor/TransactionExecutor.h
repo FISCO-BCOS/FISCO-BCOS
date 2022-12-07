351e9bbaac68cdefb9e02ecc1c26f6c31bd62740
monan <651932351@qq.com>
2022-12-07 16:35:54 +0800

    Update transaction executor interfaces

--- concepts/bcos-concepts/transaction-executor/TransactionExecutor.h
+++ concepts/bcos-concepts/transaction-executor/TransactionExecutor.h
@@ -5,13 +5,37 @@
 
 namespace bcos::concepts::transaction_executor
 {
+
+// All auto interfaces is awaitable
 template <class Impl>
 class TransactionExecutorBase
 {
 public:
-    task::Task<void> executeTransactions(block::Block auto const& block)
+    void nextBlockHeader(block::Block auto const& blockHeader)
     {
-        return impl().impl_executeTransactions(block);
+        return impl_nextBlockHeader(blockHeader);
+    }
+
+    // Return awaitable receipt::TransactionReceipt
+    auto call(transaction::Transaction auto const& transaction) { return impl_call(transaction); }
+
+    // Return awaitable receipts
+    auto executeTransactions(RANGES::range auto const& transactions) requires
+        transaction::Transaction<RANGES::range_value_t<decltype(transactions)>>
+    {
+        return impl().impl_executeTransactions(transactions);
+    }
+
+    // Return awaitable bytes
+    auto getABI(bytebuffer::ByteBuffer auto const& contract)
+    {
+        return impl().impl_getABI(contract);
+    }
+
+    // Return awaitable bytes
+    auto getCode(bytebuffer::ByteBuffer auto const& contract)
+    {
+        return impl().impl_getCode(contract);
     }
 
 private:
