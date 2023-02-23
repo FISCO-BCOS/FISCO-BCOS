e2b162dac48fb61e1c72ff5de77ae0aabadda9ad
monan <651932351@qq.com>
2023-02-23 14:44:40 +0800

    Add three way string_view for macos

---
+++ bcos-utilities/bcos-utilities/ThreeWay4StringView.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef __APPLE__
+#include "Ranges.h"
+#include <compare>
+#include <range/v3/algorithm/lexicographical_compare.hpp>
+#include <string_view>
+
+namespace std
+{
+weak_ordering operator<=>(const string_view& lhs, const string_view& rhs)
+{
+    auto sizeComp = lhs.size() <=> rhs.size();
+    if (sizeComp != weak_ordering::equivalent)
+    {
+        return sizeComp;
+    }
+
+    if (RANGES::lexicographical_compare(lhs, rhs))
+    {
+        return weak_ordering::greater;
+    }
+    return weak_ordering::less;
+}
+}  // namespace std
+
+#endif
