b8c45ef4ef2337fec77f557764cc78c3bac5baef
octopus <912554887@qq.com>
2023-03-17 11:03:19 +0800

    optimized log print

--- bcos-gateway/bcos-gateway/libp2p/Common.h
+++ bcos-gateway/bcos-gateway/libp2p/Common.h
@@ -17,6 +17,7 @@ namespace gateway
 #define P2PMSG_LOG(LEVEL) BCOS_LOG(LEVEL) << "[P2PService][P2PMessage]"
 #define P2PSESSION_LOG(LEVEL) BCOS_LOG(LEVEL) << "[P2PService][P2PSession]"
 #define SERVICE_LOG(LEVEL) BCOS_LOG(LEVEL) << "[P2PService][Service]"
+#define SERVICE2_LOG(LEVEL) BCOS_LOG(LEVEL) << "[P2PService][Service2]"
 #define SERVICE_ROUTER_LOG(LEVEL) BCOS_LOG(LEVEL) << "[P2PService][Router]"
 
 /// default compress threshold: 1KB
