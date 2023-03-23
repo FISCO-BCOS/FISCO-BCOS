8d0ce07a955eb4be80563861800bde70c6c8c95e
wenlinli <1574249665@qq.com>
2023-03-23 20:33:44 +0800

    Revert "fix getABI problem && add getBlockByNumber try-catch (#3518)"
    This reverts commit 6a85510ef6c01619d4950a15c25d9fdf8b3436e1.

--- bcos-ledger/src/libledger/LedgerImpl.h
+++ bcos-ledger/src/libledger/LedgerImpl.h
@@ -197,11 +197,8 @@ private:
 
         // create keyPageStorage
         auto stateStorageFactory = std::make_shared<storage::StateStorageFactory>(m_keyPageSize);
-        // getABI function begin in version 320
-        auto keyPageIgnoreTables = std::make_shared<std::set<std::string, std::less<>>>(
-                storage::IGNORED_ARRAY_310.begin(), storage::IGNORED_ARRAY_310.end());
         auto stateStorage =
-            stateStorageFactory->createStateStorage(m_backupStorage, m_compatibilityVersion, true, keyPageIgnoreTables);
+            stateStorageFactory->createStateStorage(m_backupStorage, m_compatibilityVersion);
 
         // try to get codeHash
         auto codeHashEntry = stateStorage->getRow(contractTableName, "codeHash");
