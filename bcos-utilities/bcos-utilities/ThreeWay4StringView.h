6c0c575fdf86601a776f60c15d4c3fadd0d5ea09
monan <651932351@qq.com>
2023-03-31 17:36:06 +0800

    Fix macos compile error

--- bcos-utilities/bcos-utilities/ThreeWay4StringView.h
+++ bcos-utilities/bcos-utilities/ThreeWay4StringView.h
@@ -7,19 +7,21 @@
 
 namespace std
 {
-inline weak_ordering operator<=>(const string_view& lhs, const string_view& rhs)
+inline strong_ordering operator<=>(const string_view& lhs, const string_view& rhs)
 {
-    auto sizeComp = lhs.size() <=> rhs.size();
-    if (sizeComp != weak_ordering::equivalent)
-    {
-        return sizeComp;
-    }
+    std::string lhsStr(lhs);
+    std::string rhsStr(rhs);
+    return lhsStr <=> rhsStr;
 
-    if (RANGES::lexicographical_compare(lhs, rhs))
-    {
-        return weak_ordering::greater;
-    }
-    return weak_ordering::less;
+    // if (lhs.size() == rhs.size())
+    // {
+    //     if (RANGES::lexicographical_compare(lhs, rhs))
+    //     {
+    //         return strong_ordering::greater;
+    //     }
+    //     return strong_ordering::less;
+    // }
+    // return lhs.size() <=> rhs.size();
 }
 }  // namespace std
 
