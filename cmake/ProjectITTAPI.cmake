b1d8032b004af2accb6ab6ac991b2f606f5a6b1a
monan <651932351@qq.com>
2023-04-04 09:05:21 +0800

    Use custom_command insted of install command

--- cmake/ProjectITTAPI.cmake
+++ cmake/ProjectITTAPI.cmake
@@ -1,10 +1,5 @@
 include(ExternalProject)
 
-set(ITT_INSTALL_COMMAND "true")
-if(WITH_VTUNE_ITT)
-    set(ITT_INSTALL_COMMAND "objcopy --weaken <SOURCE_DIR>/bin/libittnotify.a")
-endif()
-
 ExternalProject_Add(ittapi_project
     PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/deps
     DOWNLOAD_NAME ittapi_v3.24.0.tar.gz
@@ -13,7 +8,7 @@ ExternalProject_Add(ittapi_project
     URL_HASH SHA256=f230c07da3ea5063227794a7426d676829a2eb1890012a365ec27abda79d63ba
     BUILD_IN_SOURCE 1
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DITT_API_FORTRAN_SUPPORT=OFF
-    INSTALL_COMMAND ${ITT_INSTALL_COMMAND}
+    INSTALL_COMMAND ""
     LOG_CONFIGURE 1
     LOG_DOWNLOAD 1
     LOG_UPDATE 1
@@ -23,7 +18,6 @@ ExternalProject_Add(ittapi_project
 )
 
 ExternalProject_Get_Property(ittapi_project SOURCE_DIR)
-
 set(ITTAPI_INCLUDE_DIR ${SOURCE_DIR}/include)
 set(ITTAPI_LIBRARY ${SOURCE_DIR}/bin/libittnotify.a)
 file(MAKE_DIRECTORY ${ITTAPI_INCLUDE_DIR})  # Must exist.
@@ -31,6 +25,9 @@ file(MAKE_DIRECTORY ${ITTAPI_INCLUDE_DIR})  # Must exist.
 if(WITH_VTUNE_ITT)
     add_library(ittapi IMPORTED STATIC)
     set_property(TARGET ittapi PROPERTY IMPORTED_LOCATION ${ITTAPI_LIBRARY})
+    add_custom_command(TARGET ittapi_project POST_BUILD
+        COMMAND ${CMAKE_OBJCOPY} --weaken ${ITTAPI_LIBRARY}
+        DEPENDS ittapi_project)
 else()
     add_library(ittapi INTERFACE IMPORTED GLOBAL)
 endif()
