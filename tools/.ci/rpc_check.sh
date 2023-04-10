fdbc1f001fa395576224384844fca6f2bfe010a0
wenlinlee <1574249665@qq.com>
2023-04-10 18:35:33 +0800

    update 1

--- tools/.ci/rpc_check.sh
+++ tools/.ci/rpc_check.sh
@@ -43,8 +43,19 @@ start_node()
     fi
 }
 
-
+install_postmanCLI()
+{
+    cd ${current_path}
+    LOG_INFO " >>>>>>>>>> download postmanCLI  <<<<<<<<<<<<"
+    curl -o- "https://dl-cli.pstmn.io/install/osx_64.sh" | sh
+    LOG_INFO " >>>>>>>>>> login postmanCLI <<<<<<<<"
+    postman login --with-api-key PMAK-6433dcbdbbfd385fce4a624a-b5c1caca9d6eb6d092f66996d2d90cdb5c
+    LOG_INFO ">>>>>>>>>>> Run API tests  <<<<<<<<<<<<<<"
+    postman collection run "26671222-41a40221-e907-4286-91fb-a6c100cff181" -e "26671222-0b39412b-11d6-4a0a-8d6b-75f99851e352"
+}
 
 # non-sm test
 LOG_INFO "======== FISCOBCOS 3.0 RPC API check ========"
 start_node
+install_postmanCLI
+stop_node
