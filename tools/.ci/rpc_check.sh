c62677227bee35fad2794bfde843daf056751416
wenlinlee <1574249665@qq.com>
2023-04-11 14:38:01 +0800

    upload report 2

--- tools/.ci/rpc_check.sh
+++ tools/.ci/rpc_check.sh
@@ -52,13 +52,8 @@ open_disablessl(){
 rpc_apiTest()
 {
     cd ${current_path}
-    LOG_INFO " >>>>>>>>>> download postmanCLI  <<<<<<<<<<<<"
-    curl -o- "https://dl-cli.pstmn.io/install/osx_64.sh" | sh
-    LOG_INFO " >>>>>>>>>> login postmanCLI <<<<<<<<"
-    postman login --with-api-key PMAK-6433dcbdbbfd385fce4a624a-b5c1caca9d6eb6d092f66996d2d90cdb5c
     LOG_INFO ">>>>>>>>>>> Run API tests  <<<<<<<<<<<<<<"
-    #postman collection run "26671222-41a40221-e907-4286-91fb-a6c100cff181" -e "26671222-0b39412b-11d6-4a0a-8d6b-75f99851e352"
-    newman run 26671222-41a40221-e907-4286-91fb-a6c100cff181 -e 26671222-0b39412b-11d6-4a0a-8d6b-75f99851e352 --reporters junit --reporter-junit-export test-report.xml
+    newman run fiscobcos.rpcapi.collection.json --reporters junit --reporter-junit-export rpcapitest-report.xml
 }
 
 open_disablessl
