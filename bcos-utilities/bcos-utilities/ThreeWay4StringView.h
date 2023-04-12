f18eb38a6b1ceffbc71d83324e92bb826e5cd4d9
monan <651932351@qq.com>
2023-04-12 12:46:05 +0800

    Update macos threeway comapre

--- bcos-utilities/bcos-utilities/ThreeWay4StringView.h
+++ bcos-utilities/bcos-utilities/ThreeWay4StringView.h
@@ -1,17 +1,13 @@
 #pragma once
 
 #ifdef __APPLE__
-#include "Ranges.h"
-#include <compare>
 #include <string_view>
 
 namespace std
 {
 inline strong_ordering operator<=>(const string_view& lhs, const string_view& rhs)
 {
-    std::string lhsString(lhs);
-    std::string rhsString(rhs);
-    return lhsString <=> rhsString;
+    return (lhs.compare(rhs)) <=> 0;
 }
 }  // namespace std
 
