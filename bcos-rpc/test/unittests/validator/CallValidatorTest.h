e969ea607127dbf6e0afa722dbe60abf6524ac96
kyonRay <rui.kwok@outlook.com>
2023-04-18 15:34:37 +0800

    <feat>(rpc): add call with sign interface.

---
+++ bcos-rpc/test/unittests/validator/CallValidatorTest.h
@@ -0,0 +1,30 @@
+/**
+ *  Copyright (C) 2022 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * @file CallValidatorTest.h
+ * @author: kyonGuo
+ * @date 2023/4/18
+ */
+
+#ifndef FISCO_BCOS_CALLVALIDATORTEST_H
+#define FISCO_BCOS_CALLVALIDATORTEST_H
+
+
+class CallValidatorTest
+{
+};
+
+
+#endif  // FISCO_BCOS_CALLVALIDATORTEST_H
