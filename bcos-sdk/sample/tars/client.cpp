2186e9f7333785ad93f10a542e6b2ecb76482381
morebtcg <651932351@qq.com>
2023-06-08 19:02:00 +0800

    Update tars client

--- bcos-sdk/sample/tars/client.cpp
+++ bcos-sdk/sample/tars/client.cpp
@@ -2,8 +2,12 @@
 
 int main(int argc, char* argv[])
 {
-    // std::string_view connectionString = "";
-    // bcos::sdk::RPCClient rpcClient;
+    std::string_view connectionString;
+    bcos::sdk::RPCClient rpcClient(std::string{connectionString});
+
+    // Deploy contract
+
+    // rpcClient.sendTransaction(const bcos::protocol::Transaction &transaction)
 
     return 0;
 }
