2436823e0d3efb682ba00996b2b3692b05add2c9
kyonRay <rui.kwok@outlook.com>
2023-06-15 14:55:11 +0800

    <perf>(project): fix coverage, add coverage filter.

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -16,15 +16,18 @@
 # File: Coverage.cmake
 # Function: Define coverage related functions
 # ------------------------------------------------------------------------------
-# REMOVE_FILE_PATTERN eg.: '/usr*' '${CMAKE_SOURCE_DIR}/deps**' '${CMAKE_SOURCE_DIR}/evmc*' ‘${CMAKE_SOURCE_DIR}/fisco-bcos*’
+# REMOVE_FILE_PATTERN eg.: '/usr*' '${CMAKE_SOURCE_DIR}/deps**' '${CMAKE_SOURCE_DIR}/evmc*' '${CMAKE_SOURCE_DIR}/fisco-bcos*'
 function(config_coverage TARGET REMOVE_FILE_PATTERN)
     find_program(LCOV_TOOL lcov)
     message(STATUS "lcov tool: ${LCOV_TOOL}")
     if (LCOV_TOOL)
+        message(STATUS "coverage dir: " ${CMAKE_BINARY_DIR})
+        message(STATUS "coverage TARGET: " ${TARGET})
+        message(STATUS "coverage REMOVE_FILE_PATTERN: " ${REMOVE_FILE_PATTERN})
         add_custom_target(${TARGET}
-            COMMAND ${LCOV_TOOL} -o ${CMAKE_BINARY_DIR}/coverage.info.in -c -d ${CMAKE_BINARY_DIR}/
-            COMMAND ${LCOV_TOOL} -r ${CMAKE_BINARY_DIR}/coverage.info.in ${REMOVE_FILE_PATTERN} -o ${CMAKE_BINARY_DIR}/coverage.info
-            COMMAND genhtml -q -o ${CMAKE_BINARY_DIR}/CodeCoverage ${CMAKE_BINARY_DIR}/coverage.info)
+            COMMAND ${LCOV_TOOL} --rc lcov_branch_coverage=1 -o ${CMAKE_BINARY_DIR}/coverage.info.in -c -d ${CMAKE_BINARY_DIR}/
+            COMMAND ${LCOV_TOOL} --rc lcov_branch_coverage=1 -r ${CMAKE_BINARY_DIR}/coverage.info.in '*MacOS*' '/usr*' 'boost/*' '*test*' '*build*' '*deps*' ${REMOVE_FILE_PATTERN} -o ${CMAKE_BINARY_DIR}/coverage.info
+            COMMAND genhtml --rc lcov_branch_coverage=1 -q -o ${CMAKE_BINARY_DIR}/CodeCoverage ${CMAKE_BINARY_DIR}/coverage.info)
     else ()
         message(FATAL_ERROR "Can't find lcov tool. Please install lcov")
     endif()
