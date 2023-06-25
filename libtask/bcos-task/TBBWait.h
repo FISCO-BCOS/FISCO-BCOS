53fa59ad1b4441a1c4a4abe789d30cdcbdc5303b
MO NAN <651932351@qq.com>
2023-06-25 10:25:33 +0800

    Add ut for libtask (#3711)

--- libtask/bcos-task/TBBWait.h
+++ libtask/bcos-task/TBBWait.h
@@ -4,6 +4,7 @@
 #include "Trait.h"
 #include <oneapi/tbb/task.h>
 #include <exception>
+#include <mutex>
 #include <optional>
 #include <type_traits>
 #include <variant>
@@ -21,28 +22,39 @@ auto syncWait(auto&& task) -> AwaitableReturnType<std::remove_cvref_t<decltype(t
         std::variant<std::monostate, ReturnType, std::exception_ptr>>
         value;
 
-    oneapi::tbb::task::suspend([&](oneapi::tbb::task::suspend_point tag) {
-        auto waitTask = [](Task&& task, decltype(value)& value,
-                            oneapi::tbb::task::suspend_point tag) -> task::Task<void> {
-            try
-            {
-                if constexpr (std::is_void_v<typename Task::ReturnType>)
-                {
-                    co_await task;
-                }
-                else
-                {
-                    value.template emplace<ReturnType>(co_await task);
-                }
-            }
-            catch (...)
-            {
-                value.template emplace<std::exception_ptr>(std::current_exception());
-            }
-            oneapi::tbb::task::resume(tag);
-        }(std::forward<Task>(task), value, tag);
-        waitTask.start();
-    });
+    std::mutex tagMutex;
+    std::atomic_bool finished = false;
+    std::atomic<oneapi::tbb::task::suspend_point> tbbTag;
+    auto tbbTask = [](Task&& task, decltype(value)& value, std::mutex& tagMutex,
+                       std::atomic_bool& finished,
+                       std::atomic<oneapi::tbb::task::suspend_point>& tbbTag) -> task::Task<void> {
+        if constexpr (std::is_void_v<ReturnType>)
+        {
+            co_await task;
+        }
+        else
+        {
+            value.template emplace<ReturnType>(co_await task);
+        }
+
+        std::unique_lock<std::mutex> lock(tagMutex);
+        finished = true;
+        if (tbbTag)
+        {
+            lock.unlock();
+            oneapi::tbb::task::resume(tbbTag);
+        }
+    }(std::forward<Task>(task), value, tagMutex, finished, tbbTag);
+
+    tbbTask.start();
+    std::unique_lock<std::mutex> lock(tagMutex);
+    if (!finished)
+    {
+        oneapi::tbb::task::suspend([&](oneapi::tbb::task::suspend_point tag) {
+            tbbTag = tag;
+            lock.unlock();
+        });
+    }
 
     if (std::holds_alternative<std::exception_ptr>(value))
     {
@@ -53,6 +65,6 @@ auto syncWait(auto&& task) -> AwaitableReturnType<std::remove_cvref_t<decltype(t
     {
         return std::move(std::get<ReturnType>(value));
     }
-}
+};
 
 }  // namespace bcos::task::tbb
