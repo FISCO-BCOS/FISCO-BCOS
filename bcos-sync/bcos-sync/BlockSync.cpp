704f5870f3f4382aa144d041f0411832a643d1aa
lucasli <410567249@qq.com>
2023-07-11 10:28:42 +0800

    observer node don't print error log

--- bcos-sync/bcos-sync/BlockSync.cpp
+++ bcos-sync/bcos-sync/BlockSync.cpp
@@ -190,19 +190,37 @@ void BlockSync::printBehindPeers()
 
     auto knownHighestNumber = m_config->knownHighestNumber();
     auto blockNumThreshold = m_blockNumThreshold;
-    m_syncStatus->foreachPeer([knownHighestNumber, blockNumThreshold](PeerStatus::Ptr _p) {
-        if (std::abs(_p->number() - knownHighestNumber) > blockNumThreshold)
+    auto observerNodeList = m_config->observerNodeList();
+    for (auto observerNode : observerNodeList)
+    {
+        if (m_config->nodeID()->hex() == observerNode->nodeID()->hex())
         {
-            BLKSYNC_LOG(ERROR) << "The lowest blocknumber is too far behind the highest block "
-                                  "number queried by getSyncStatus, "
-                               << "[highest block number: " << knownHighestNumber
-                               << ", current nodeid: " << _p->nodeId()->shortHex()
-                               << ", current block number: " << _p->number() << "]";
+            return;
         }
-        return true;
-    });
+    }
 
-    lastLogUtcTime = utcTime();
+    m_syncStatus->foreachPeer(
+        [knownHighestNumber, blockNumThreshold, observerNodeList](PeerStatus::Ptr _p) {
+            for (auto observerNode : observerNodeList)
+            {
+                // observer node don't need to print error log
+                if (_p->nodeId()->hex() == observerNode->nodeID()->hex())
+                {
+                    return true;
+                }
+            }
+
+            if (std::abs(_p->number() - knownHighestNumber) > blockNumThreshold)
+            {
+                BLKSYNC_LOG(ERROR) << "The lowest blocknumber is too far behind the highest block "
+                                      "number queried by getSyncStatus, "
+                                   << "[highest block number: " << knownHighestNumber
+                                   << ", current nodeid: " << _p->nodeId()->shortHex()
+                                   << ", current block number: " << _p->number() << "]";
+                lastLogUtcTime = utcTime();
+            }
+            return true;
+        });
 }
 
 void BlockSync::executeWorker()
