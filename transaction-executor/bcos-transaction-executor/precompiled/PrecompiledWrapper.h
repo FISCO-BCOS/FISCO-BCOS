4871d635f86649239383bd9a52a3dbd284710dc5
MO NAN <651932351@qq.com>
2023-07-12 21:19:57 +0800

    Add PrecompileWrapper

---
+++ transaction-executor/bcos-transaction-executor/precompiled/PrecompiledWrapper.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "bcos-utilities/FixedBytes.h"
+namespace bcos::transaction_executor
+{
+
+class PrecompiledWrapper
+{
+private:
+    PrecompiledMap m_precompiledMap;
+};
+
+}  // namespace bcos::transaction_executor
