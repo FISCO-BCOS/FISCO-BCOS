2cbd76988d9e6f538f302894cdd42a1c98021e4c
莫楠 <651932351@qq.com>
2023-08-03 17:10:25 +0800

    Remove useless helper function

--- bcos-sdk/SWIG/Helper.cpp
+++ bcos-sdk/SWIG/Helper.cpp
@@ -1,10 +1,5 @@
 #include "Helper.h"
 
-bcos::bytesConstRef bcos::sdk::swig::toBytesConstRef(char* STRING, size_t LENGTH)
-{
-    return {(const bcos::byte*)STRING, LENGTH};
-}
-
 bcos::bytes bcos::sdk::swig::toBytes(char* STRING, size_t LENGTH)
 {
     return {(const bcos::byte*)STRING, (const bcos::byte*)STRING + LENGTH};
