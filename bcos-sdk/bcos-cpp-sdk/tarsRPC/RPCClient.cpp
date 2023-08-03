6247496734b44e76a72f7c9d91a2ded0970cdc6a
莫楠 <651932351@qq.com>
2023-08-03 08:49:02 +0800

    Add connectionString generate logic

--- bcos-sdk/bcos-cpp-sdk/tarsRPC/RPCClient.cpp
+++ bcos-sdk/bcos-cpp-sdk/tarsRPC/RPCClient.cpp
@@ -1,6 +1,33 @@
 #include "RPCClient.h"
 #include "bcos-tars-protocol/protocol/TransactionImpl.h"
+#include "bcos-utilities/Exceptions.h"
 #include <boost/throw_exception.hpp>
+
+struct InvalidHostPortStringError : public bcos::Exception
+{
+};
+
+bcostars::RPCPrx& bcos::sdk::RPCClient::rpcProxy()
+{
+    return m_rpcProxy;
+}
+std::string bcos::sdk::RPCClient::toConnectionString(const std::vector<std::string>& hostAndPorts)
+{
+    std::string connectionString = "fiscobcos.rpc.RPCObj@";
+    std::vector<std::string> items;
+    for (auto const& host : hostAndPorts)
+    {
+        items.clear();
+        boost::split(items, host, boost::is_any_of(":"), boost::token_compress_on);
+
+        if (items.size() < 2)
+        {
+            BOOST_THROW_EXCEPTION(InvalidHostPortStringError{});
+        }
+        connectionString += "tcp -h " + items[0] + " -p " + items[1] + " -t 60000:";
+    }
+    return connectionString.substr(0, connectionString.size() - 1);
+}
 
 void bcos::sdk::RPCClient::onMessage(tars::ReqMessagePtr message)
 {
@@ -12,7 +39,6 @@ void bcos::sdk::RPCClient::onMessage(tars::ReqMessagePtr message)
         callbackBase.callback()->onMessage();
     }
 }
-
 bcos::sdk::RPCClient::RPCClient(const std::string& connectionString)
 {
     constexpr static int timeout = 60000;
@@ -22,7 +48,7 @@ bcos::sdk::RPCClient::RPCClient(const std::string& connectionString)
     m_rpcProxy->tars_async_timeout(timeout);
 }
 
-bcos::sdk::SendTransaction::SendTransaction(RPCClient& rpcClient) : Handle(rpcClient){};
+bcos::sdk::SendTransaction::SendTransaction(RPCClient& rpcClient) : Handle(rpcClient) {}
 bcos::sdk::SendTransaction& bcos::sdk::SendTransaction::send(
     const bcos::protocol::Transaction& transaction)
 {
@@ -38,7 +64,7 @@ bcos::sdk::SendTransaction& bcos::sdk::SendTransaction::send(
     return *this;
 }
 
-bcos::sdk::BlockNumber::BlockNumber(RPCClient& rpcClient) : Handle(rpcClient){};
+bcos::sdk::BlockNumber::BlockNumber(RPCClient& rpcClient) : Handle(rpcClient) {}
 bcos::sdk::BlockNumber& bcos::sdk::BlockNumber::send()
 {
     std::promise<tars::ReqMessagePtr> promise;
