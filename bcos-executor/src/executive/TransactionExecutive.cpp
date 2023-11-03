5e0a9378867bb4dd5796ef23c37953b167f53acd
Mojicode <lascion@163.com>
2023-11-03 18:57:33 +0800

    add TransactionExecutiveSerial to support serial tx exec

--- bcos-executor/src/executive/TransactionExecutive.cpp
+++ bcos-executor/src/executive/TransactionExecutive.cpp
@@ -74,68 +74,12 @@ CallParameters::UniquePtr TransactionExecutive::start(CallParameters::UniquePtr 
     EXECUTIVE_LOG(TRACE) << "Execute start\t" << input->toFullString();
 
     auto& callParameters = input;
-    uint64_t currentSeq = input->seq;
 
     auto message = execute(std::move(callParameters));
 
     message->contextID = contextID();
     message->seq = seq();
     message->hasContractTableChanged = m_hasContractTableChanged;
-
-    if (currentSeq == 0)
-    {
-        CallParameters::UniquePtr callParam4AccountPre = 
-            std::make_unique<CallParameters>(CallParameters::MESSAGE);
-        auto codec = CodecWrapper(m_blockContext.hashHandler(), m_blockContext.isWasm());
-        bytes getBalanceIn = codec.encodeWithSig("getAccountBalance()");
-        callParam4AccountPre->data = getBalanceIn;
-        callParam4AccountPre->senderAddress = input->senderAddress;
-        callParam4AccountPre->receiveAddress = ACCOUNT_ADDRESS;
-        auto balanceRet = callPrecompiled(std::move(callParam4AccountPre));
-        if(balanceRet->type == CallParameters::REVERT)
-        {
-            message->type = balanceRet->type;
-            message->status = balanceRet->status;
-            message->message = balanceRet->message;
-            return message;
-        }
-        s256 balance;
-        codec.decode(ref(balanceRet->data), balance);
-
-        CallParameters::UniquePtr callParam4SystemConfig = 
-            std::make_unique<CallParameters>(CallParameters::MESSAGE);
-        bytes gasPriceInput = codec.encodeWithSig("getValueByKey(string)", std::string("gasPrice"));
-        callParam4SystemConfig->data = gasPriceInput;
-        callParam4SystemConfig->senderAddress = input->senderAddress;
-        callParam4SystemConfig->receiveAddress = SYS_CONFIG_ADDRESS;
-        auto gasPriceRet = callPrecompiled(std::move(callParam4SystemConfig));
-        if(gasPriceRet->type == CallParameters::REVERT)
-        {
-            message->type = gasPriceRet->type;
-            message->status = gasPriceRet->status;
-            message->message = gasPriceRet->message;
-            return message;
-        }
-
-        s256 gasPrice;
-        codec.decode(ref(gasPriceRet->data), gasPrice);
-
-        if(balance < message->gas * gasPrice)
-        {
-            revert();
-        }
-
-        bytes subBalanceIn = codec.encodeWithSig("subAccountBalance(uint256)", message->gas * gasPrice);
-        callParam4AccountPre->data = subBalanceIn;
-        auto subBalanceRet = callPrecompiled(std::move(callParam4AccountPre));
-        if(subBalanceRet->type == CallParameters::REVERT)
-        {
-            message->type = subBalanceRet->type;
-            message->status = subBalanceRet->status;
-            message->message = subBalanceRet->message;
-            return message;
-        }
-    }
 
     EXECUTIVE_LOG(TRACE) << "Execute finish\t" << message->toFullString();
 
