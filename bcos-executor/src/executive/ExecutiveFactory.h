02a9f61f1074d9875787c6ee6dd2d37a2b1bab53
Mojicode <lascion@163.com>
2023-11-07 09:47:41 +0800

    add default params for executive building

--- bcos-executor/src/executive/ExecutiveFactory.h
+++ bcos-executor/src/executive/ExecutiveFactory.h
@@ -62,7 +62,7 @@ public:
 //    virtual std::shared_ptr<TransactionExecutive> build(const std::string& _contractAddress,
 //        int64_t contextID, int64_t seq, bool useCoroutine = true);
     virtual std::shared_ptr<TransactionExecutive> build(const std::string& _contractAddress,
-        int64_t contextID, int64_t seq, ExecutiveType execType);
+        int64_t contextID, int64_t seq, ExecutiveType execType = ExecutiveType::coroutine);
     const BlockContext& getBlockContext() { return m_blockContext; };
 
     std::shared_ptr<precompiled::Precompiled> getPrecompiled(const std::string& address) const;
@@ -98,7 +98,7 @@ public:
     ~ShardingExecutiveFactory() override = default;
 
     std::shared_ptr<TransactionExecutive> build(const std::string& _contractAddress,
-        int64_t contextID, int64_t seq, ExecutiveType execType) override;
+        int64_t contextID, int64_t seq, ExecutiveType execType = ExecutiveType::coroutine) override;
 };
 
 }  // namespace bcos::executor
