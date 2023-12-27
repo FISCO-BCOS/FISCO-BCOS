03419eb1863ed67dd07332714e05b5851608dcca
莫楠 <651932351@qq.com>
2023-12-27 10:56:45 +0800

    Remove usage of std::stack in vminstance

--- transaction-executor/bcos-transaction-executor/vm/VMInstance.cpp
+++ transaction-executor/bcos-transaction-executor/vm/VMInstance.cpp
@@ -10,16 +10,11 @@ bcos::transaction_executor::EVMCResult bcos::transaction_executor::VMInstance::e
     const evmc_message* msg, const uint8_t* code, size_t codeSize)
 {
     static auto const* evm = evmc_create_evmone();
-    static thread_local std::stack<std::unique_ptr<evmone::advanced::AdvancedExecutionState>>
-        executionStates;
+    static thread_local std::unique_ptr<evmone::advanced::AdvancedExecutionState>
+        localExecutionState;
 
-    std::unique_ptr<evmone::advanced::AdvancedExecutionState> executionState;
-    if (!executionStates.empty())
-    {
-        executionState = std::move(executionStates.top());
-        executionStates.pop();
-    }
-    else
+    auto executionState = std::move(localExecutionState);
+    if (!executionState)
     {
         executionState = std::make_unique<evmone::advanced::AdvancedExecutionState>();
     }
@@ -28,7 +23,11 @@ bcos::transaction_executor::EVMCResult bcos::transaction_executor::VMInstance::e
         *msg, rev, *host, context, std::basic_string_view<uint8_t>(code, codeSize));
     auto result = EVMCResult(evmone::baseline::execute(
         *static_cast<evmone::VM const*>(evm), msg->gas, *executionState, *m_instance));
-    executionStates.push(std::move(executionState));
+
+    if (!localExecutionState)
+    {
+        localExecutionState = std::move(executionState);
+    }
     return result;
 }
 
