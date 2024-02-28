0de50b687c5632c54d15686c111fe33730dea543
wenlinli <1574249665@qq.com>
2024-02-28 16:34:53 +0800

    add vcpkg_update.sh

---
+++ tools/.ci/vcpkg_update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+current_path=`pwd`
+old_baseline_path="./tools/.ci/old_baseline.txt"
+
+# 从vcpkg-configuration.json文件中提取当前的baseline值
+current_baseline=$(awk -F'"' '/"baseline":/{print $4}' vcpkg-configuration.json)
+echo "current_baseline: ${current_baseline}"
+
+# 在脚本中记录的旧baseline值
+old_baseline=$(<"$old_baseline_path")
+echo "old_baseline: ${old_baseline}"
+
+# 比较当前baseline和旧baseline是否一致
+if [ "$current_baseline" = "$old_baseline" ]; then
+    echo "baseline not changed, vcpkg not need update..."
+    exit 0
+else
+    echo "Baseline has changed, need ./bootstrap-vcpkg.sh ..."
+    cd vcpkg  && git fetch origin master && ./bootstrap-vcpkg.sh && cd ..
+    echo "Update current_baseline to  old_baseline.txt..."
+    echo "$current_baseline" > "$old_baseline_path"
+fi
