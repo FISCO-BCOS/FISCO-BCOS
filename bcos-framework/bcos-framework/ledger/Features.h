28e5d07123e0f9c3a5ace475b3738e723ba42276
jimmyshi <417711026@qq.com>
2024-03-15 15:58:25 +0800

    <fix>(execute): Fix uppercase addr bug to support pass eip55 addr && no  need to fetch gasPrice every block (#4297)

--- bcos-framework/bcos-framework/ledger/Features.h
+++ bcos-framework/bcos-framework/ledger/Features.h
@@ -40,6 +40,7 @@ public:
         bugfix_internal_create_permission_denied,
         bugfix_sharding_call_in_child_executive,
         bugfix_empty_abi_reset,  // support empty abi reset of same code
+        bugfix_eip55_addr,
         feature_dmc2serial,
         feature_sharding,
         feature_rpbft,
@@ -149,7 +150,8 @@ public:
                 {Flag::bugfix_keypage_system_entry_hash,
                     Flag::bugfix_internal_create_redundant_storage}},
             {protocol::BlockVersion::V3_7_0_VERSION,
-                {Flag::bugfix_empty_abi_reset, Flag::bugfix_sharding_call_in_child_executive,
+                {Flag::bugfix_empty_abi_reset, Flag::bugfix_eip55_addr,
+                    Flag::bugfix_sharding_call_in_child_executive,
                     Flag::bugfix_internal_create_permission_denied}},
         });
         for (const auto& upgradeFeatures : upgradeRoadmap)
