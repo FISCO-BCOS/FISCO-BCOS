d114b66e62888955abef14e60b5e569a8c22e74e
wenlinli <1574249665@qq.com>
2024-03-28 16:28:07 +0800

    add transient rollback

--- bcos-executor/src/executive/TransactionExecutive.h
+++ bcos-executor/src/executive/TransactionExecutive.h
@@ -221,6 +221,7 @@ protected:
     std::shared_ptr<std::map<std::string, std::shared_ptr<PrecompiledContract>>> m_evmPrecompiled;
     std::shared_ptr<PrecompiledMap> m_precompiled;
     std::shared_ptr<const std::set<std::string>> m_staticPrecompiled;
+    std::shared_ptr<storage::StateStorageInterface> m_transientStateStorage;
 
     std::string m_contractAddress;
     int64_t m_contextID;
