4e2d82c0c8942eb1f501169447da610ac5f705bd
monan <651932351@qq.com>
2024-05-01 00:42:36 +0800

    Fix format problem

--- bcos-codec/bcos-codec/wrapper/CodecWrapper.h
+++ bcos-codec/bcos-codec/wrapper/CodecWrapper.h
@@ -48,11 +48,10 @@ public:
             codec::abi::ContractABICodec abi(*m_hash);
             return abi.abiIn("", _args...);
         }
-        
-                    codec::scale::ScaleEncoderStream s;
-            (s << ... << std::forward<Args>(_args));
-            return s.data();
-       
+
+        codec::scale::ScaleEncoderStream s;
+        (s << ... << std::forward<Args>(_args));
+        return s.data();
     }
     template <typename... Args>
     bytes encodeWithSig(const std::string& _sig, Args&&... _args) const
