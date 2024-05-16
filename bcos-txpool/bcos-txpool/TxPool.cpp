c2c55928b2b2309cd456305b3408dea5c70a466d
Kyon <32325790+kyonRay@users.noreply.github.com>
2024-05-16 16:11:12 +0800

    <fix>(txpool): fix proposal nonce check failed when same transaction in txpool. (#4448)

--- bcos-txpool/bcos-txpool/TxPool.cpp
+++ bcos-txpool/bcos-txpool/TxPool.cpp
@@ -556,6 +556,8 @@ void TxPool::init()
     // init syncConfig
     TXPOOL_LOG(INFO) << LOG_DESC("init sync config");
     auto txsSyncConfig = m_transactionSync->config();
+    txsSyncConfig->setConsensusNodeList(ledgerConfig->consensusNodeList());
+    txsSyncConfig->setObserverList(ledgerConfig->observerNodeList());
     m_transactionSync->config()->setMaxResponseTxsToNodesWithEmptyTxs(
         ledgerConfig->blockTxCountLimit());
     TXPOOL_LOG(INFO) << LOG_DESC("init sync config success");
