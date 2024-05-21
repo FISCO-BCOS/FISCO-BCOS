b51492f307fcb6e850bd243882229bec306d94d2
Kyon <32325790+kyonRay@users.noreply.github.com>
2024-05-21 17:02:38 +0800

    <fix>(feature,tools): fix feature 3.9.0 bugfix, update version to 3.9.0. (#4472)

--- tools/BcosAirBuilder/build_chain.sh
+++ tools/BcosAirBuilder/build_chain.sh
@@ -39,7 +39,7 @@ ca_dir=""
 prometheus_dir=""
 config_path=""
 docker_mode=
-default_version="v3.8.0"
+default_version="v3.9.0"
 compatibility_version=${default_version}
 default_mtail_version="3.0.0-rc49"
 compatibility_mtail_version=${default_mtail_version}
@@ -70,11 +70,11 @@ supported_consensus=(pbft rpbft)
 
 # for pro or max default setting
 bcos_builder_package=BcosBuilder.tgz
-bcos_builder_version=v3.8.0
+bcos_builder_version=v3.9.0
 use_exist_binary="false"
 download_specific_binary_flag="false"
 download_service_binary_type="cdn"
-service_binary_version="v3.8.0"
+service_binary_version="v3.9.0"
 download_service_binary_path="binary"
 download_service_binary_path_flag="false"
 service_type="all"
