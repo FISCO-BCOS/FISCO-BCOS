0002ce200020bcd3e0c375d73506be22dca8daf8
wenlinli <1574249665@qq.com>
2024-07-19 14:40:14 +0800

    Revert "update version"

--- tools/.ci/rpcapi_ci_prepare.sh
+++ tools/.ci/rpcapi_ci_prepare.sh
@@ -35,11 +35,12 @@ wait_and_start()
 
 init()
 {
+    sm_option="${1}"
     cd ${current_path}
     echo " ==> fisco-bcos version: "
     ${fisco_bcos_path} -v
     clear_node
-    bash ${build_chain_path} -p 30300,20200 -l "127.0.0.1:4" -e ${fisco_bcos_path}
+    bash ${build_chain_path} -l "127.0.0.1:4" -e ${fisco_bcos_path} "${sm_option}"
     open_disablessl
     cd ..
     wait_and_start
@@ -78,4 +79,4 @@ open_disablessl(){
 
 
 
-init
+init ""
