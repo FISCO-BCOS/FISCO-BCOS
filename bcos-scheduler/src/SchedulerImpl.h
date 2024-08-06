c06b1ebfb8d8a056d61e6b46eb6b82e58420d22e
monan <651932351@qq.com>
2024-08-06 22:55:11 +0800

    Add maybe_unused for macos compile

--- bcos-scheduler/src/SchedulerImpl.h
+++ bcos-scheduler/src/SchedulerImpl.h
@@ -218,9 +218,9 @@ private:
     bcos::txpool::TxPoolInterface::Ptr m_txPool;
     bcos::protocol::TransactionSubmitResultFactory::Ptr m_transactionSubmitResultFactory;
     bcos::crypto::Hash::Ptr m_hashImpl;
-    bool m_isAuthCheck = false;
+    [[maybe_unused]] bool m_isAuthCheck = false;
     bool m_isWasm = false;
-    bool m_isSerialExecute = false;
+    [[maybe_unused]] bool m_isSerialExecute = false;
 
     std::function<void(protocol::BlockNumber blockNumber)> m_blockNumberReceiver;
     std::function<void(bcos::protocol::BlockNumber, bcos::protocol::TransactionSubmitResultsPtr,
