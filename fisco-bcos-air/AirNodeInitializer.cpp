3e381984fa63e03e057350c9013f6fe563a43eaf
monan <651932351@qq.com>
2024-08-08 16:13:55 +0800

    Move baseline init to standalone target

--- fisco-bcos-air/AirNodeInitializer.cpp
+++ fisco-bcos-air/AirNodeInitializer.cpp
@@ -19,10 +19,7 @@
  * @date 2021-10-28
  */
 #include "AirNodeInitializer.h"
-#include "bcos-gateway/libnetwork/Session.h"
-#include "bcos-gateway/libnetwork/Socket.h"
-#include "bcos-gateway/libp2p/P2PMessageV2.h"
-#include "bcos-utilities/ratelimiter/DistributedRateLimiter.h"
+#include "bcos-boostssl/bcos-boostssl/websocket/WsMessage.h"
 #include <bcos-crypto/signature/key/KeyFactoryImpl.h>
 #include <bcos-framework/protocol/GlobalConfig.h>
 #include <bcos-gateway/GatewayFactory.h>
