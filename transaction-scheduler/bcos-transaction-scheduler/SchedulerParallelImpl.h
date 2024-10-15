f2d0b2253c02e1d9aa24c1bf60272d4d721123df
monan <651932351@qq.com>
2024-10-15 15:47:39 +0800

    Move init into pipeline

--- transaction-scheduler/bcos-transaction-scheduler/SchedulerParallelImpl.h
+++ transaction-scheduler/bcos-transaction-scheduler/SchedulerParallelImpl.h
@@ -107,6 +107,7 @@ public:
                     << " transactions";
                 break;
             }
+            context.init(index, *context.transaction, *context.receipt);
 
             context.coro.emplace(transaction_executor::execute3Step(m_executor.get(),
                 m_readWriteSetStorage, blockHeader, *context.transaction, context.contextID,
@@ -349,11 +350,6 @@ task::Task<std::vector<protocol::TransactionReceipt::Ptr>> tag_invoke(
         ledger::LedgerConfig const&, task::tbb::SyncWait>;
 
     std::vector<ExecutionContext<CoroType>> contexts(transactionCount);
-    for (auto&& [index, context] : RANGES::views::enumerate(contexts))
-    {
-        context.init(index, transactions[index], receipts[index]);
-    }
-
     tbb::task_arena arena(
         tbb::task_arena::constraints{}.set_max_concurrency(scheduler.m_maxConcurrency));
     arena.execute([&]() {
