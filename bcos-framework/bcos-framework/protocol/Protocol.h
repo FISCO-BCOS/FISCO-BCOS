9b43596f70ddbeb9909a74e291020596419ae4d5
monan <651932351@qq.com>
2025-04-13 10:23:21 +0800

    Upgrade version to 3.15.2

--- bcos-framework/bcos-framework/protocol/Protocol.h
+++ bcos-framework/bcos-framework/protocol/Protocol.h
@@ -115,6 +115,7 @@ enum ProtocolVersion : uint32_t
 
 enum class BlockVersion : uint32_t
 {
+    V3_15_2_VERSION = 0x030f0200,  // 3.15.2
     V3_15_1_VERSION = 0x030f0100,  // 3.15.1
     V3_15_0_VERSION = 0x030f0000,  // 3.15.0
     V3_14_0_VERSION = 0x030e0000,  // 3.14.0
@@ -151,7 +152,7 @@ enum class BlockVersion : uint32_t
     V3_0_VERSION = 0x03000000,
     RC4_VERSION = 4,
     MIN_VERSION = RC4_VERSION,
-    MAX_VERSION = V3_15_1_VERSION,  // 3.15.1
+    MAX_VERSION = V3_15_2_VERSION,  // 3.15.2
 };
 
 enum class TransactionVersion : uint32_t
@@ -166,7 +167,7 @@ const std::string RC4_VERSION_STR = "3.0.0-rc4";
 const std::string RC_VERSION_PREFIX = "3.0.0-rc";
 const std::string V3_9_VERSION_STR = "3.9.0";
 
-constexpr BlockVersion DEFAULT_VERSION = bcos::protocol::BlockVersion::V3_15_1_VERSION;  // 3.15.1
+constexpr BlockVersion DEFAULT_VERSION = bcos::protocol::BlockVersion::V3_15_2_VERSION;  // 3.15.2
 const std::string DEFAULT_VERSION_STR = V3_9_VERSION_STR;
 constexpr uint8_t MAX_MAJOR_VERSION = std::numeric_limits<uint8_t>::max();
 constexpr uint8_t MIN_MAJOR_VERSION = 3;
