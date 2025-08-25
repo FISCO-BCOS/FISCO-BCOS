2d812b3f24adfcbd7b8413c233b8918f6d92a40e
monan <651932351@qq.com>
2025-08-25 14:26:00 +0800

    Use new transactions and receipts method

---
+++ bcos-rpc/bcos-rpc/web3jsonrpc/model/BlockResponse.cpp
@@ -0,0 +1,78 @@
+#include "BlockResponse.h"
+void bcos::rpc::combineBlockResponse(
+    Json::Value& result, bcos::protocol::Block::Ptr&& block, bool fullTxs)
+{
+    result["number"] = toQuantity(block->blockHeader()->number());
+    result["hash"] = block->blockHeader()->hash().hexPrefixed();
+    // Only one parent block in BCOS. It is empty for genesis block
+    for (const auto& info : block->blockHeader()->parentInfo())
+    {
+        result["parentHash"] = info.blockHash.hexPrefixed();
+    }
+    result["nonce"] = "0x0000000000000000";
+    // empty uncle hash: keccak256(RLP([]))
+    result["sha3Uncles"] = "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347";
+    rpc::Logs logs;
+    for (auto receipt : block->receipts())
+    {
+        for (auto const& log : receipt->logEntries())
+        {
+            rpc::Log logObj{
+                .address = bcos::bytes(log.address().begin(), log.address().end()),
+                .topics = bcos::h256s(log.topics().begin(), log.topics().end()),
+                .data = bcos::bytes(log.data().begin(), log.data().end()),
+            };
+            logs.push_back(std::move(logObj));
+        }
+    }
+    auto logsBloom = getLogsBloom(logs);
+    result["logsBloom"] = toHexStringWithPrefix(logsBloom);
+    result["transactionsRoot"] = block->blockHeader()->txsRoot().hexPrefixed();
+    result["stateRoot"] = block->blockHeader()->stateRoot().hexPrefixed();
+    result["receiptsRoot"] = block->blockHeader()->receiptsRoot().hexPrefixed();
+    if (std::cmp_greater(block->blockHeader()->sealerList().size(), block->blockHeader()->sealer()))
+    {
+        auto pk = block->blockHeader()->sealerList()[block->blockHeader()->sealer()];
+        auto hash = crypto::keccak256Hash(bcos::ref(pk));
+        Address address = right160(hash);
+        auto addrString = address.hex();
+        auto addrHash = crypto::keccak256Hash(bytesConstRef(addrString)).hex();
+        toChecksumAddress(addrString, addrHash);
+        result["miner"] = "0x" + addrString;
+    }
+    // genesis block
+    if (block->blockHeader()->number() == 0)
+    {
+        result["miner"] = "0x0000000000000000000000000000000000000000";
+        result["parentHash"] = "0x0000000000000000000000000000000000000000000000000000000000000000";
+    }
+    result["difficulty"] = "0x0";
+    result["totalDifficulty"] = "0x0";
+    result["extraData"] = toHexStringWithPrefix(block->blockHeader()->extraData());
+    result["size"] = toQuantity(block->size());
+    // TODO: change it wen block gas limit apply
+    result["gasLimit"] = toQuantity(30000000ULL);
+    result["gasUsed"] = toQuantity((uint64_t)block->blockHeader()->gasUsed());
+    result["timestamp"] = toQuantity(block->blockHeader()->timestamp() / 1000);  // to seconds
+    if (fullTxs)
+    {
+        Json::Value txList = Json::arrayValue;
+        for (auto tx : block->transactions())
+        {
+            Json::Value txJson = Json::objectValue;
+            combineTxResponse(txJson, *tx, nullptr, block.get());
+            txList.append(txJson);
+        }
+        result["transactions"] = std::move(txList);
+    }
+    else
+    {
+        Json::Value txHashesList = Json::arrayValue;
+        for (size_t i = 0; i < block->transactionsHashSize(); i++)
+        {
+            txHashesList.append(block->transactionHash(i).hexPrefixed());
+        }
+        result["transactions"] = std::move(txHashesList);
+    }
+    result["uncles"] = Json::Value(Json::arrayValue);
+}
