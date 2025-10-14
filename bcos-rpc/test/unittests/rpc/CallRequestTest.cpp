7ca4f55fa51bcf47815f4865868f2f0652afd379
monan <651932351@qq.com>
2025-10-14 23:00:18 +0800

    Update check data

--- bcos-rpc/test/unittests/rpc/CallRequestTest.cpp
+++ bcos-rpc/test/unittests/rpc/CallRequestTest.cpp
@@ -20,30 +20,35 @@ BOOST_AUTO_TEST_CASE(decode_non_object_root)
     auto [ok, req] = decodeCallRequest(root);
 
     // Then decode should fail and request stays default-initialized
-    BOOST_CHECK(!ok);
-    BOOST_CHECK(req.to.empty());
-    BOOST_CHECK(req.data.empty());
-    BOOST_CHECK(!req.from.has_value());
-    BOOST_CHECK(!req.gas.has_value());
-    BOOST_CHECK(!req.gasPrice.has_value());
-    BOOST_CHECK(!req.value.has_value());
-    BOOST_CHECK(!req.maxFeePerGas.has_value());
-    BOOST_CHECK(!req.maxPriorityFeePerGas.has_value());
+    BOOST_TEST(!ok);
+    BOOST_TEST(req.to.empty());
+    BOOST_TEST(req.data.empty());
+    BOOST_TEST(!req.from.has_value());
+    BOOST_TEST(!req.gas.has_value());
+    BOOST_TEST(!req.gasPrice.has_value());
+    BOOST_TEST(!req.value.has_value());
+    BOOST_TEST(!req.maxFeePerGas.has_value());
+    BOOST_TEST(!req.maxPriorityFeePerGas.has_value());
 }
 
 BOOST_AUTO_TEST_CASE(decode_data_and_input_precedence)
 {
     // Given both "data" and "input" present, "data" should take precedence
     Json::Value root(Json::objectValue);
-    root["data"] = "0xdeadbeef";
+    constexpr std::string_view hexData =
+        "0x26c8917e00000000000000000000000000000000000000000000000000000000000000600000000000000000"
+        "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+        "00000000000000000003e8000000000000000000000000000000000000000000000000000000000000002chszs"
+        "ray4r2rgyr8vmossvvgh2xawx7csbmhn4gbqmwhd00000000000000000000";
+    root["data"] = std::string{hexData};
     root["input"] = "0x1122";
     root["to"] = "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
     auto [ok, req] = decodeCallRequest(root);
-    BOOST_CHECK(ok);
-    constexpr std::string_view hexData = "0xdeadbeef";
+    BOOST_TEST(ok);
+
     auto expected = fromHexWithPrefix(hexData);
-    BOOST_CHECK(req.data == expected);
+    BOOST_TEST(req.data == expected);
     BOOST_CHECK_EQUAL(req.to, "0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 }
 
@@ -61,27 +66,27 @@ BOOST_AUTO_TEST_CASE(decode_all_fields)
     root["maxFeePerGas"] = "0x4";
 
     auto [ok, req] = decodeCallRequest(root);
-    BOOST_CHECK(ok);
+    BOOST_TEST(ok);
 
-    BOOST_CHECK(req.from.has_value());
+    BOOST_TEST(req.from.has_value());
     BOOST_CHECK_EQUAL(req.from.value(), "0x1234567890abcdef1234567890abcdef12345678");
     BOOST_CHECK_EQUAL(req.to, "0xabcdefabcdefabcdefabcdefabcdefabcdefabcd");
     constexpr std::string_view dataHex = "0x010203";
-    BOOST_CHECK(req.data == fromHexWithPrefix(dataHex));
+    BOOST_TEST(req.data == fromHexWithPrefix(dataHex));
 
-    BOOST_CHECK(req.gas.has_value());
+    BOOST_TEST(req.gas.has_value());
     BOOST_CHECK_EQUAL(req.gas.value(), 21000);
 
-    BOOST_CHECK(req.gasPrice.has_value());
+    BOOST_TEST(req.gasPrice.has_value());
     BOOST_CHECK_EQUAL(req.gasPrice.value(), "0x1");
 
-    BOOST_CHECK(req.value.has_value());
+    BOOST_TEST(req.value.has_value());
     BOOST_CHECK_EQUAL(req.value.value(), "0x2");
 
-    BOOST_CHECK(req.maxPriorityFeePerGas.has_value());
+    BOOST_TEST(req.maxPriorityFeePerGas.has_value());
     BOOST_CHECK_EQUAL(req.maxPriorityFeePerGas.value(), "0x3");
 
-    BOOST_CHECK(req.maxFeePerGas.has_value());
+    BOOST_TEST(req.maxFeePerGas.has_value());
     BOOST_CHECK_EQUAL(req.maxFeePerGas.value(), "0x4");
 }
 
@@ -93,8 +98,8 @@ BOOST_AUTO_TEST_CASE(decode_invalid_hex_data_is_ignored)
     root["to"] = "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
 
     auto [ok, req] = decodeCallRequest(root);
-    BOOST_CHECK(ok);
-    BOOST_CHECK(req.data.empty());
+    BOOST_TEST(ok);
+    BOOST_TEST(req.data.empty());
     BOOST_CHECK_EQUAL(req.to, "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
 }
 
@@ -103,9 +108,9 @@ BOOST_AUTO_TEST_CASE(decode_uses_input_when_data_missing)
     Json::Value root(Json::objectValue);
     root["input"] = "0xabcdef";
     auto [ok, req] = decodeCallRequest(root);
-    BOOST_CHECK(ok);
+    BOOST_TEST(ok);
     constexpr std::string_view inputHex = "0xabcdef";
-    BOOST_CHECK(req.data == fromHexWithPrefix(inputHex));
+    BOOST_TEST(req.data == fromHexWithPrefix(inputHex));
 }
 
 BOOST_AUTO_TEST_CASE(decode_invalid_gas_string)
@@ -126,8 +131,8 @@ BOOST_AUTO_TEST_CASE(decode_numeric_gas_treated_as_hex)
     Json::Value root(Json::objectValue);
     root["gas"] = Json::Int64(kGasDecimal);  // asString => "21000"; hex 0x21000 == 135168
     auto [ok, req] = decodeCallRequest(root);
-    BOOST_CHECK(ok);
-    BOOST_CHECK(req.gas.has_value());
+    BOOST_TEST(ok);
+    BOOST_TEST(req.gas.has_value());
     BOOST_CHECK_EQUAL(req.gas.value(), kExpectedHexParsed);
 }
 
@@ -137,8 +142,8 @@ BOOST_AUTO_TEST_CASE(decode_invalid_from_does_not_fail)
     Json::Value root(Json::objectValue);
     root["from"] = "not_an_address";
     auto [ok, req] = decodeCallRequest(root);
-    BOOST_CHECK(ok);
-    BOOST_CHECK(req.from.has_value());
+    BOOST_TEST(ok);
+    BOOST_TEST(req.from.has_value());
     BOOST_CHECK_EQUAL(req.from.value(), "not_an_address");
 }
 
@@ -151,14 +156,14 @@ BOOST_AUTO_TEST_CASE(decode_invalid_fee_fields_pass_through)
     root["maxPriorityFeePerGas"] = "abc";
     root["maxFeePerGas"] = "\t";
     auto [ok, req] = decodeCallRequest(root);
-    BOOST_CHECK(ok);
-    BOOST_CHECK(req.gasPrice.has_value());
+    BOOST_TEST(ok);
+    BOOST_TEST(req.gasPrice.has_value());
     BOOST_CHECK_EQUAL(req.gasPrice.value(), "0xzz");
-    BOOST_CHECK(req.value.has_value());
+    BOOST_TEST(req.value.has_value());
     BOOST_CHECK_EQUAL(req.value.value(), "-123");
-    BOOST_CHECK(req.maxPriorityFeePerGas.has_value());
+    BOOST_TEST(req.maxPriorityFeePerGas.has_value());
     BOOST_CHECK_EQUAL(req.maxPriorityFeePerGas.value(), "abc");
-    BOOST_CHECK(req.maxFeePerGas.has_value());
+    BOOST_TEST(req.maxFeePerGas.has_value());
     BOOST_CHECK_EQUAL(req.maxFeePerGas.value(), "\t");
 }
 
