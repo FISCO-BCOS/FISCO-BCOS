b06ba2798adf7d31099fb55e9f5e9f2510fef06a
monan <651932351@qq.com>
2025-10-28 18:55:21 +0800

    Update transactions struct

--- bcos-txpool/bcos-txpool/txpool/storage/Web3Transactions.h
+++ bcos-txpool/bcos-txpool/txpool/storage/Web3Transactions.h
@@ -1,15 +1,15 @@
 #pragma once
 
 #include "bcos-framework/bcos-framework/protocol/Transaction.h"
-#include "txpool/interfaces/TxPoolStorageInterface.h"
-#include <oneapi/tbb/concurrent_unordered_map.h>
+#include <tbb/concurrent_unordered_map.h>
 #include <boost/multi_index/composite_key.hpp>
 #include <boost/multi_index/hashed_index.hpp>
 #include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index/sequenced_index.hpp>
 #include <boost/multi_index_container.hpp>
-#include <string>
+#include <string_view>
 
 namespace bcos::txpool
 {
@@ -25,67 +25,29 @@ struct TransactionData
     int64_t nonce() const;
 };
 
-class AccountTransactions
+class Web3Transactions
 {
 private:
     using Transactions = boost::multi_index_container<TransactionData,
         boost::multi_index::indexed_by<
-            boost::multi_index::ordered_unique<boost::multi_index::const_mem_fun<TransactionData,
-                                                   int64_t, &TransactionData::nonce>,
-                std::less<>>,
+            boost::multi_index::ordered_unique<
+                boost::multi_index::composite_key<TransactionData,
+                    boost::multi_index::const_mem_fun<TransactionData, std::string_view,
+                        &TransactionData::sender>,
+                    boost::multi_index::const_mem_fun<TransactionData, int64_t,
+                        &TransactionData::nonce>>,
+                boost::multi_index::composite_key_compare<std::less<>, std::less<>>>,
             boost::multi_index::hashed_unique<boost::multi_index::const_mem_fun<TransactionData,
                 bcos::crypto::HashType, &TransactionData::hash>>,
             boost::multi_index::sequenced<>>>;
 
     Transactions m_transactions;
-    Transactions::nth_index<0>::type::const_iterator m_pendingEnd = m_transactions.begin();
-    int64_t m_sealed = 0;
     std::mutex m_mutex;
 
 public:
-    using OrderedIndexConstIterator = Transactions::nth_index<0>::type::const_iterator;
-
     bool add(protocol::Transaction::Ptr transaction);
-    void remove(int64_t lastNonce);
-    void seal(int64_t limit, std::vector<protocol::Transaction::Ptr>& out);
-    void mark(int64_t lastNonce);
-
-    void updateLongestSequence(OrderedIndexConstIterator updatedIt);
-};
-
-
-class Web3Transactions
-{
-private:
-    struct StringHash
-    {
-        struct StringEqual
-        {
-            using is_transparent = void;
-            template <std::convertible_to<std::string_view> T,
-                std::convertible_to<std::string_view> U>
-            bool operator()(const T& lhs, const U& rhs) const
-            {
-                return lhs == rhs;
-            }
-        };
-        using transparent_key_equal = StringEqual;
-
-        template <std::convertible_to<std::string_view> T>
-        std::size_t operator()(const T& str) const
-        {
-            return std::hash<std::string_view>{}(std::string_view(str));
-        }
-    };
-
-    tbb::concurrent_unordered_map<std::string, AccountTransactions, StringHash>
-        m_accountTransactions;
-
-public:
-    bool add(protocol::Transaction::Ptr transaction);
-    void remove(std::string_view sender, int64_t lastNonce);
-    std::vector<protocol::Transaction::Ptr> seal(int64_t limit);
-    void mark(std::string_view sender, int64_t lastNonce);
+    void seal(std::string_view sender, int64_t startNonce, int64_t limit,
+        std::vector<protocol::Transaction::Ptr>& out);
 };
 
 }  // namespace bcos::txpool
