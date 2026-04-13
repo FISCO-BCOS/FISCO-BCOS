e87603a1f3b6b4fb5807fb86b57eda1b54c3a32f
monan <651932351@qq.com>
2026-04-13 23:20:18 +0800

    Fix warning

--- bcos-framework/bcos-framework/ledger/EVMAccount.h
+++ bcos-framework/bcos-framework/ledger/EVMAccount.h
@@ -3,7 +3,7 @@
 #include "bcos-framework/executor/PrecompiledTypeDef.h"
 #include "bcos-framework/ledger/LedgerTypeDef.h"
 #include "bcos-framework/storage/Entry.h"
-#include "bcos-framework/storage/LegacyStorageMethods.h"
+// #include "bcos-framework/storage/LegacyStorageMethods.h"
 #include "bcos-task/Task.h"
 #include "bcos-utilities/Exceptions.h"
 #include <evmc/evmc.h>
@@ -279,7 +279,7 @@ public:
             storage,
             [](const bcos::Address& address) {
                 evmc_address evmcAddress;
-                ::ranges::copy(address, evmcAddress.bytes);
+                ::ranges::copy(address, std::span(evmcAddress.bytes).data());
                 return evmcAddress;
             }(address),
             binaryAddress)
