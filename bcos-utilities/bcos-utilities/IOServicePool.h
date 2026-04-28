2315c3e040aa66300e3e50fac2a8f8e6a52ad154
monan <651932351@qq.com>
2026-04-28 08:13:52 +0800

    Move impl to cpp

--- bcos-utilities/bcos-utilities/IOServicePool.h
+++ bcos-utilities/bcos-utilities/IOServicePool.h
@@ -32,83 +32,17 @@ public:
     using ExecutorType = boost::asio::io_context::executor_type;
     using Work = boost::asio::executor_work_guard<ExecutorType>;
     using WorkPtr = std::unique_ptr<Work>;
-    explicit IOServicePool(size_t _workerNum = std::thread::hardware_concurrency() + 1)
-      : m_works(_workerNum), m_nextIOService(0)
-    {
-        // create the ioservices
-        for (size_t i = 0; i < _workerNum; i++)
-        {
-            m_ioServices.emplace_back(std::make_shared<IOService>());
-        }
-    }
+    explicit IOServicePool(size_t _workerNum = std::thread::hardware_concurrency() + 1);
 
     IOServicePool(const IOServicePool&) = delete;
     IOServicePool& operator=(const IOServicePool&) = delete;
-    virtual ~IOServicePool() { stop(); }
+    virtual ~IOServicePool();
 
-    void start()
-    {
-        if (m_running)
-        {
-            return;
-        }
-        m_running = true;
-        for (size_t i = 0; i < m_ioServices.size(); ++i)
-        {
-            m_works[i] = std::make_unique<Work>(m_ioServices[i]->get_executor());
-        }
+    void start();
 
-        // one io_context per thread
-        for (const auto& ioService : m_ioServices)
-        {
-            // https://github.com/chriskohlhoff/asio/issues/932#issuecomment-968103444
-            m_threads.emplace_back([ioService, running = std::ref(m_running)]() {
-                if (!running)
-                {
-                    return;
-                }
+    std::shared_ptr<IOService> getIOService();
 
-                bcos::pthread_setThreadName("ioService");
-                ioService->run();
-            });
-        }
-    }
-
-    std::shared_ptr<IOService> getIOService()
-    {
-        auto selectedIoService = (m_nextIOService.fetch_add(1) % m_ioServices.size());
-        return m_ioServices.at(selectedIoService);
-    }
-
-    void stop()
-    {
-        if (!m_running)
-        {
-            return;
-        }
-        m_running = false;
-
-        // stop the work
-        for (auto& work : m_works)
-        {
-            work.reset();
-        }
-
-        // stop the io service
-        for (auto& ioService : m_ioServices)
-        {
-            ioService->stop();
-        }
-        // stop the thread
-        for (auto& thread : m_threads)
-        {
-            if (thread.joinable())
-            {
-                thread.join();
-            }
-        }
-        m_threads.clear();
-    }
+    void stop();
 
 private:
     std::vector<std::shared_ptr<IOService>> m_ioServices;
--- bcos-utilities/bcos-utilities/RateCollector.cpp
+++ bcos-utilities/bcos-utilities/RateCollector.cpp
@@ -19,10 +19,45 @@
  */
 
 #include "RateCollector.h"
+#include "bcos-utilities/BoostLog.h"
+#include "bcos-utilities/Timer.h"
 
 using namespace bcos;
 
 bool c_enableStatCollector = false;
+
+RateCollector::RateCollector(std::string _moduleName, uint64_t _intervalMS)
+  : m_moduleName(std::move(_moduleName)), m_intervalMS(_intervalMS)
+{
+    m_reportTimer = std::make_shared<Timer>(_intervalMS, m_moduleName);
+    m_reportTimer->registerTimeoutHandler([this]() {
+        report();
+        flush();
+        m_reportTimer->restart();
+    });
+}
+
+RateCollector::~RateCollector()
+{
+    stop();
+}
+
+void RateCollector::start()
+{
+    if (m_reportTimer)
+    {
+        m_reportTimer->start();
+    }
+}
+
+void RateCollector::stop()
+{
+    if (m_reportTimer)
+    {
+        m_reportTimer->stop();
+        m_reportTimer->destroy();
+    }
+}
 
 
 void RateCollector::enable()
@@ -38,4 +73,48 @@ void RateCollector::disable()
 bool RateCollector::isEnable()
 {
     return c_enableStatCollector;
+}
+
+void RateCollector::report()
+{
+    if (!isEnable())
+    {
+        return;
+    }
+
+    auto& stat = m_rateCollectorStat;
+    BCOS_LOG(INFO) << LOG_BADGE("RateCollector")
+                   << LOG_BADGE(m_moduleName)
+                   << LOG_KV("lastCount", stat.lastCount)
+                   << LOG_KV("lastTotalDataSize", stat.lastTotalDataSize)
+                   << LOG_KV("lastFailedCount", stat.lastFailedCount)
+                   << LOG_KV("lastTotalFailedDataSize", stat.lastTotalFailedDataSize)
+                   << LOG_KV("lastRate(Mb/s)", calcAvgRate(stat.lastTotalDataSize, m_intervalMS))
+                   << LOG_KV("lastQPS(request/s)", calcAvgQPS(stat.lastCount, m_intervalMS));
+}
+
+void RateCollector::flush()
+{
+    m_rateCollectorStat.lastCount = 0;
+    m_rateCollectorStat.lastFailedCount = 0;
+    m_rateCollectorStat.lastTotalDataSize = 0;
+    m_rateCollectorStat.lastTotalFailedDataSize = 0;
+}
+
+void RateCollector::update(std::size_t _dataSize, bool _success)
+{
+    if (_success)
+    {
+        m_rateCollectorStat.totalCount++;
+        m_rateCollectorStat.lastCount++;
+        m_rateCollectorStat.totalDataSize += _dataSize;
+        m_rateCollectorStat.lastTotalDataSize += _dataSize;
+    }
+    else
+    {
+        m_rateCollectorStat.totalFailedCount++;
+        m_rateCollectorStat.lastFailedCount++;
+        m_rateCollectorStat.totalFailedDataSize += _dataSize;
+        m_rateCollectorStat.lastTotalFailedDataSize += _dataSize;
+    }
 }
