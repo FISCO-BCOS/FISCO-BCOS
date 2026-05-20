5f1259f7b6261f34f728753dff1ba8964396e212
MO NAN <651932351@qq.com>
2026-05-20 20:23:40 +0800

    Implement checkpoint storage for RocksDB (#5208)

--- bcos-storage/bcos-storage/CheckpointRocksDBStorage.cpp
+++ bcos-storage/bcos-storage/CheckpointRocksDBStorage.cpp
@@ -1,47 +1,40 @@
 #include "bcos-storage/CheckpointRocksDBStorage.h"
-#include "bcos-utilities/Exceptions.h"
-#include <rocksdb/options.h>
+#include <rocksdb/utilities/checkpoint.h>
 #include <boost/throw_exception.hpp>
 #include <filesystem>
+#include <optional>
 
 namespace bcos::storage2::rocksdb
 {
-
-CheckpointRocksDBStorage::CheckpointRocksDBStorage(std::string_view rootDir)
-  : m_path(std::filesystem::path(rootDir).lexically_normal().string())
-{}
-
-const std::string& CheckpointRocksDBStorage::path() const noexcept
+namespace
 {
-    return m_path;
+std::string resolveCheckpointsPath(std::string_view rootDir)
+{
+    auto path = std::filesystem::path(rootDir) / "checkpoints";
+    return path.lexically_normal().string();
 }
 
-std::string CheckpointRocksDBStorage::latestPath() const
+std::optional<bcos::h256> parseCheckpointName(std::string const& checkpointName)
 {
-    return resolveLatestPath(m_path);
+    try
+    {
+        return bcos::h256(checkpointName);
+    }
+    catch (...)
+    {
+        return std::nullopt;
+    }
 }
+}  // namespace
 
-std::string CheckpointRocksDBStorage::checkpointPath(std::string_view checkpointName) const
+namespace detail
 {
-    return resolveCheckpointPath(m_path, checkpointName);
-}
-
-std::unique_ptr<::rocksdb::DB> CheckpointRocksDBStorage::open(
-    LatestCheckpointStorageTag latestTag) const
-{
-    return openRocksDB(resolveLatestPath(m_path), latestOptions(latestTag));
-}
-
-std::unique_ptr<::rocksdb::DB> CheckpointRocksDBStorage::open(std::string_view checkpointName) const
-{
-    return openRocksDB(resolveCheckpointPath(m_path, checkpointName), checkpointOptions());
-}
-
-std::unique_ptr<::rocksdb::DB> CheckpointRocksDBStorage::openRocksDB(
-    const std::string& path, const ::rocksdb::Options& options)
+std::unique_ptr<::rocksdb::DB> openCheckpointRocksDB(
+    const std::string& path, const ::rocksdb::Options& options, bool readOnly)
 {
     ::rocksdb::DB* rocksDB = nullptr;
-    auto status = ::rocksdb::DB::Open(options, path, &rocksDB);
+    auto status = readOnly ? ::rocksdb::DB::OpenForReadOnly(options, path, &rocksDB) :
+                             ::rocksdb::DB::Open(options, path, &rocksDB);
     if (!status.ok())
     {
         BOOST_THROW_EXCEPTION(
@@ -51,32 +44,126 @@ std::unique_ptr<::rocksdb::DB> CheckpointRocksDBStorage::openRocksDB(
     return std::unique_ptr<::rocksdb::DB>(rocksDB);
 }
 
-::rocksdb::Options CheckpointRocksDBStorage::latestOptions(LatestCheckpointStorageTag /*latestTag*/)
+::rocksdb::Options latestCheckpointOptions()
 {
     ::rocksdb::Options options;
     options.create_if_missing = true;
     return options;
 }
 
-::rocksdb::Options CheckpointRocksDBStorage::checkpointOptions()
+::rocksdb::Options historicalCheckpointOptions()
 {
     ::rocksdb::Options options;
     options.create_if_missing = false;
     return options;
 }
 
-std::string CheckpointRocksDBStorage::resolveLatestPath(std::string_view rootDir)
+void ensureCheckpointDirectories(std::string_view rootDir)
+{
+    auto normalizedRoot = std::filesystem::path(rootDir).lexically_normal();
+    std::filesystem::create_directories(normalizedRoot);
+    std::filesystem::create_directories(normalizedRoot / "checkpoints");
+}
+
+std::string resolveLatestCheckpointPath(std::string_view rootDir)
 {
     auto path = std::filesystem::path(rootDir) / "latest";
     return path.lexically_normal().string();
 }
 
-std::string CheckpointRocksDBStorage::resolveCheckpointPath(
-    std::string_view rootDir, std::string_view checkpointName)
+std::string resolveHistoricalCheckpointPath(
+    std::string_view rootDir, bcos::h256 const& checkpointName)
 {
-    auto path =
-        std::filesystem::path(rootDir) / "checkpoints" / std::filesystem::path(checkpointName);
+    auto path = std::filesystem::path(rootDir) / "checkpoints" /
+                std::filesystem::path(checkpointName.hex());
     return path.lexically_normal().string();
 }
 
+void createHistoricalCheckpoint(
+    ::rocksdb::DB& latestStorage, std::string_view rootDir, bcos::h256 const& checkpointName)
+{
+    auto path = resolveHistoricalCheckpointPath(rootDir, checkpointName);
+    if (std::filesystem::exists(path))
+    {
+        BOOST_THROW_EXCEPTION(CheckpointRocksDBException{} << bcos::errinfo_comment(
+                                  "Checkpoint already exists: " + checkpointName.hex()));
+    }
+
+    ::rocksdb::Checkpoint* rawCheckpoint = nullptr;
+    auto status = ::rocksdb::Checkpoint::Create(&latestStorage, &rawCheckpoint);
+    if (!status.ok())
+    {
+        BOOST_THROW_EXCEPTION(
+            CheckpointRocksDBException{} << bcos::errinfo_comment(
+                "Create rocksdb checkpoint handler failed, error: " + status.ToString()));
+    }
+
+    std::unique_ptr<::rocksdb::Checkpoint> checkpointGuard(rawCheckpoint);
+    status = checkpointGuard->CreateCheckpoint(path);
+    if (!status.ok())
+    {
+        BOOST_THROW_EXCEPTION(CheckpointRocksDBException{} << bcos::errinfo_comment(
+                                  "Create rocksdb checkpoint failed, path: " + path +
+                                  ", error: " + status.ToString()));
+    }
+}
+
+void deleteHistoricalCheckpoint(std::string_view rootDir, bcos::h256 const& checkpointName)
+{
+    std::error_code errorCode;
+    std::filesystem::remove_all(
+        resolveHistoricalCheckpointPath(rootDir, checkpointName), errorCode);
+    if (errorCode)
+    {
+        BOOST_THROW_EXCEPTION(CheckpointRocksDBException{} << bcos::errinfo_comment(
+                                  "Delete checkpoint failed, name: " + checkpointName.hex() +
+                                  ", error: " + errorCode.message()));
+    }
+}
+
+std::optional<bcos::h256> findCheckpointByTime(std::string_view rootDir, bool latest)
+{
+    std::error_code errorCode;
+    auto checkpointsPath = resolveCheckpointsPath(rootDir);
+    if (!std::filesystem::exists(checkpointsPath, errorCode) || errorCode)
+    {
+        return std::nullopt;
+    }
+
+    std::optional<std::pair<bcos::h256, std::filesystem::file_time_type>> selected;
+    for (auto const& entry : std::filesystem::directory_iterator(checkpointsPath, errorCode))
+    {
+        if (errorCode)
+        {
+            break;
+        }
+
+        if (!entry.is_directory(errorCode) || errorCode)
+        {
+            errorCode.clear();
+            continue;
+        }
+
+        auto checkpoint = parseCheckpointName(entry.path().filename().string());
+        if (!checkpoint)
+        {
+            continue;
+        }
+
+        auto time = entry.last_write_time(errorCode);
+        if (errorCode)
+        {
+            errorCode.clear();
+            continue;
+        }
+
+        if (!selected || (latest ? time > selected->second : time < selected->second))
+        {
+            selected = std::make_pair(*checkpoint, time);
+        }
+    }
+
+    return selected ? std::make_optional(selected->first) : std::nullopt;
+}
+}  // namespace detail
 }  // namespace bcos::storage2::rocksdb
