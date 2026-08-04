1ac99b22e1ba3160431cacabb219ccf74f283b9f
Shichen-Wu <stanwu@webank.com>
2026-08-04 20:15:17 +0800

    fix TestEthereumExecutorScheduler.cpp

--- transaction-scheduler/tests/TestEthereumExecutorScheduler.cpp
+++ transaction-scheduler/tests/TestEthereumExecutorScheduler.cpp
@@ -193,8 +193,7 @@ task::Task<void> EERunTransfers(Scheduler& scheduler, EthereumExecutor& executor
     // (the parent) stays resolvable.
     co_await EEWriteCurrentNumber(backendStorage, 0);
 
-    bcostars::protocol::BlockHeaderImpl blockHeader(
-        [inner = bcostars::BlockHeader()]() mutable { return std::addressof(inner); });
+    bcostars::protocol::BlockHeaderImpl blockHeader;
     blockHeader.setNumber(1);
     blockHeader.calculateHash(*cryptoSuite->hashImpl());
 
@@ -435,8 +434,7 @@ BOOST_AUTO_TEST_CASE(serialInvalidTxDoesNotAbortBlock)
         co_await EEWriteBlockHash(
             backendStorage, 1, cryptoSuite->hashImpl()->hash(std::string("block-1")));
 
-        bcostars::protocol::BlockHeaderImpl blockHeader(
-            [inner = bcostars::BlockHeader()]() mutable { return std::addressof(inner); });
+        bcostars::protocol::BlockHeaderImpl blockHeader;
         blockHeader.setNumber(1);
         blockHeader.calculateHash(*cryptoSuite->hashImpl());
 
@@ -497,8 +495,7 @@ BOOST_AUTO_TEST_CASE(serialSameNonceSecondRejected)
             backendStorage, 1, cryptoSuite->hashImpl()->hash(std::string("block-1")));
         co_await EEWriteCurrentNumber(backendStorage, 0);
 
-        bcostars::protocol::BlockHeaderImpl blockHeader(
-            [inner = bcostars::BlockHeader()]() mutable { return std::addressof(inner); });
+        bcostars::protocol::BlockHeaderImpl blockHeader;
         blockHeader.setNumber(1);
         blockHeader.calculateHash(*cryptoSuite->hashImpl());
 
@@ -556,8 +553,7 @@ BOOST_AUTO_TEST_CASE(parallelSameNonceSecondRejected)
             backendStorage, 1, cryptoSuite->hashImpl()->hash(std::string("block-1")));
         co_await EEWriteCurrentNumber(backendStorage, 0);
 
-        bcostars::protocol::BlockHeaderImpl blockHeader(
-            [inner = bcostars::BlockHeader()]() mutable { return std::addressof(inner); });
+        bcostars::protocol::BlockHeaderImpl blockHeader;
         blockHeader.setNumber(1);
         blockHeader.calculateHash(*cryptoSuite->hashImpl());
 
