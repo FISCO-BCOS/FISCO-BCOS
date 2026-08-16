4e0673e887ab6ac47f0125cda18aae39a2759a13
monan <651932351@qq.com>
2026-08-16 16:58:15 +0800

    Fix gcc compile

--- bcos-evm/test/opstack/TestMain.cpp
+++ bcos-evm/test/opstack/TestMain.cpp
@@ -1,1 +1,2 @@
 #define BOOST_TEST_MODULE BcosEvmOpstackTests
+#include <boost/test/unit_test.hpp>
--- legacy/bcos-ledger/test/main.cpp
+++ legacy/bcos-ledger/test/main.cpp
@@ -1,1 +1,2 @@
 #define BOOST_TEST_MODULE "test-legacy-ledger"
+#include <boost/test/unit_test.hpp>
