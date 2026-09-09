a92629902da1086afbca9b4a1d30db70f7827e8d
user.email <912554887@qq.com>
2026-09-09 17:36:01 +0800

    fix(rpc): remove duplicate feeHistory declaration in EthEndpoint
    Cherry-pick merge left two identical method declarations.

--- bcos-rpc/bcos-rpc/web3jsonrpc/endpoints/EthEndpoint.h
+++ bcos-rpc/bcos-rpc/web3jsonrpc/endpoints/EthEndpoint.h
@@ -80,7 +80,6 @@ public:
     task::Task<void> maxPriorityFeePerGas(const Json::Value&, Json::Value&);
     task::Task<void> feeHistory(const Json::Value&, Json::Value&);
     task::Task<void> getProof(const Json::Value&, Json::Value&);
-    task::Task<void> feeHistory(const Json::Value&, Json::Value&);
 
 private:
     NodeService::Ptr m_nodeService;
