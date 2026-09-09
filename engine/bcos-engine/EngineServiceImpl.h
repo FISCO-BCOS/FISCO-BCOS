f6d63e9ff3a7d1b31576dd3c86e2a92028812bfa
user.email <912554887@qq.com>
2026-09-09 14:34:52 +0800

    chore(style): restore release license headers on touched files
    Revert Apache-2.0 header spacing to match fisco/release-3.18.0 on files
    that already existed there, removing comment-cleanup diff noise while
    keeping all functional changes intact.

--- engine/bcos-engine/EngineServiceImpl.h
+++ engine/bcos-engine/EngineServiceImpl.h
@@ -1,24 +1,24 @@
 /**
- * Copyright (C) 2024 FISCO BCOS.
- * SPDX-License-Identifier: Apache-2.0
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Copyright (C) 2024 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
  * @file EngineServiceImpl.h
  * @brief Engine API template still constructed by EngineServiceInitializer.
  *
- * Tracker, PayloadCache, and engine_common helpers live beside the legacy template.
- * Production Initializer still instantiates EngineServiceImpl; switching to
- * EthEngineService / OpEngineService is a follow-up cutover PR.
+ * This extract adds Tracker / PayloadCache / engine_common beside the live
+ * template. It does not cut production over: Initializer still instantiates
+ * EngineServiceImpl. EthEngineService / OpEngineService are later PRs.
  */
 
 #pragma once
